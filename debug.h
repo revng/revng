b360797d2dc1bd454c6f609c70182f041948a8ee
Alessandro Di Federico <ale@clearmind.me>
2016-08-20 00:23:02 +0200

    `ScopedDebugFeature`: scope-limited debug feature

--- debug.h
+++ debug.h
@@ -2,7 +2,9 @@
 #define _DEBUG_H
 
 // Standard includes
+#include <functional>
 #include <ostream>
+#include <string>
 
 // LLVM includes
 #include "llvm/Support/Debug.h"
@@ -16,11 +18,32 @@ void enableDebugFeature(std::string Name);
 void disableDebugFeature(std::string Name);
 
 /// Executes \p code only if \p feature is enabled
+// TODO: switch to lambda
 #define DBG(feature, code) do {                                     \
     if (DebuggingEnabled && isDebugFeatureEnabled(feature)) {       \
       code;                                                         \
     }                                                               \
   } while (0)
 
+/// \brief Enables a debug feature and disables it when goes out of scope
+class ScopedDebugFeature {
+public:
+  /// \param Name the name of the debugging feature
+  /// \param Enable whether to actually enable it or not
+  ScopedDebugFeature(std::string Name, bool Enable)
+    : Name(Name), Enabled(Enable) {
+    if (Enabled)
+      enableDebugFeature(Name);
+  }
+
+  ~ScopedDebugFeature() {
+    if (Enabled)
+      disableDebugFeature(Name);
+  }
+
+private:
+  std::string Name;
+  bool Enabled;
+};
 
 #endif // _DEBUG_H
