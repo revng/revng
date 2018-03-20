8c148607d367b309b4d7d09931b6660be76d6bcb
Alessandro Di Federico <ale@clearmind.me>
2018-03-20 12:51:10 +0100

    Introduce `ClassSentinel`
    Sometimes, dealing with move-semantics in C++ can be challenging. To
    mitigate this problem, this commit introduces `ClassSentinel`, a simple
    class that can be added as a member of a class and that will allow the
    user to easily monitor if an instance of a class is used after being
    moved in an unwanted way. To do so, simply call the
    `ClassSentinel::check` method on the class member.
    
    `ClassSentinel` performs similar (but less reliable) checks on usage of
    destroyed objects.
    
    `ClassSentinel` can also (optionally, though the `SENTINEL_STACKTRACES`
    macro) collect stack traces of the points where the object was moved or
    destroyed.
    
    This commit also includes some basic testing for the class.

--- tests/Unit/UnitTests.cmake
+++ tests/Unit/UnitTests.cmake
@@ -9,7 +9,8 @@ set(SRC "${CMAKE_SOURCE_DIR}/tests/Unit")
 set(Boost_ADDITIONAL_VERSIONS "1.63" "1.63.0")
 find_package(Boost 1.63.0 REQUIRED COMPONENTS unit_test_framework)
 
-add_executable(test_lazysmallbitvector "${SRC}/lazysmallbitvector.cpp")
+add_executable(test_lazysmallbitvector
+  "${SRC}/lazysmallbitvector.cpp")
 target_include_directories(test_lazysmallbitvector PRIVATE "${CMAKE_SOURCE_DIR}" "${Boost_INCLUDE_DIRS}")
 target_compile_definitions(test_lazysmallbitvector PRIVATE "BOOST_TEST_DYN_LINK=1")
 target_link_libraries(test_lazysmallbitvector ${Boost_UNIT_TEST_FRAMEWORK_LIBRARY})
@@ -20,3 +21,11 @@ target_include_directories(test_stackanalysis PRIVATE "${CMAKE_SOURCE_DIR}" "${B
 target_compile_definitions(test_stackanalysis PRIVATE "BOOST_TEST_DYN_LINK=1")
 target_link_libraries(test_stackanalysis ${Boost_UNIT_TEST_FRAMEWORK_LIBRARY} ${LLVM_LIBRARIES})
 add_test(NAME test_stackanalysis COMMAND test_stackanalysis)
+
+add_executable(test_classsentinel
+  "${SRC}/classsentinel.cpp"
+  "${CMAKE_SOURCE_DIR}/debug.cpp")
+target_include_directories(test_classsentinel PRIVATE "${CMAKE_SOURCE_DIR}" "${Boost_INCLUDE_DIRS}")
+target_compile_definitions(test_classsentinel PRIVATE "BOOST_TEST_DYN_LINK=1")
+target_link_libraries(test_classsentinel ${Boost_UNIT_TEST_FRAMEWORK_LIBRARY} ${LLVM_LIBRARIES})
+add_test(NAME test_classsentinel COMMAND test_classsentinel)
