fb7ad64070fede6df18d39f9b374f89656540fbf
Alessandro Di Federico <ale@clearmind.me>
2018-08-31 08:32:27 +0200

    CMake: one argument per line
    This commit simply reduces the length of lines in CMake by splitting the
    statements over multiple lines. This is particularly useful when listing
    the translation units composing a program/library. In fact, it makes
    merge much easier.

--- tests/Unit/UnitTests.cmake
+++ tests/Unit/UnitTests.cmake
@@ -12,9 +12,14 @@ find_package(Boost 1.63.0 REQUIRED COMPONENTS unit_test_framework)
 add_executable(test_lazysmallbitvector
   "${SRC}/lazysmallbitvector.cpp"
   "${CMAKE_SOURCE_DIR}/revng-assert.cpp")
-target_include_directories(test_lazysmallbitvector PRIVATE "${CMAKE_SOURCE_DIR}" "${Boost_INCLUDE_DIRS}")
-target_compile_definitions(test_lazysmallbitvector PRIVATE "BOOST_TEST_DYN_LINK=1")
-target_link_libraries(test_lazysmallbitvector ${Boost_UNIT_TEST_FRAMEWORK_LIBRARY} ${LLVM_LIBRARIES})
+target_include_directories(test_lazysmallbitvector
+  PRIVATE "${CMAKE_SOURCE_DIR}"
+          "${Boost_INCLUDE_DIRS}")
+target_compile_definitions(test_lazysmallbitvector
+  PRIVATE "BOOST_TEST_DYN_LINK=1")
+target_link_libraries(test_lazysmallbitvector
+  ${Boost_UNIT_TEST_FRAMEWORK_LIBRARY}
+  ${LLVM_LIBRARIES})
 add_test(NAME test_lazysmallbitvector COMMAND test_lazysmallbitvector)
 
 add_executable(test_stackanalysis
@@ -22,16 +27,28 @@ add_executable(test_stackanalysis
   "${CMAKE_SOURCE_DIR}/debug.cpp"
   "${CMAKE_SOURCE_DIR}/statistics.cpp"
   "${CMAKE_SOURCE_DIR}/revng-assert.cpp")
-target_include_directories(test_stackanalysis PRIVATE "${CMAKE_SOURCE_DIR}" "${Boost_INCLUDE_DIRS}")
-target_compile_definitions(test_stackanalysis PRIVATE "BOOST_TEST_DYN_LINK=1")
-target_link_libraries(test_stackanalysis ${Boost_UNIT_TEST_FRAMEWORK_LIBRARY} ${LLVM_LIBRARIES})
+target_include_directories(test_stackanalysis
+  PRIVATE "${CMAKE_SOURCE_DIR}"
+          "${CMAKE_SOURCE_DIR}/lib/StackAnalysis"
+          "${CMAKE_BINARY_DIR}/lib/StackAnalysis"
+          "${Boost_INCLUDE_DIRS}")
+target_compile_definitions(test_stackanalysis
+  PRIVATE "BOOST_TEST_DYN_LINK=1")
+target_link_libraries(test_stackanalysis
+  ${Boost_UNIT_TEST_FRAMEWORK_LIBRARY}
+  ${LLVM_LIBRARIES})
 add_test(NAME test_stackanalysis COMMAND test_stackanalysis)
 
 add_executable(test_classsentinel
   "${SRC}/classsentinel.cpp"
   "${CMAKE_SOURCE_DIR}/debug.cpp"
   "${CMAKE_SOURCE_DIR}/revng-assert.cpp")
-target_include_directories(test_classsentinel PRIVATE "${CMAKE_SOURCE_DIR}" "${Boost_INCLUDE_DIRS}")
-target_compile_definitions(test_classsentinel PRIVATE "BOOST_TEST_DYN_LINK=1")
-target_link_libraries(test_classsentinel ${Boost_UNIT_TEST_FRAMEWORK_LIBRARY} ${LLVM_LIBRARIES})
+target_include_directories(test_classsentinel
+  PRIVATE "${CMAKE_SOURCE_DIR}"
+          "${Boost_INCLUDE_DIRS}")
+target_compile_definitions(test_classsentinel
+  PRIVATE "BOOST_TEST_DYN_LINK=1")
+target_link_libraries(test_classsentinel
+  ${Boost_UNIT_TEST_FRAMEWORK_LIBRARY}
+  ${LLVM_LIBRARIES})
 add_test(NAME test_classsentinel COMMAND test_classsentinel)
