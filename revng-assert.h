15178146ee335538bd6dfc802df8937dd3027175
Alessandro Di Federico <ale@clearmind.me>
2018-09-24 22:07:46 +0200

    Make message optional for `revng_` assertions
    This commit introduces a trick for having optional arguments in
    macros. This enables the `revng_abort` and `revng_unreachable` macros to
    have 0 or 1 argument, and the `revng_assert` and `revng_check` macros to
    have 1 or 2 arguments.
    
    Note that, by employing a macro trick (instead of C++ default
    arguments), we keep the header compatible with C files.

--- revng-assert.h
+++ revng-assert.h
@@ -92,7 +92,7 @@ noret void revng_do_abort(const char *Message, const char *File, unsigned Line);
 ///
 /// Use this macro to ensure program termination in case of an unexpected
 /// situation.
-#define revng_abort(message)                     \
+#define revng_abort_impl(message)                \
   do {                                           \
     revng_do_abort(message, __FILE__, __LINE__); \
   } while (0)
@@ -101,7 +101,7 @@ noret void revng_do_abort(const char *Message, const char *File, unsigned Line);
 ///
 /// Use this macro to ensure program termination in case of an unexpected
 /// situation.
-#define revng_check(what, message)                          \
+#define revng_check_impl(what, message)                     \
   do {                                                      \
     bool Condition = boolcast(what);                        \
     if (!Condition) {                                       \
@@ -118,14 +118,14 @@ noret void revng_do_abort(const char *Message, const char *File, unsigned Line);
 /// Use this macro to catch bugs during development an tell the compiler that a
 /// certain situation will never happen at run-time, which might open up to new
 /// optimization opportunities.
-#define revng_unreachable(message) revng_abort(message)
+#define revng_unreachable_impl(message) revng_abort(message)
 
 /// \brief Asserts \a what or, in debug mode, aborts with \a message.
 ///
 /// Use this macro to catch bugs during development an tell the compiler that a
 /// certain situation will never happen at run-time, which might open up to new
 /// optimization opportunities.
-#define revng_assert(what, message)                          \
+#define revng_assert_impl(what, message)                     \
   do {                                                       \
     bool Condition = boolcast(what);                         \
     if (!Condition) {                                        \
@@ -136,15 +136,64 @@ noret void revng_do_abort(const char *Message, const char *File, unsigned Line);
 
 #else
 
-#define revng_unreachable(message) __builtin_unreachable()
+#define revng_unreachable_impl(message) __builtin_unreachable()
 
-#define revng_assert(what, message) \
-  do {                              \
-    (void) sizeof((what));          \
-    silent_assume(what);            \
+#define revng_assert_impl(what, message) \
+  do {                                   \
+    (void) sizeof((what));               \
+    silent_assume(what);                 \
   } while (0)
 
 #endif
+
+#define COMMA_IF_INVOKED(...) ,
+#define CONCAT_TOKENS(a, b) a##b
+#define CONCAT2(a, b) CONCAT_TOKENS(a, b)
+#define CONCAT3(a, b, c) CONCAT2(CONCAT2(a, b), c)
+#define CONCAT4(a, b, c, d) CONCAT2(CONCAT3(a, b, c), d)
+#define CONCAT5(a, b, c, d, e) CONCAT2(CONCAT4(a, b, c, d), e)
+#define GET_10TH(_1, _2, _3, _4, _5, _6, _7, _8, _9, _10, ...) _10
+
+/// \brief Check if the argument list contains a comma
+#define HAS_COMMA(...) GET_10TH(__VA_ARGS__, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0)
+
+/// Check if the argument list is empty
+///
+/// This is a workaround to avoid using ##__VA_ARGS__ (which eats the previous
+/// comma if empty), since it's a GNU extension, and to void using default
+/// arguments, since we want this header to be functioncal in C files too.
+#define IS_EMPTY(...)                                        \
+  HAS_COMMA(CONCAT5(IS_EMPTY_,                               \
+                    HAS_COMMA(__VA_ARGS__),                  \
+                    HAS_COMMA(COMMA_IF_INVOKED __VA_ARGS__), \
+                    HAS_COMMA(__VA_ARGS__()),                \
+                    HAS_COMMA(COMMA_IF_INVOKED __VA_ARGS__())))
+// It's empty if the first three conditions are fasle and the last is true
+#define IS_EMPTY_0001 ,
+
+#define REVNG_ABORT_0(...) revng_abort_impl(__VA_ARGS__)
+#define REVNG_ABORT_1(...) revng_abort_impl("")
+#define revng_abort(...) \
+  CONCAT2(REVNG_ABORT_, IS_EMPTY(__VA_ARGS__))(__VA_ARGS__)
+
+#define REVNG_UNREACHABLE_0(...) revng_unreachable_impl(__VA_ARGS__)
+#define REVNG_UNREACHABLE_1(...) revng_unreachable_impl("")
+#define revng_unreachable(...) \
+  CONCAT2(REVNG_UNREACHABLE_, IS_EMPTY(__VA_ARGS__))(__VA_ARGS__)
+
+#define MACRO_OVERLOAD_1_OR_2(_1, _2, NAME, ...) NAME
+
+#define revng_assert_impl_nomsg(what) revng_assert_impl(what, "")
+#define revng_assert(...)                        \
+  MACRO_OVERLOAD_1_OR_2(__VA_ARGS__,             \
+                        revng_assert_impl,       \
+                        revng_assert_impl_nomsg) \
+  (__VA_ARGS__)
+
+#define revng_check_impl_nomsg(what) revng_check_impl(what, "")
+#define revng_check(...)                                                       \
+  MACRO_OVERLOAD_1_OR_2(__VA_ARGS__, revng_check_impl, revng_check_impl_nomsg) \
+  (__VA_ARGS__)
 
 #ifdef __cplusplus
 }
