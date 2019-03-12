b4758ae02de627a2402c5f5cd75ea899e242dbb6
Pietro Fezzardi <pietrofezzardi@gmail.com>
2019-03-12 18:29:50 +0100

    Move PHI analysis in PHIASAPAssignmentInfo

--- lib/Decompiler/PHIASAPAssignmentInfo.h
+++ lib/Decompiler/PHIASAPAssignmentInfo.h
@@ -5,11 +5,32 @@
 #include <llvm/Pass.h>
 
 struct PHIASAPAssignmentInfo : public llvm::FunctionPass {
+
+  using PHIIncomingMap = SmallMap<llvm::PHINode *, unsigned, 4>;
+  using BlockToPHIIncomingMap = SmallMap<llvm::BasicBlock *, PHIIncomingMap, 4>;
+
   static char ID;
 
   PHIASAPAssignmentInfo() : llvm::FunctionPass(ID) {}
 
   bool runOnFunction(llvm::Function &F) override;
+
+  struct PHIInfo {
+    llvm::BasicBlock *AllocaBlock;
+    llvm::SmallVector<llvm::BasicBlock *, 2> *AssignmentBlocks;
+
+    PHIInfo() = default;
+
+    PHIInfo(const PHIInfo &) = default;
+    PHIInfo &operator=(const PHIInfo &) = default;
+
+    PHIInfo(PHIInfo &&) = default;
+    PHIInfo &operator=(PHIInfo &&) = default;
+  };
+
+  private:
+  BlockToPHIIncomingMap PHIInfoMap;
+
 };
 
 #endif /* ifndef REVNGC_PHIASAPASSIGNMENTINFO_H */
