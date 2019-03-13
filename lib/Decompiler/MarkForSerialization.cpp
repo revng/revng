bc6e2fcb01a79595646ac3b744a3254427cc83c6
Pietro Fezzardi <pietrofezzardi@gmail.com>
2019-03-13 14:53:19 +0100

    Add MarkForSerialization MonotoneFramework

---
+++ lib/Decompiler/MarkForSerialization.cpp
@@ -0,0 +1,136 @@
+//
+// This file is distributed under the MIT License. See LICENSE.md for details.
+//
+
+/// \brief Dataflow analysis to identify which Instructions must be serialized
+
+// LLVM includes
+#include <llvm/IR/BasicBlock.h>
+#include <llvm/IR/Instructions.h>
+
+// revng includes
+#include <revng/Support/IRHelpers.h>
+#include <revng/Support/MonotoneFramework.h>
+
+// local libraries includes
+#include "revng-c/RestructureCFGPass/BasicBlockNode.h"
+#include "revng-c/RestructureCFGPass/RegionCFGTree.h"
+
+#include "MarkForSerialization.h"
+
+static Logger<> MarkLog("mark-serialization");
+
+using namespace llvm;
+
+namespace MarkForSerialization {
+
+static bool isPure(const Instruction & /*Call*/) {
+  return false;
+}
+
+void Analysis::initialize() {
+  Base::initialize();
+  ToSerialize.clear();
+  NDuplicates.clear();
+
+  {
+    // one empty set for each BB
+    using InstrSet = std::set<Instruction *>;
+    ToSerializeInBB = std::vector<InstrSet>(F.size(), InstrSet{});
+  }
+
+  size_t I = 0;
+  for (BasicBlock &BB : F) {
+    // map BasicBlock pointer to the position in ToSerializeInBB
+    BBToIdMap[&BB] = I++;
+  }
+  for (BasicBlockNode *BBNode : RCFG.nodes()) {
+    BasicBlock *BB = BBNode->getBasicBlock();
+    if (BBNode->isCode()) {
+      revng_assert(BB != nullptr);
+      NDuplicates[BB] += 1;
+    } else {
+      revng_assert(BB == nullptr);
+    }
+  }
+}
+
+Analysis::InterruptType Analysis::transfer(BasicBlock *BB) {
+  revng_log(MarkLog,
+            "transfer: BB in Function: " << BB->getParent()->getName() << '\n'
+                                         << BB);
+
+  LatticeElement Pending = this->State[BB].copy();
+
+  size_t NumDuplicatesOfBB = NDuplicates.at(BB);
+  for (Instruction &I : *BB) {
+    revng_log(MarkLog, "Analyzing: '" << &I << "': " << dumpToString(&I));
+    // PHINodes are never serialized
+    if (isa<PHINode>(&I))
+      continue;
+    // Skip this for now. We'll need to change it if we ever want to emit code
+    // with goto statements
+    // Branch instructions are never serialized directly, because it's only
+    // after building an AST and matching ifs, loops, switches and others that
+    // we really know what kind of C statement we want to emit for a given
+    // branch.
+    if (isa<BranchInst>(&I))
+      continue;
+    Pending.insert(&I);
+    revng_log(MarkLog, "Add to pending: '" << &I << "': " << dumpToString(&I));
+    revng_log(MarkLog, "Operands:");
+    MarkLog.indent();
+    for (auto &TheUse : I.operands()) {
+      Value *V = TheUse.get();
+      revng_log(MarkLog, "Op: '" << V << "': " << dumpToString(V));
+      MarkLog.indent();
+      if (auto *UseInstr = dyn_cast<Instruction>(V)) {
+        revng_log(MarkLog, "Op is Instruction");
+        Pending.erase(UseInstr);
+      } else {
+        revng_log(MarkLog, "Op is NOT Instruction");
+        revng_assert(isa<Argument>(V) or isa<Constant>(V) or isa<BasicBlock>(V)
+                     or isa<MetadataAsValue>(V));
+      }
+      MarkLog.unindent();
+    }
+    MarkLog.unindent();
+
+    bool HasSideEffects = isa<StoreInst>(&I)
+                          or (isa<CallInst>(&I) and not isPure(I));
+    if (HasSideEffects) {
+      revng_log(MarkLog, "Serialize Pending");
+      markSetToSerialize(Pending);
+      Pending = LatticeElement::top(); // empty set
+      markValueToSerialize(&I);
+    } else {
+      switch (I.getNumUses()) {
+      case 1: {
+        User *U = I.uses().begin()->getUser();
+        if (not isa<Instruction>(U)) {
+          Pending.insert(&I);
+          continue;
+        }
+        Instruction *UserI = cast<Instruction>(U);
+        if (isa<BranchInst>(UserI)) {
+          markValueToSerialize(&I);
+        } else if (NumDuplicatesOfBB != NDuplicates.at(UserI->getParent())) {
+          revng_assert(NumDuplicatesOfBB < NDuplicates.at(UserI->getParent()));
+          markValueToSerialize(&I);
+        } else {
+          Pending.insert(&I);
+        }
+
+      } break;
+      default:
+        revng_log(MarkLog, "Mark this to serialize");
+        markValueToSerialize(&I);
+        break;
+      }
+    }
+  }
+
+  return InterruptType::createInterrupt(std::move(Pending));
+}
+
+} // namespace MarkForSerialization
---
+++ lib/Decompiler/MarkForSerialization.h
@@ -0,0 +1,110 @@
+#ifndef REVNGC_MARKFORSERIALIZATION_H
+#define REVNGC_MARKFORSERIALIZATION_H
+//
+// This file is distributed under the MIT License. See LICENSE.md for details.
+//
+
+/// \brief Dataflow analysis to identify which Instructions must be serialized
+
+// std includes
+#include <set>
+
+// LLVM includes
+#include <llvm/IR/Function.h>
+
+class RegionCFG;
+
+namespace MarkForSerialization {
+
+using LatticeElement = IntersectionMonotoneSet<llvm::Instruction *>;
+
+class Analysis
+  : public MonotoneFramework<Analysis,
+                             llvm::BasicBlock *,
+                             LatticeElement,
+                             VisitType::ReversePostOrder,
+                             llvm::SmallVector<llvm::BasicBlock *, 2>> {
+protected:
+  llvm::Function &F;
+  RegionCFG &RCFG;
+  std::set<llvm::Instruction *> ToSerialize;
+  std::vector<std::set<llvm::Instruction *>> ToSerializeInBB;
+  std::map<llvm::BasicBlock *, size_t> BBToIdMap;
+  std::map<llvm::BasicBlock *, size_t> NDuplicates;
+
+public:
+  using Base = MonotoneFramework<Analysis,
+                                 llvm::BasicBlock *,
+                                 LatticeElement,
+                                 VisitType::ReversePostOrder,
+                                 llvm::SmallVector<llvm::BasicBlock *, 2>>;
+
+  void assertLowerThanOrEqual(const LatticeElement &A,
+                              const LatticeElement &B) const {
+    revng_abort();
+  }
+
+  Analysis(llvm::Function &F, RegionCFG &RCFG) :
+    Base(&F.getEntryBlock()),
+    F(F),
+    RCFG(RCFG),
+    ToSerialize(),
+    ToSerializeInBB(),
+    BBToIdMap(),
+    NDuplicates() {
+      Base::registerExtremal(&F.getEntryBlock());
+  }
+
+  void dumpFinalState() const { revng_abort(); }
+
+  llvm::SmallVector<llvm::BasicBlock *, 2>
+  successors(llvm::BasicBlock *BB, InterruptType &) const {
+    llvm::SmallVector<llvm::BasicBlock *, 2> Result;
+    for (llvm::BasicBlock *Successor : make_range(succ_begin(BB), succ_end(BB)))
+      Result.push_back(Successor);
+    return Result;
+  }
+
+  llvm::Optional<LatticeElement>
+  handleEdge(const LatticeElement & /*Original*/,
+             llvm::BasicBlock * /*Source*/,
+             llvm::BasicBlock * /*Destination*/) const {
+    return llvm::Optional<LatticeElement>();
+  }
+
+  size_t successor_size(llvm::BasicBlock *BB, InterruptType &) const {
+    return succ_end(BB) - succ_begin(BB);
+  }
+
+  static LatticeElement extremalValue(llvm::BasicBlock *) {
+    return LatticeElement::top();
+  }
+
+  InterruptType transfer(llvm::BasicBlock *);
+
+  void initialize();
+
+  const std::set<llvm::Instruction *> &getToSerialize() const {
+    return ToSerialize;
+  }
+
+  const std::set<llvm::Instruction *> &
+  getToSerialize(llvm::BasicBlock *B) const {
+    return ToSerializeInBB.at(BBToIdMap.at(B));
+  }
+
+protected:
+  void markValueToSerialize(llvm::Instruction *I) {
+    ToSerialize.insert(I);
+    ToSerializeInBB.at(BBToIdMap.at(I->getParent())).insert(I);
+  }
+
+  void markSetToSerialize(const LatticeElement &S) {
+    for (llvm::Instruction *I : S)
+      markValueToSerialize(I);
+  }
+};
+
+} // end namespace MarkForSerialization
+
+#endif /* ifndef REVNGC_MARKFORSERIALIZATION_H */
