656cffaac5f42aa06d6394008ced61e64f31dc6f
Andrea Gussoni <andrealinux1@gmail.com>
2019-04-02 14:48:35 +0200

    `Boost` test infrastructure
    Add the skeleton for the `boost` testing infrastrucure.

---
+++ tests/Unit/UnitTests.cmake
@@ -0,0 +1,9 @@
+#
+# This file is distributed under the MIT License. See LICENSE.md for details.
+#
+
+cmake_policy(SET CMP0060 NEW)
+
+set(SRC "${CMAKE_SOURCE_DIR}/tests/Unit")
+
+set(Boost_ADDITIONAL_VERSIONS "1.63" "1.63.0")
