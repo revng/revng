9e52b0c47330900600de727e7f6094c3a278a059
Alessandro Di Federico <ale@rev.ng>
2019-09-20 09:17:41 +0200

    Switch ARM tests to Thumb

--- tests/Tests.cmake
+++ tests/Tests.cmake
@@ -8,3 +8,5 @@ enable_testing()
 include(${CMAKE_SOURCE_DIR}/tests/runtime/RuntimeTests.cmake)
 include(${CMAKE_SOURCE_DIR}/tests/analysis/AnalysisTests.cmake)
 include(${CMAKE_SOURCE_DIR}/tests/unit/UnitTests.cmake)
+
+    set(TEST_CFLAGS_${ARCH} "${TEST_CFLAGS_${ARCH}} -mthumb")
