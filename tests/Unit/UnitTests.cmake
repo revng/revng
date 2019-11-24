ed00c19a1282d8eaa53bced8cb0e8f4caca7ca11
Alessandro Di Federico <ale@clearmind.me>
2019-11-24 22:48:23 +0100

    Switch to the Boost CMake package

--- tests/Unit/UnitTests.cmake
+++ tests/Unit/UnitTests.cmake
@@ -6,8 +6,7 @@ cmake_policy(SET CMP0060 NEW)
 
 set(SRC "${CMAKE_SOURCE_DIR}/tests/Unit")
 
-set(Boost_ADDITIONAL_VERSIONS "1.63" "1.63.0")
-find_package(Boost 1.63.0 REQUIRED COMPONENTS unit_test_framework)
+find_package(Boost REQUIRED COMPONENTS unit_test_framework)
 
 #
 # test_lazysmallbitvector
@@ -15,14 +14,11 @@ find_package(Boost 1.63.0 REQUIRED COMPONENTS unit_test_framework)
 
 add_executable(test_lazysmallbitvector "${SRC}/LazySmallBitVector.cpp")
 target_include_directories(test_lazysmallbitvector
-  PRIVATE "${CMAKE_SOURCE_DIR}"
-          "${Boost_INCLUDE_DIRS}")
-target_compile_definitions(test_lazysmallbitvector
-  PRIVATE "BOOST_TEST_DYN_LINK=1")
+  PRIVATE "${CMAKE_SOURCE_DIR}")
 target_link_libraries(test_lazysmallbitvector
   revngSupport
   revngUnitTestHelpers
-  ${Boost_UNIT_TEST_FRAMEWORK_LIBRARY}
+  Boost::unit_test_framework
   ${LLVM_LIBRARIES})
 add_test(NAME test_lazysmallbitvector COMMAND test_lazysmallbitvector)
 set_tests_properties(test_lazysmallbitvector PROPERTIES LABELS "unit")
@@ -35,15 +31,12 @@ add_executable(test_stackanalysis "${SRC}/StackAnalysis.cpp")
 target_include_directories(test_stackanalysis
   PRIVATE "${CMAKE_SOURCE_DIR}"
           "${CMAKE_SOURCE_DIR}/lib/StackAnalysis"
-          "${CMAKE_BINARY_DIR}/lib/StackAnalysis"
-          "${Boost_INCLUDE_DIRS}")
-target_compile_definitions(test_stackanalysis
-  PRIVATE "BOOST_TEST_DYN_LINK=1")
+          "${CMAKE_BINARY_DIR}/lib/StackAnalysis")
 target_link_libraries(test_stackanalysis
   revngStackAnalysis
   revngSupport
   revngUnitTestHelpers
-  ${Boost_UNIT_TEST_FRAMEWORK_LIBRARY}
+  Boost::unit_test_framework
   ${LLVM_LIBRARIES})
 add_test(NAME test_stackanalysis COMMAND test_stackanalysis)
 set_tests_properties(test_stackanalysis PROPERTIES LABELS "unit")
@@ -54,14 +47,11 @@ set_tests_properties(test_stackanalysis PROPERTIES LABELS "unit")
 
 add_executable(test_classsentinel "${SRC}/ClassSentinel.cpp")
 target_include_directories(test_classsentinel
-  PRIVATE "${CMAKE_SOURCE_DIR}"
-          "${Boost_INCLUDE_DIRS}")
-target_compile_definitions(test_classsentinel
-  PRIVATE "BOOST_TEST_DYN_LINK=1")
+  PRIVATE "${CMAKE_SOURCE_DIR}")
 target_link_libraries(test_classsentinel
   revngSupport
   revngUnitTestHelpers
-  ${Boost_UNIT_TEST_FRAMEWORK_LIBRARY}
+  Boost::unit_test_framework
   ${LLVM_LIBRARIES})
 add_test(NAME test_classsentinel COMMAND test_classsentinel)
 set_tests_properties(test_classsentinel PROPERTIES LABELS "unit")
@@ -72,14 +62,11 @@ set_tests_properties(test_classsentinel PROPERTIES LABELS "unit")
 
 add_executable(test_irhelpers "${SRC}/IRHelpers.cpp")
 target_include_directories(test_irhelpers
-  PRIVATE "${CMAKE_SOURCE_DIR}"
-          "${Boost_INCLUDE_DIRS}")
-target_compile_definitions(test_irhelpers
-  PRIVATE "BOOST_TEST_DYN_LINK=1")
+  PRIVATE "${CMAKE_SOURCE_DIR}")
 target_link_libraries(test_irhelpers
   revngSupport
   revngUnitTestHelpers
-  ${Boost_UNIT_TEST_FRAMEWORK_LIBRARY}
+  Boost::unit_test_framework
   ${LLVM_LIBRARIES})
 add_test(NAME test_irhelpers COMMAND test_irhelpers)
 set_tests_properties(test_irhelpers PROPERTIES LABELS "unit")
@@ -90,14 +77,11 @@ set_tests_properties(test_irhelpers PROPERTIES LABELS "unit")
 
 add_executable(test_advancedvalueinfo "${SRC}/AdvancedValueInfo.cpp")
 target_include_directories(test_advancedvalueinfo
-  PRIVATE "${CMAKE_SOURCE_DIR}"
-          "${Boost_INCLUDE_DIRS}")
-target_compile_definitions(test_advancedvalueinfo
-  PRIVATE "BOOST_TEST_DYN_LINK=1")
+  PRIVATE "${CMAKE_SOURCE_DIR}")
 target_link_libraries(test_advancedvalueinfo
   revngSupport
   revngBasicAnalyses
-  ${Boost_UNIT_TEST_FRAMEWORK_LIBRARY}
+  Boost::unit_test_framework
   ${LLVM_LIBRARIES})
 add_test(NAME test_advancedvalueinfo COMMAND test_advancedvalueinfo)
 set_tests_properties(test_advancedvalueinfo PROPERTIES LABELS "unit")
@@ -108,14 +92,11 @@ set_tests_properties(test_advancedvalueinfo PROPERTIES LABELS "unit")
 
 add_executable(test_zipmapiterator "${SRC}/ZipMapIterator.cpp")
 target_include_directories(test_zipmapiterator
-  PRIVATE "${CMAKE_SOURCE_DIR}"
-          "${Boost_INCLUDE_DIRS}")
-target_compile_definitions(test_zipmapiterator
-  PRIVATE "BOOST_TEST_DYN_LINK=1")
+  PRIVATE "${CMAKE_SOURCE_DIR}")
 target_link_libraries(test_zipmapiterator
   revngSupport
   revngUnitTestHelpers
-  ${Boost_UNIT_TEST_FRAMEWORK_LIBRARY}
+  Boost::unit_test_framework
   ${LLVM_LIBRARIES})
 add_test(NAME test_zipmapiterator COMMAND test_zipmapiterator)
 set_tests_properties(test_zipmapiterator PROPERTIES LABELS "unit")
@@ -126,14 +107,11 @@ set_tests_properties(test_zipmapiterator PROPERTIES LABELS "unit")
 
 add_executable(test_constantrangeset "${SRC}/ConstantRangeSet.cpp")
 target_include_directories(test_constantrangeset
-  PRIVATE "${CMAKE_SOURCE_DIR}"
-          "${Boost_INCLUDE_DIRS}")
-target_compile_definitions(test_constantrangeset
-  PRIVATE "BOOST_TEST_DYN_LINK=1")
+  PRIVATE "${CMAKE_SOURCE_DIR}")
 target_link_libraries(test_constantrangeset
   revngSupport
   revngUnitTestHelpers
-  ${Boost_UNIT_TEST_FRAMEWORK_LIBRARY}
+  Boost::unit_test_framework
   ${LLVM_LIBRARIES})
 add_test(NAME test_constantrangeset COMMAND test_constantrangeset)
 set_tests_properties(test_constantrangeset PROPERTIES LABELS "unit")
@@ -145,14 +123,11 @@ set_tests_properties(test_constantrangeset PROPERTIES LABELS "unit")
 
 add_executable(test_shrinkinstructionoperands "${SRC}/ShrinkInstructionOperandsPass.cpp")
 target_include_directories(test_shrinkinstructionoperands
-  PRIVATE "${CMAKE_SOURCE_DIR}"
-          "${Boost_INCLUDE_DIRS}")
-target_compile_definitions(test_shrinkinstructionoperands
-  PRIVATE "BOOST_TEST_DYN_LINK=1")
+  PRIVATE "${CMAKE_SOURCE_DIR}")
 target_link_libraries(test_shrinkinstructionoperands
   revngSupport
   revngUnitTestHelpers
-  ${Boost_UNIT_TEST_FRAMEWORK_LIBRARY}
+  Boost::unit_test_framework
   ${LLVM_LIBRARIES})
 add_test(NAME test_shrinkinstructionoperands COMMAND test_shrinkinstructionoperands)
 set_tests_properties(test_shrinkinstructionoperands PROPERTIES LABELS "unit")
