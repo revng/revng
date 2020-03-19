93d409558491f53d5216f55129f0b4fb673f9f1f
Andrea Gussoni <andreagus@rev.ng>
2020-03-19 11:12:59 +0100

    Add new dispatcher node
    Added a new constructor for the dispatcher nodes which will be emitted
    as `switch` nodes by the decompiler.
    These nodes are introduced during the restructuring phase as loop
    dispatchers.

--- include/revng-c/RestructureCFGPass/RegionCFGTree.h
+++ include/revng-c/RestructureCFGPass/RegionCFGTree.h
@@ -237,6 +237,20 @@ public:
     return Dispatcher;
   }
 
+  BBNodeT *addWeavingSwitch() {
+    using Type = typename BasicBlockNodeT::Type;
+    using Kind = typename BasicBlockNodeT::DispatcherKind;
+    using BBNodeT = BasicBlockNodeT;
+    std::string Name = "weaving switch";
+    auto Tmp = std::make_unique<BBNodeT>(this,
+                                         Name,
+                                         Type::Dispatcher,
+                                         Kind::Entry);
+    BlockNodes.emplace_back(std::move(Tmp));
+    BBNodeT *Dispatcher = BlockNodes.back().get();
+    return Dispatcher;
+  }
+
   BBNodeT *addSetStateNode(unsigned long StateVariableValue,
                            llvm::StringRef TargetName) {
     using Type = typename BasicBlockNodeT::Type;
