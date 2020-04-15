89258b832d152995f1f028678ea9f22db751355c
Alessandro Di Federico <ale@rev.ng>
2020-04-15 11:47:01 +0200

    Add ARM's `PCMContextIndex`
    `PCMContextIndex` is the index of the PC register within the `mcontex_t`
    `struct`.

--- tools/revng-lift/BinaryFile.cpp
+++ tools/revng-lift/BinaryFile.cpp
@@ -352,6 +352,7 @@ BinaryFile::BinaryFile(std::string FilePath, MetaAddress BaseAddress) :
     ABIRegisters = { { "r0" },  { "r1" },  { "r2" },  { "r3" },  { "r4" },
                      { "r5" },  { "r6" },  { "r7" },  { "r8" },  { "r9" },
                      { "r10" }, { "r11" }, { "r12" }, { "r13" }, { "r14" } };
+    PCMContextIndex = 18;
 
     HasRelocationAddend = false;
 
