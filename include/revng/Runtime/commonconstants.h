f7cb681b2307ef5adbcbc9daa094e1f8a59df7a5
Alessandro Di Federico <ale@rev.ng>
2020-11-01 23:17:49 +0100

    Whitespace changes

--- include/revng/Runtime/commonconstants.h
+++ include/revng/Runtime/commonconstants.h
@@ -8,15 +8,16 @@
 /// \brief Type of generated message for debugging exceptions at runtime when
 ///        function isolation is applied
 typedef enum {
-  StandardTranslatedBlock, ///< Unexpected control flow at the end
-                           ///  of a translated basic block
-  StandardNonTranslatedBlock, ///< Unexpected control flow at the
-                              ///  end of a non-translated basic
-                              ///  block (anypc or unexpectedpc)
-  BadReturnAddress, ///< Expected and actual return address after function call
-                    ///  not matching
-  FunctionDispatcherFallBack ///< Call to the function dispatcher with a PC not
-                             ///  corresponding to any function entry block
+  /// Unexpected control flow at the end of a translated basic block
+  StandardTranslatedBlock,
+  /// Unexpected control flow at the end of a non-translated basic block (anypc
+  /// or unexpectedpc)
+  StandardNonTranslatedBlock,
+  /// Expected and actual return address after function call not matching
+  BadReturnAddress,
+  /// Call to the function dispatcher with a PC not corresponding to any
+  /// function entry block
+  FunctionDispatcherFallBack
 } Reason;
 
 #endif // COMMONCONSTANTS_H
