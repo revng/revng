5de14227cfef9e6bd91a210e1dbf7a42f3665f91
Pietro Fezzardi <pietro@rev.ng>
2020-11-06 14:57:33 +0100

    Improve generation of valid C identifiers
    This commit does not implement real mangling yet, but it should at least
    get most low-hanging fruits, to prevent generation of invalid C
    identifiers.

--- lib/Decompiler/Mangling.h
+++ lib/Decompiler/Mangling.h
@@ -1,13 +1,25 @@
 #ifndef REVNGC_MANGLING_H
 #define REVNGC_MANGLING_H
 
+//
+// Copyright (c) rev.ng Srls 2017-2020.
+//
+
+// Standard includes
+#include <cctype>
+
+// LLVM includes
+#include <llvm/ADT/STLExtras.h>
+
 // This is very simple for now.
 // In the future we might consider making it more robust using something like
 // Punycode https://tools.ietf.org/html/rfc3492 , which also has the nice
 // property of being deterministically reversible.
 inline std::string makeCIdentifier(std::string S) {
-  std::replace(S.begin(), S.end(), '.', '_');
-  std::replace(S.begin(), S.end(), ':', '_');
+  llvm::for_each(S, [](char &C) {
+    if (not std::isalnum(C))
+      C = '_';
+  });
   return S;
 }
 
