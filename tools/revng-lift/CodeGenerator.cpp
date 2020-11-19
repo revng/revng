94d41336ee5b9b4026155c4d9dba02580f07f223
Antonio Frighetto <me@antoniofrighetto.com>
2020-11-19 11:23:38 +0100

    CodeGen: remove variadic args in calls to `newpc`
    After the discovery of new code to be translated, the calls to `newpc`
    can be stripped of their variadic arguments (reference to local vars)
    in order to let SROA perform more optimization.

--- tools/revng-lift/CodeGenerator.cpp
+++ tools/revng-lift/CodeGenerator.cpp
@@ -1268,6 +1268,8 @@ void CodeGenerator::translate(Optional<uint64_t> RawVirtualAddress) {
 
   Variables.setDataLayout(&TheModule->getDataLayout());
 
+  Translator.finalizeNewPCMarkers(CoveragePath);
+
   // SROA must run before InstCombine because in this way InstCombine has many
   // more elementary operations to combine
   legacy::PassManager PreInstCombinePM;
@@ -1309,8 +1311,6 @@ void CodeGenerator::translate(Optional<uint64_t> RawVirtualAddress) {
                                       *MainFunction,
                                       PCH.get());
   JumpOutHandler.createExternalJumpsHandler();
-
-  Translator.finalizeNewPCMarkers(CoveragePath);
 
   Variables.finalize();
 
