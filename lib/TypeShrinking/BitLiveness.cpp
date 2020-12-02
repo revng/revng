923debe1a60afc3b0087ab3659080c94e4d4ce9f
Qian Matteo Chen <chq.matteo@gmail.com>
2020-12-02 08:12:20 -0800

    [TypeShrinking] Use std::min and std::max

--- lib/TypeShrinking/BitLiveness.cpp
+++ lib/TypeShrinking/BitLiveness.cpp
@@ -35,20 +35,12 @@ bool isDataFlowSink(const Instruction *Ins) {
   return false;
 }
 
-uint32_t min(const uint32_t &a, const uint32_t &b) {
-  return a < b ? a : b;
-}
-
-uint32_t max(const uint32_t &a, const uint32_t &b) {
-  return a > b ? a : b;
-}
-
 uint32_t getMaxOperandSize(Instruction *Ins) {
   uint32_t Max = 0;
   for (uint32_t i = 0; i < Ins->getNumOperands(); ++i) {
     auto *Operand = Ins->getOperand(i);
     if (Operand->getType()->isIntegerTy())
-      Max = max(Max, Operand->getType()->getIntegerBitWidth());
+      Max = std::max(Max, Operand->getType()->getIntegerBitWidth());
     else
       return Top;
   }
@@ -58,7 +50,7 @@ uint32_t getMaxOperandSize(Instruction *Ins) {
 /// A specialization of the transfer function for the and instruction
 /// In cases where one of the operands is constant
 uint32_t transferMask(const uint32_t &Element, const uint32_t &MaskIndex) {
-  return min(Element, MaskIndex);
+  return std::min(Element, MaskIndex);
 }
 
 uint32_t transferAnd(Instruction *Ins, const uint32_t &Element) {
@@ -68,7 +60,7 @@ uint32_t transferAnd(Instruction *Ins, const uint32_t &Element) {
     if (llvm::isa<llvm::ConstantInt>(Operand)) {
       auto OpVal = llvm::cast<llvm::ConstantInt>(Operand)->getUniqueInteger();
       auto MostSignificantBit = OpVal.getBitWidth() - OpVal.countLeadingZeros();
-      Result = min(Result, transferMask(Element, MostSignificantBit));
+      Result = std::min(Result, transferMask(Element, MostSignificantBit));
     }
   }
   return Result;
@@ -89,9 +81,10 @@ uint32_t transferLogicalShiftRight(Instruction *Ins, const uint32_t &Element) {
   uint32_t OperandSize = getMaxOperandSize(Ins);
   if (auto ConstOp = llvm::dyn_cast<llvm::ConstantInt>(Ins->getOperand(1))) {
     auto OpVal = ConstOp->getZExtValue();
+    revng_assert(OpVal < Top);
     if (Top - OpVal < Element)
       return Top;
-    return min(OperandSize, Element + OpVal);
+    return std::min(OperandSize, Element + (uint32_t) OpVal);
   }
   return OperandSize;
 }
@@ -101,19 +94,20 @@ transferArithmeticalShiftRight(Instruction *Ins, const uint32_t &Element) {
   uint32_t OperandSize = getMaxOperandSize(Ins);
   if (auto ConstOp = llvm::dyn_cast<llvm::ConstantInt>(Ins->getOperand(1))) {
     auto OpVal = ConstOp->getZExtValue();
+    revng_assert(OpVal < Top);
     if (Top - OpVal < Element)
       return Top;
-    return min(OperandSize, Element + OpVal);
+    return std::min(OperandSize, Element + (uint32_t) OpVal);
   }
   return OperandSize;
 }
 
 uint32_t transferTrunc(Instruction *Ins, const uint32_t &Element) {
-  return min(Element, Ins->getType()->getIntegerBitWidth());
+  return std::min(Element, Ins->getType()->getIntegerBitWidth());
 }
 
 uint32_t transferZExt(Instruction *Ins, const uint32_t &Element) {
-  return min(Element, getMaxOperandSize(Ins));
+  return std::min(Element, getMaxOperandSize(Ins));
 }
 
 uint32_t
@@ -127,7 +121,7 @@ BitLivenessAnalysis::applyTransferFunction(DataFlowNode *L, const uint32_t E) {
   case Instruction::Add:
   case Instruction::Sub:
   case Instruction::Mul:
-    return min(E, getMaxOperandSize(L->Instruction));
+    return std::min(E, getMaxOperandSize(L->Instruction));
   case Instruction::Shl:
     return transferShiftLeft(Ins, E);
   case Instruction::LShr:
@@ -146,7 +140,7 @@ BitLivenessAnalysis::applyTransferFunction(DataFlowNode *L, const uint32_t E) {
 
 uint32_t
 BitLivenessAnalysis::combineValues(const uint32_t &lh, const uint32_t &rh) {
-  return max(lh, rh);
+  return std::max(lh, rh);
 }
 
 bool BitLivenessAnalysis::isLessOrEqual(const uint32_t &lh,
