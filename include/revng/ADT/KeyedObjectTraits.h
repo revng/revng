073b752c426350e09086fb7df50bafb6637ff0f9
Alessandro Di Federico <ale@rev.ng>
2021-01-26 16:42:13 +0100

    Import MutableSet and SortedVector
    These data structures are substitutes for a `std::map<Key, Value>` where
    `Key` is embedded in `Value`. Their main goal is to be serializable in a
    YAML sequence while preserving the order enforced by the key.
    
    `MutableSet` is implemented using a map.
    `SortedVector` is implemented using a sorted vector.

---
+++ include/revng/ADT/KeyedObjectTraits.h
@@ -0,0 +1,26 @@
+#pragma once
+
+//
+// This file is distributed under the MIT License. See LICENSE.md for details.
+//
+
+#include "revng/ADT/STLExtras.h"
+
+template<typename T, typename = void>
+struct KeyedObjectTraits {
+  // static * key(const T &);
+  // static T fromKey(* Key);
+};
+
+/// Inherit if T is the key of itself
+template<typename T>
+struct IdentityKeyedObjectTraits {
+  static T key(const T &Obj) { return Obj; }
+
+  static T fromKey(T Obj) { return Obj; }
+};
+
+/// Trivial specialization for integral types
+template<typename T>
+struct KeyedObjectTraits<T, enable_if_is_integral_t<T>>
+  : public IdentityKeyedObjectTraits<T> {};
