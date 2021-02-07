c69a63d6898f31f7c388c70d545aba0f7a58167c
Alessandro Di Federico <ale@rev.ng>
2021-02-07 19:22:37 +0100

    Whitespace and other minor changes

--- lib/FunctionCallIdentification/PruneRetSuccessors.cpp
+++ lib/FunctionCallIdentification/PruneRetSuccessors.cpp
@@ -5,7 +5,6 @@
 // This file is distributed under the MIT License. See LICENSE.md for details.
 //
 
-// Local libraries includes
 #include "revng/FunctionCallIdentification/PruneRetSuccessors.h"
 #include "revng/Support/Debug.h"
 
