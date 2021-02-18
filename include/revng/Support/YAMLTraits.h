e9ca81247da48284b3a0465eb300f5c82686a4d7
Alessandro Di Federico <ale@rev.ng>
2021-02-18 18:11:49 +0100

    Improve `get*FromYAML*`
    This commit fixes the following issues:
    
    1. It introduces `getNameFromYAMLEnumScalar` which returns a
       `llvm::StringRef` instead of a `std::string`.
    2. Lets users decide what to do when `getValueFromYAMLScalar` does not
       get any match (abort by defaul, or return an "Invalid" value).

--- include/revng/Support/YAMLTraits.h
+++ include/revng/Support/YAMLTraits.h
@@ -15,38 +15,52 @@ using has_MappingTraits = has_MappingTraits<T, EmptyContext>;
 
 template<typename T, typename K = void>
 using ei_hmt = std::enable_if_t<has_MappingTraits<T>::value, K>;
+
 } // namespace detail
 
 template<typename T, typename K = void>
 using enable_if_has_MappingTraits = detail::ei_hmt<T, K>;
 
 template<typename T>
+inline llvm::StringRef getNameFromYAMLEnumScalar(T V) {
+  using namespace llvm::yaml;
+  static_assert(has_ScalarEnumerationTraits<T>::value);
+  struct GetScalarIO {
+    llvm::StringRef Result;
+    void enumCase(const T &V,
+                  llvm::StringRef Name,
+                  const T &M,
+                  llvm::yaml::QuotingType = llvm::yaml::QuotingType::None) {
+      if (V == M) {
+        Result = Name;
+      }
+    }
+  };
+  GetScalarIO ExtractName;
+  llvm::yaml::ScalarEnumerationTraits<T>::enumeration(ExtractName, V);
+  return ExtractName.Result;
+}
+
+template<typename T>
 inline std::string getNameFromYAMLScalar(T V) {
   using namespace llvm::yaml;
   static_assert(has_ScalarTraits<T>::value
                 or has_ScalarEnumerationTraits<T>::value);
-  std::string Buffer;
 
   if constexpr (has_ScalarTraits<T>::value) {
+    std::string Buffer;
     llvm::raw_string_ostream Stream(Buffer);
     llvm::yaml::ScalarTraits<T>::output(V, nullptr, Stream);
+    return Buffer;
   } else {
-    struct GetScalarIO {
-      llvm::StringRef Result;
-      void enumCase(const T &V,
-                    llvm::StringRef Name,
-                    const T &M,
-                    llvm::yaml::QuotingType = llvm::yaml::QuotingType::None) {
-        if (V == M) {
-          Result = Name;
-        }
-      }
-    };
-    GetScalarIO ExtractName;
-    llvm::yaml::ScalarEnumerationTraits<T>::enumeration(ExtractName, V);
+    return getNameFromYAMLEnumScalar(V);
   }
+}
 
-  return Buffer;
+template<typename T>
+T getInvalidValueFromYAMLScalar() {
+  // Default action: abort. Users can override this behavior.
+  revng_abort();
 }
 
 template<typename T>
@@ -61,17 +75,23 @@ inline T getValueFromYAMLScalar(llvm::StringRef Name) {
     llvm::yaml::ScalarTraits<T>::input(Name, nullptr, Result);
   } else {
     struct GetScalarIO {
+      bool Found = false;
       llvm::StringRef TargetName;
       void enumCase(T &V,
                     llvm::StringRef Name,
                     const T &M,
                     llvm::yaml::QuotingType = llvm::yaml::QuotingType::None) {
-        if (TargetName == Name)
+        if (TargetName == Name) {
+          revng_assert(not Found);
+          Found = true;
           V = M;
+        }
       }
     };
-    GetScalarIO ExtractValue{ Name };
+    GetScalarIO ExtractValue{ false, Name };
     llvm::yaml::ScalarEnumerationTraits<T>::enumeration(ExtractValue, Result);
+    if (not ExtractValue.Found)
+      Result = getInvalidValueFromYAMLScalar<T>();
   }
 
   return Result;
