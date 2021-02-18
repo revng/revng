5661c4645bc0328d89ed30e05688b1128335bc09
Qian Matteo Chen <chqma@rev.ng>
2021-02-18 04:19:51 -0800

    [NewMFP] Move RPOT to revng

---
+++ include/revng/ADT/ReversePostOrderTraversal.h
@@ -0,0 +1,39 @@
+#pragma once
+
+//
+// This file is distributed under the MIT License. See LICENSE.md for details.
+//
+
+#include "llvm/ADT/PostOrderIterator.h"
+
+namespace llvm {
+
+template <class GraphT, class GT = llvm::GraphTraits<GraphT>,
+          class SetType = std::set<typename llvm::GraphTraits<GraphT>::NodeRef>>
+class ReversePostOrderTraversalExt {
+  using NodeRef = typename GT::NodeRef;
+  using NodeVec = std::vector<NodeRef>;
+
+  NodeVec Blocks; // Block list in normal RPO order
+
+  void Initialize(GraphT G, SetType &WhiteList) {
+    std::copy(po_ext_begin(G, WhiteList), po_ext_end(G, WhiteList),
+              std::back_inserter(Blocks));
+  }
+
+public:
+  using rpo_iterator = typename NodeVec::reverse_iterator;
+  using const_rpo_iterator = typename NodeVec::const_reverse_iterator;
+
+  ReversePostOrderTraversalExt(GraphT G, SetType &WhiteList) {
+    Initialize(G, WhiteList);
+  }
+
+  // Because we want a reverse post order, use reverse iterators from the vector
+  rpo_iterator begin() { return Blocks.rbegin(); }
+  const_rpo_iterator begin() const { return Blocks.crbegin(); }
+  rpo_iterator end() { return Blocks.rend(); }
+  const_rpo_iterator end() const { return Blocks.crend(); }
+};
+
+} // namespace llvm
