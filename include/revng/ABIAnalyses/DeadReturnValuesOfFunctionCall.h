c049a4737b1d2cecddbd0c365913dd6d4d399739
Qian Matteo Chen <chqma@rev.ng>
2021-02-23 07:27:55 -0800

    [WIP]{NewMFPABI] Fix support for TheCall

--- include/revng/ABIAnalyses/DeadReturnValuesOfFunctionCall.h
+++ include/revng/ABIAnalyses/DeadReturnValuesOfFunctionCall.h
@@ -9,6 +9,7 @@
 #include "llvm/IR/Instruction.h"
 #include "llvm/IR/Value.h"
 
+#include "revng/ABIAnalyses/DeadReturnValuesOfFunctionCall.h"
 #include "revng/BasicAnalyses/GeneratedCodeBasicInfo.h"
 #include "revng/MFP/MFP.h"
 #include "revng/StackAnalysis/StackAnalysis.h"
@@ -18,23 +19,29 @@ namespace DeadReturnValuesOfFunctionCall {
 
 enum State { NoOrDead, Maybe, Unknown };
 enum TransferKind { Read, Write, WeakWrite, TheCall, None };
+
+// special register index, if found inside LatticeElement
+// it means that it is the first time visiting the BasicBlock
+const int32_t InitialRegisterState = -1;
 struct MFI {
   using LatticeElement = llvm::DenseMap<int32_t, State>;
   using Label = const llvm::BasicBlock *;
-  using GraphType = const llvm::Function *;
+  using GraphType = const llvm::BasicBlock *;
 
-  MFI(const llvm::DenseMap<llvm::GlobalVariable *, int32_t> &R,
-      StackAnalysis::FunctionProperties P) :
-    Properties(P), ABIRegisters(R){};
+  MFI(const llvm::Instruction *CS,
+      const llvm::DenseMap<llvm::GlobalVariable *, int32_t> &R,
+      const StackAnalysis::FunctionProperties &P) :
+    CallSite(CS), Properties(P), ABIRegisters(R){};
 
-  StackAnalysis::FunctionProperties Properties;
-  llvm::DenseMap<llvm::GlobalVariable *, int32_t> ABIRegisters;
+  const llvm::Instruction *CallSite;
+  const StackAnalysis::FunctionProperties &Properties;
+  const llvm::DenseMap<llvm::GlobalVariable *, int32_t> &ABIRegisters;
   bool isABIRegister(const llvm::Value *) const;
-  TransferKind classifyInstruction(const llvm::Instruction &) const;
+  TransferKind classifyInstruction(const llvm::Instruction *) const;
   llvm::SmallVector<int32_t, 1>
-  getRegistersWritten(const llvm::Instruction &) const;
+  getRegistersWritten(const llvm::Instruction *) const;
   llvm::SmallVector<int32_t, 1>
-  getRegistersRead(const llvm::Instruction &) const;
+  getRegistersRead(const llvm::Instruction *) const;
 
   LatticeElement
   combineValues(const LatticeElement &Lh, const LatticeElement &Rh) const;
@@ -42,6 +49,11 @@ struct MFI {
   bool isLessOrEqual(const LatticeElement &Lh, const LatticeElement &Rh) const;
 };
 
-inline std::map<MFI::Label, MFP::MFPResult<MFI::LatticeElement>>
-analyze(llvm::Function *Entry, const GeneratedCodeBasicInfo &GCBI);
+llvm::DenseMap<llvm::GlobalVariable *, State>
+analyze(const llvm::Instruction *CallSite,
+        llvm::Function *Entry,
+        const GeneratedCodeBasicInfo &GCBI,
+        const StackAnalysis::FunctionProperties &FP);
 } // namespace DeadReturnValuesOfFunctionCall
+
+namespace llvm {}; // namespace llvm
