1ebef0d55f9dfe6848125386f0492d3a6c6ad98e
Qian Matteo Chen <chqma@rev.ng>
2021-03-10 08:03:35 -0800

    [NewMFPABI] Convert abi test pass to new PM

--- lib/ABIAnalyses/ABIAnalysisPass.cpp
+++ lib/ABIAnalyses/ABIAnalysisPass.cpp
@@ -5,11 +5,14 @@
 #include "llvm/IR/Function.h"
 #include "llvm/IR/Instruction.h"
 #include "llvm/IR/Instructions.h"
+#include "llvm/IR/PassManager.h"
 #include "llvm/Support/CommandLine.h"
 
 #include "revng/ABIAnalyses/ABIAnalysis.h"
 #include "revng/ABIAnalyses/ABIAnalysisPass.h"
+#include "revng/BasicAnalyses/GeneratedCodeBasicInfo.h"
 
+using namespace ABIAnalyses;
 using namespace llvm;
 
 static cl::OptionCategory Category("new-abi-analysis");
@@ -20,12 +23,12 @@ static cl::opt<std::string> FilterFunction("new-abi-analysis-function",
                                            cl::value_desc("new-abi-analysis-"
                                                           "function"),
                                            cl::cat(Category));
-char ABIAnalysisPass::ID = 0;
+char ABIAnalysisWrapperPass::ID = 0;
 
-static RegisterPass<ABIAnalysisPass>
+static RegisterPass<ABIAnalysisWrapperPass>
   X("new-abi-analysis", "abianalysis Functions Pass", true, true);
 
-bool ABIAnalysisPass::runOnFunction(Function &F) {
+bool ABIAnalysisWrapperPass::runOnFunction(Function &F) {
   // Retrieve analysis of the GeneratedCodeBasicInfo pass
   auto &GCBI = getAnalysis<GeneratedCodeBasicInfoWrapperPass>().getGCBI();
   if (F.getName() != FilterFunction) {
@@ -33,4 +36,15 @@ bool ABIAnalysisPass::runOnFunction(Function &F) {
   }
   ABIAnalyses::analyzeOutlinedFunction(&F, GCBI);
   return false;
+}
+
+
+PreservedAnalyses
+ABIAnalysisPass::run(Function &F, FunctionAnalysisManager &FAM) {
+  const auto &GCBI = FAM.getResult<GeneratedCodeBasicInfoAnalysis>(F);
+  if (F.getName() != FilterFunction) {
+    return PreservedAnalyses::all();
+  }
+  ABIAnalyses::analyzeOutlinedFunction(&F, GCBI);
+  return PreservedAnalyses::all();
 }
