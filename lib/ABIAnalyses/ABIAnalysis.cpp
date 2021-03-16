2db8fbd99eb99fbcc936b1e3778d50f2309d8a3a
Qian Matteo Chen <chqma@rev.ng>
2021-03-16 18:13:31 +0100

    [NewMFPABI] Use acronyms for analysis names

--- lib/ABIAnalyses/ABIAnalysis.cpp
+++ lib/ABIAnalyses/ABIAnalysis.cpp
@@ -28,25 +28,20 @@ AnalysisResults
 analyzeOutlinedFunction(Function *F, const GeneratedCodeBasicInfo &GCBI) {
   AnalysisResults Results;
 
-  Results.UsedArgumentsOfFunction = UsedArgumentsOfFunction::
-    analyze(&F->getEntryBlock(), GCBI);
-  Results.DeadRegisterArgumentsOfFunction = DeadRegisterArgumentsOfFunction::
-    analyze(&F->getEntryBlock(), GCBI);
+  Results.UAOF = UsedArgumentsOfFunction::analyze(&F->getEntryBlock(), GCBI);
+  Results.DRAOF = DeadRegisterArgumentsOfFunction::analyze(&F->getEntryBlock(),
+                                                           GCBI);
   for (Instruction &I : llvm::instructions(F)) {
     BasicBlock *BB = I.getParent();
     if (auto *C = dyn_cast<CallInst>(&I)) {
       if (C->getCalledFunction()->getName().contains("precall_hook")) {
         // BB is definitely a call site and also a special basic block
-        Results.UsedReturnValuesOfFunctionCall
-          [BB] = UsedReturnValuesOfFunctionCall::analyze(BB, GCBI);
-        Results.RegisterArgumentsOfFunctionCall
-          [BB] = RegisterArgumentsOfFunctionCall::analyze(BB, GCBI);
-        Results.DeadReturnValuesOfFunctionCall
-          [BB] = DeadReturnValuesOfFunctionCall::analyze(BB, GCBI);
+        Results.URVOFC[BB] = UsedReturnValuesOfFunctionCall::analyze(BB, GCBI);
+        Results.RAOFC[BB] = RegisterArgumentsOfFunctionCall::analyze(BB, GCBI);
+        Results.DRVOFC[BB] = DeadReturnValuesOfFunctionCall::analyze(BB, GCBI);
       }
     } else if (auto *R = dyn_cast<ReturnInst>(&I)) {
-      Results.UsedReturnValuesOfFunction[BB] = UsedReturnValuesOfFunction::
-        analyze(BB, GCBI);
+      Results.URVOF[BB] = UsedReturnValuesOfFunction::analyze(BB, GCBI);
     }
   }
 
