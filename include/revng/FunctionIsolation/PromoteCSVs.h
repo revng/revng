53f6328507dba2ffcff697fc7bd08b7c50c25f2d
Alessandro Di Federico <ale@rev.ng>
2021-04-11 18:36:41 +0200

    Introduce PromoteCSVs
    This commit takes out of EnforceABI the part taking care of creating
    wrappers for calls to helpers and promoting CSV to local variables.
    This decoupling, enables to run -promote-csvs multiple times, for
    instance after inlining.

---
+++ include/revng/FunctionIsolation/PromoteCSVs.h
@@ -0,0 +1,23 @@
+#pragma once
+
+//
+// This file is distributed under the MIT License. See LICENSE.md for details.
+//
+
+#include "llvm/Pass.h"
+
+#include "revng/BasicAnalyses/GeneratedCodeBasicInfo.h"
+
+class PromoteCSVsPass : public llvm::ModulePass {
+public:
+  static char ID;
+
+public:
+  PromoteCSVsPass() : ModulePass(ID) {}
+
+  bool runOnModule(llvm::Module &M) override;
+
+  void getAnalysisUsage(llvm::AnalysisUsage &AU) const override {
+    AU.addRequired<GeneratedCodeBasicInfoWrapperPass>();
+  }
+};
