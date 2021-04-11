0c671ea7ae52ed1f5e8efba7eb26ca3f749db896
Alessandro Di Federico <ale@rev.ng>
2021-04-11 15:39:27 +0200

    Introduce RemoveExceptionalCalls
    RemoveExceptionalCalls is a simple pass whose goal is to drop all the
    calls to functions marked as `Exceptional` and replace them with an
    `UnrechableInst`.
    
    This is mainly useful in the decompilation pipeline.

---
+++ include/revng/FunctionIsolation/RemoveExceptionalCalls.h
@@ -0,0 +1,21 @@
+#pragma once
+
+//
+// This file is distributed under the MIT License. See LICENSE.md for details.
+//
+
+#include "llvm/Pass.h"
+
+class RemoveExceptionalCalls : public llvm::ModulePass {
+public:
+  static char ID;
+
+public:
+  RemoveExceptionalCalls() : llvm::ModulePass(ID) {}
+
+  void getAnalysisUsage(llvm::AnalysisUsage &AU) const override {
+    AU.setPreservesAll();
+  }
+
+  bool runOnModule(llvm::Module &F) override;
+};
