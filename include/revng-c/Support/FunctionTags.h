58f114f19f7d8376f8b3d6e9370ec3a0feeee992
Alessandro Di Federico <ale@rev.ng>
2021-11-26 16:36:52 +0100

    Introduce libSupport

---
+++ include/revng-c/Support/FunctionTags.h
@@ -0,0 +1,10 @@
+#pragma once
+
+//
+// Copyright (c) rev.ng Srls. See LICENSE.md for details.
+//
+
+#include "revng/Support/FunctionTags.h"
+
+namespace FunctionTags {
+} // namespace FunctionTags
---
+++ lib/Support/FunctionTags.cpp
@@ -0,0 +1,8 @@
+//
+// Copyright (c) rev.ng Srls. See LICENSE.md for details.
+//
+
+#include "revng-c/Support/FunctionTags.h"
+
+namespace FunctionTags {
+} // namespace FunctionTags
