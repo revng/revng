5a3a9034a1789224e433b1ae032e2be645bc78d2
Massimo Fioravanti <massimofioravanti@rev.ng>
2021-12-23 16:06:25 +0100

    Introduce revng-pipeline

---
+++ include/revng/Pipeline/GenericLLVMPipe.h
@@ -0,0 +1,174 @@
+#pragma once
+
+//
+// This file is distributed under the MIT License. See LICENSE.md for details.
+//
+
+#include <array>
+#include <memory>
+#include <system_error>
+#include <utility>
+#include <vector>
+
+#include "llvm/IR/LegacyPassManager.h"
+
+#include "revng/Pipeline/ContainerFactorySet.h"
+#include "revng/Pipeline/Contract.h"
+#include "revng/Pipeline/LLVMContainer.h"
+#include "revng/Pipeline/Step.h"
+#include "revng/Support/Concepts.h"
+#include "revng/Support/Debug.h"
+
+namespace pipeline {
+
+class LLVMPassWrapperBase {
+public:
+  virtual ~LLVMPassWrapperBase() = default;
+  virtual void registerPasses(llvm::legacy::PassManager &Manager) = 0;
+  virtual const std::vector<ContractGroup> &getContract() const = 0;
+  virtual std::unique_ptr<LLVMPassWrapperBase> clone() const = 0;
+  virtual llvm::StringRef getName() const = 0;
+};
+
+template<typename T>
+concept LLVMPass = requires(T a) {
+  { T::Name } -> convertible_to<const char *>;
+  { a.registerPasses(std::declval<llvm::legacy::PassManager &>()) };
+};
+
+/// LLVM pipes are pipes composed of any number of llvm passes
+///
+/// The contract is deduced by the used passes, and the passes are assembled
+/// into pass managers when needed.
+template<LLVMPass T>
+class LLVMPassWrapper : public LLVMPassWrapperBase {
+public:
+  using RegistrationFunctionType = void (*)(llvm::legacy::PassManager &);
+
+private:
+  T PipePass;
+  std::vector<ContractGroup> Contract;
+
+public:
+  LLVMPassWrapper(T Pass) :
+    PipePass(std::move(Pass)), Contract(this->PipePass.getContract()) {}
+
+  LLVMPassWrapper(const LLVMPassWrapper &Other) = default;
+  LLVMPassWrapper(LLVMPassWrapper &&Other) = default;
+
+  template<typename... ArgsT>
+  LLVMPassWrapper(ArgsT &&...Args) :
+    PipePass(std::forward<ArgsT>(Args)...),
+    Contract(this->PipePass.getContract()) {}
+
+  ~LLVMPassWrapper() override = default;
+
+public:
+  llvm::StringRef getName() const override { return T ::Name; }
+
+public:
+  void registerPasses(llvm::legacy::PassManager &Manager) override {
+    PipePass.registerPasses(Manager);
+  }
+
+  const std::vector<ContractGroup> &getContract() const override {
+    return Contract;
+  }
+
+  std::unique_ptr<LLVMPassWrapperBase> clone() const override {
+    return std::make_unique<LLVMPassWrapper>(*this);
+  }
+};
+
+/// Implementation of the LLVM pipes to be instantiated for a particular LLVM
+/// container
+template<typename LLVMContainer>
+class GenericLLVMPipe {
+private:
+  llvm::SmallVector<std::unique_ptr<LLVMPassWrapperBase>, 4> Passes;
+
+public:
+  static constexpr auto Name = "GenericLLVMPipe";
+  template<LLVMPass... T>
+  explicit GenericLLVMPipe(T... Pass) {
+    (addPass<T>(std::move(Pass)), ...);
+  }
+
+  GenericLLVMPipe &operator=(const GenericLLVMPipe &Other) {
+    if (this == &Other)
+      return *this;
+
+    llvm::SmallVector<std::unique_ptr<LLVMPassWrapperBase>, 4> NewPasses;
+
+    for (const auto &P : Other.Passes)
+      NewPasses.push_back(P->clone());
+
+    Passes = std::move(NewPasses);
+    return *this;
+  }
+
+  GenericLLVMPipe(const GenericLLVMPipe &Other) {
+    for (const auto &P : Other.Passes)
+      Passes.push_back(P->clone());
+  }
+  GenericLLVMPipe &operator=(GenericLLVMPipe &&Other) = default;
+  GenericLLVMPipe(GenericLLVMPipe &&Other) = default;
+  ~GenericLLVMPipe() = default;
+
+  std::vector<ContractGroup> getContract() const {
+    std::vector<ContractGroup> Contract;
+    for (const auto &Element : Passes)
+      for (const auto &C : Element->getContract())
+        Contract.push_back(C);
+
+    return Contract;
+  }
+
+  void run(const Context &, LLVMContainer &Container) {
+    llvm::legacy::PassManager Manager;
+    for (const auto &Element : Passes)
+      Element->registerPasses(Manager);
+    Manager.run(Container.getModule());
+  }
+
+  template<LLVMPass T>
+  void addPass(T Pass) {
+    using Type = LLVMPassWrapper<T>;
+    auto Wrapper = std::make_unique<Type>(std::forward<T>(Pass));
+    Passes.emplace_back(std::move(Wrapper));
+  }
+
+  template<LLVMPass T, typename... ArgsT>
+  void emplacePass(ArgsT &&...Args) {
+    using Type = LLVMPassWrapper<T>;
+    auto Wrapper = std::make_unique<Type>(std::forward<ArgsT>(Args)...);
+    Passes.emplace_back(std::move(Wrapper));
+  }
+
+  void addPass(std::unique_ptr<LLVMPassWrapperBase> Impl) {
+    Passes.emplace_back(std::move(Impl));
+  }
+
+public:
+  template<typename OStream>
+  void dump(OStream &OS, size_t Indentation = 0) const {
+    for (const auto &Pass : Passes) {
+      indent(OS, Indentation);
+      OS << Pass->getName().str() << "\n";
+    }
+  }
+
+  void dump() const debug_function { dump(dbg); }
+};
+
+class O2Pipe {
+public:
+  static constexpr auto Name = "02 Pipe";
+  std::vector<ContractGroup> getContract() const { return {}; }
+
+  void registerPasses(llvm::legacy::PassManager &Manager);
+};
+
+using LLVMPipe = GenericLLVMPipe<LLVMContainer>;
+
+} // namespace pipeline
---
+++ include/revng/Pipeline/Runner.h
@@ -0,0 +1,220 @@
+#pragma once
+
+//
+// This file is distributed under the MIT License. See LICENSE.md for details.
+//
+
+#include <string>
+#include <utility>
+
+#include "llvm/ADT/SmallVector.h"
+#include "llvm/ADT/StringMap.h"
+#include "llvm/ADT/StringRef.h"
+#include "llvm/Support/Error.h"
+#include "llvm/Support/raw_ostream.h"
+
+#include "revng/Pipeline/ContainerFactorySet.h"
+#include "revng/Pipeline/Context.h"
+#include "revng/Pipeline/KindsRegistry.h"
+#include "revng/Pipeline/Step.h"
+#include "revng/Pipeline/Target.h"
+#include "revng/Support/Debug.h"
+
+namespace pipeline {
+
+/// A Runner is a wrapper around a pipeline structure and the context needed to
+/// run it.
+/// It is the top level object on which to invoke operations.
+class Runner {
+private:
+  using Map = llvm::StringMap<Step>;
+  using Vector = std::vector<Step *>;
+
+private:
+  Context *TheContext;
+  ContainerFactorySet ContainerFactoriesRegistry;
+  bool IsContainerFactoriesRegistryFinalized = false;
+
+  Map Steps;
+  Vector ReversePostOrderIndexes;
+
+public:
+  using iterator = revng::DereferenceIteratorType<Vector::iterator>;
+  using const_iterator = revng::DereferenceIteratorType<Vector::const_iterator>;
+
+  using State = llvm::StringMap<ContainerToTargetsMap>;
+  using InvalidationMap = llvm::StringMap<ContainerToTargetsMap>;
+
+public:
+  explicit Runner(Context &C) : TheContext(&C) {}
+
+public:
+  void getCurrentState(State &Out) const;
+
+public:
+  size_t registeredContainersCount() const {
+    return ContainerFactoriesRegistry.size();
+  }
+
+  const ContainerFactorySet &getContainerFactorySet() const {
+    return ContainerFactoriesRegistry;
+  }
+
+  const Context &getContext() const { return *TheContext; }
+
+  const KindsRegistry &getKindsRegistry() const {
+    return TheContext->getKindsRegistry();
+  }
+
+public:
+  Step &operator[](llvm::StringRef Name) { return getStep(Name); }
+  const Step &operator[](llvm::StringRef Name) const { return getStep(Name); }
+
+  Step &getStep(llvm::StringRef Name) {
+    revng_assert(Steps.find(Name) != Steps.end());
+    return Steps.find(Name)->second;
+  }
+
+  const Step &getStep(llvm::StringRef Name) const {
+    revng_assert(Steps.find(Name) != Steps.end());
+    return Steps.find(Name)->second;
+  }
+
+public:
+  /// Given a target, all occurrences of that target from every container in
+  /// every step will be registered in the returned invalidation map. The
+  /// propagations will not be calculated.
+  llvm::Error
+  getInvalidations(const Target &Target, InvalidationMap &Invalidations) const;
+
+  /// Deduces and register in the invalidation map all the targets that have
+  /// been produced starting from targets already presents in the map.
+  llvm::Error getInvalidations(InvalidationMap &Invalidated) const;
+
+public:
+  template<typename... PipeWrappers>
+  Step &emplaceStep(llvm::StringRef PreviousStepName,
+                    llvm::StringRef StepName,
+                    PipeWrappers &&...Wrappers) {
+    IsContainerFactoriesRegistryFinalized = true;
+    if (PreviousStepName.empty())
+      return addStep(Step(StepName.str(),
+                          ContainerFactoriesRegistry.createEmpty(),
+                          std::forward<PipeWrappers>(Wrappers)...));
+    else
+      return addStep(Step(StepName.str(),
+                          ContainerFactoriesRegistry.createEmpty(),
+                          operator[](PreviousStepName),
+                          std::forward<PipeWrappers>(Wrappers)...));
+  }
+
+  Step &addStep(Step &&NewStep);
+
+  llvm::Error run(llvm::StringRef EndingStepName,
+                  const ContainerToTargetsMap &Targets,
+                  llvm::raw_ostream *DiagnosticLog = nullptr);
+
+  void addContainerFactory(llvm::StringRef Name, ContainerFactory Entry) {
+    ContainerFactoriesRegistry.registerContainerFactory(Name, std::move(Entry));
+  }
+
+  /// Prefer this overload when the container you wish to add is default
+  /// constructible. This should be most of the time, but sometimes this is not
+  /// possible, in particular when some context information is needed such as
+  /// llvm containers that require the llvm context to be constructed.
+  template<typename Container>
+  void addDefaultConstructibleFactory(llvm::StringRef Name) {
+    auto *Message = "you can only registers containers before adding a step, "
+                    "otherwise the already present steps will not be aware of "
+                    "the newly registered containers.";
+    revng_assert(not IsContainerFactoriesRegistryFinalized, Message);
+    auto &Registry = ContainerFactoriesRegistry;
+    Registry.registerDefaultConstructibleFactory<Container>(Name);
+  }
+
+public:
+  /// Remove the provided target from all containers in all the steps, as well
+  /// as all all their transitive dependencies
+  llvm::Error invalidate(const Target &Target);
+  llvm::Error invalidate(const InvalidationMap &Invalidations);
+
+public:
+  llvm::Error storeToDisk(llvm::StringRef DirPath) const;
+  llvm::Error loadFromDisk(llvm::StringRef DirPath);
+
+public:
+  void deduceAllPossibleTargets(State &State) const;
+
+public:
+  bool hasSuccessors(llvm::StringRef Name) const {
+    return hasSuccessors(operator[](Name));
+  }
+
+  bool hasSuccessors(const Step &Current) const {
+    const auto IsSuccessor = [&Current](const Step &MaybeSucessor) {
+      if (not MaybeSucessor.hasPredecessor())
+        return false;
+
+      return &Current == &MaybeSucessor.getPredecessor();
+    };
+
+    return llvm::any_of(*this, IsSuccessor);
+  }
+
+  bool containsStep(llvm::StringRef Name) const {
+    return llvm::any_of(*this, [&Name](const Step &Step) {
+      return Step.getName() == Name;
+    });
+  }
+
+public:
+  iterator begin() {
+    return revng::dereferenceIterator(ReversePostOrderIndexes.begin());
+  }
+  iterator end() {
+    return revng::dereferenceIterator(ReversePostOrderIndexes.end());
+  }
+
+  const_iterator begin() const {
+    return revng::dereferenceIterator(ReversePostOrderIndexes.begin());
+  }
+  const_iterator end() const {
+    return revng::dereferenceIterator(ReversePostOrderIndexes.end());
+  }
+
+  size_t size() const { return Steps.size(); }
+
+public:
+  template<typename OStream>
+  void dump(OStream &OS, size_t Indentation = 0) const {
+    for (const auto &Step : Steps) {
+      indent(OS, Indentation);
+      OS << Step.first().str() << "\n";
+      Step.second.dump(OS, Indentation);
+    }
+  }
+
+  void dump() const debug_function { dump(dbg); }
+};
+
+class PipelineFileMapping {
+private:
+  std::string Step;
+  std::string Container;
+  std::string InputFile;
+
+public:
+  PipelineFileMapping(llvm::StringRef Step,
+                      llvm::StringRef Container,
+                      llvm::StringRef InputFile) :
+    Step(Step.str()), Container(Container.str()), InputFile(InputFile.str()) {}
+
+public:
+  static llvm::Expected<PipelineFileMapping> parse(llvm::StringRef ToParse);
+
+public:
+  llvm::Error loadFromDisk(Runner &LoadInto) const;
+  llvm::Error storeToDisk(const Runner &LoadInto) const;
+};
+
+} // namespace pipeline
