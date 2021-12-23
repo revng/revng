cb3d8122a971bf57f1d8fd00d5070d426cd07615
Massimo Fioravanti <massimofioravanti@rev.ng>
2021-12-23 16:06:25 +0100

    Added TypeWithID

--- include/revng/Support/Concepts.h
+++ include/revng/Support/Concepts.h
@@ -4,5 +4,15 @@
 // This file is distributed under the MIT License. See LICENSE.md for details.
 //
 
+#include <type_traits>
+
 template<typename T, typename U>
 concept same_as = std::is_same_v<T, U>;
+
+template<typename T, typename U>
+concept convertible_to = std::is_convertible_v<U, T>;
+
+template<typename T>
+concept TypeWithID = requires(T a) {
+  { T::ID } -> convertible_to<const char &>;
+};
