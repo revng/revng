371f5c455508fa1b176305faaa11ecc3a2185c39
Ivan Krysak <ivan@rev.ng>
2022-01-19 17:16:10 +0200

    Implement ABI-based register state deductions
    This will help EarlyFunctionAnalysis in providing more accurate results,
    in case assumptions about the ABI can be made.

--- include/revng/ABI/RegisterState.h
+++ include/revng/ABI/RegisterState.h
@@ -4,8 +4,11 @@
 // This file is distributed under the MIT License. See LICENSE.md for details.
 //
 
+#include "llvm/ADT/SmallVector.h"
 #include "llvm/ADT/StringRef.h"
 
+#include "revng/Model/Architecture.h"
+#include "revng/Model/Register.h"
 #include "revng/Support/Assert.h"
 
 namespace abi::RegisterState {
@@ -19,7 +22,6 @@ enum Values {
   YesOrDead,
   Maybe,
   Contradiction,
-
   Count
 };
 
@@ -77,5 +79,86 @@ inline bool isYesOrDead(abi::RegisterState::Values V) {
 inline bool shouldEmit(abi::RegisterState::Values V) {
   return isYesOrDead(V);
 }
+
+class Map {
+public:
+  struct StatePair {
+    RegisterState::Values IsUsedForPassingArguments = RegisterState::Invalid;
+    RegisterState::Values IsUsedForReturningValues = RegisterState::Invalid;
+  };
+
+public:
+  explicit Map(model::Architecture::Values Architecture) :
+    Architecture(Architecture),
+    Internal(model::Register::getCount(Architecture)) {}
+
+  StatePair &at(model::Register::Values Requested) {
+    return Internal[Requested - model::Register::getFirst(Architecture)];
+  }
+  const StatePair &at(model::Register::Values Requested) const {
+    return Internal[Requested - model::Register::getFirst(Architecture)];
+  }
+
+  StatePair &operator[](model::Register::Values Requested) {
+    return at(Requested);
+  }
+  const StatePair &operator[](model::Register::Values Requested) const {
+    return at(Requested);
+  }
+
+  size_t size() const { return Internal.size(); }
+
+private:
+  std::pair<model::Register::Values, StatePair &> mapIt(StatePair &Value) {
+    size_t Offset = model::Register::getFirst(Architecture);
+    auto ResultIndex = Offset + std::distance(&Internal.front(), &Value);
+    return { model::Register::Values(ResultIndex), Value };
+  }
+  std::pair<model::Register::Values, const StatePair &>
+  mapIt(const StatePair &Value) const {
+    size_t Offset = model::Register::getFirst(Architecture);
+    auto ResultIndex = Offset + std::distance(&Internal.front(), &Value);
+    return { model::Register::Values(ResultIndex), Value };
+  }
+
+  auto mapped_range() {
+    auto L = [this](StatePair &Value) { return this->mapIt(Value); };
+    return llvm::make_range(revng::map_iterator(Internal.begin(), L),
+                            revng::map_iterator(Internal.end(), L));
+  }
+  auto reverse_mapped_range() {
+    auto L = [this](StatePair &Value) { return this->mapIt(Value); };
+    return llvm::make_range(revng::map_iterator(Internal.rbegin(), L),
+                            revng::map_iterator(Internal.rend(), L));
+  }
+  auto const_mapped_range() const {
+    auto L = [this](const StatePair &Value) { return this->mapIt(Value); };
+    return llvm::make_range(revng::map_iterator(Internal.begin(), L),
+                            revng::map_iterator(Internal.end(), L));
+  }
+  auto const_reverse_mapped_range() const {
+    auto L = [this](const StatePair &Value) { return this->mapIt(Value); };
+    return llvm::make_range(revng::map_iterator(Internal.rbegin(), L),
+                            revng::map_iterator(Internal.rend(), L));
+  }
+
+public:
+  auto begin() { return mapped_range().begin(); }
+  auto rbegin() { return reverse_mapped_range().begin(); }
+  auto cbegin() const { return const_mapped_range().begin(); }
+  auto crbegin() const { return const_reverse_mapped_range().begin(); }
+
+  auto end() { return mapped_range().end(); }
+  auto rend() { return reverse_mapped_range().end(); }
+  auto cend() const { return const_mapped_range().end(); }
+  auto crend() const { return const_reverse_mapped_range().end(); }
+
+  auto begin() const { return cbegin(); }
+  auto end() const { return cend(); }
+
+private:
+  const model::Architecture::Values Architecture;
+  llvm::SmallVector<StatePair, 64> Internal;
+};
 
 } // namespace abi::RegisterState
---
+++ include/revng/ABI/RegisterStateDeductions.h
@@ -0,0 +1,27 @@
+#pragma once
+
+//
+// This file is distributed under the MIT License. See LICENSE.md for details.
+//
+
+#include <array>
+#include <iterator>
+#include <optional>
+#include <span>
+#include <variant>
+
+#include "revng/ABI/RegisterState.h"
+#include "revng/ADT/STLExtras.h"
+#include "revng/Model/ABI.h"
+#include "revng/Model/Architecture.h"
+#include "revng/Model/Register.h"
+#include "revng/Support/EnumSwitch.h"
+
+namespace abi {
+
+std::optional<abi::RegisterState::Map>
+applyRegisterStateDeductions(const abi::RegisterState::Map &State,
+                             model::ABI::Values ABI,
+                             bool EnforceABIConformance = false);
+
+} // namespace abi
