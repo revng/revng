1df6bbc6d618a3533ced26807a39de2bd82b7b0e
Ivan Krysak <ivan@rev.ng>
2022-01-19 17:18:16 +0200

    Introduce ABI testing infrastructure

--- tests/Tests.cmake
+++ tests/Tests.cmake
@@ -9,6 +9,7 @@ include(${CMAKE_SOURCE_DIR}/tests/unit/UnitTests.cmake)
 include(${CMAKE_SOURCE_DIR}/tests/analysis/AnalysisTests.cmake)
 include(${CMAKE_SOURCE_DIR}/tests/runtime/RuntimeTests.cmake)
 
-add_subdirectory(${CMAKE_SOURCE_DIR}/tests/tools/pipeline)
+add_subdirectory(tests/tools/pipeline)
+add_subdirectory(tests/abi)
 
 set(TEST_CFLAGS_${ARCH} "${TEST_CFLAGS_${ARCH}} -mthumb")
