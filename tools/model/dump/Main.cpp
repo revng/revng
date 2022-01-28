3980ee3fad3b5b18d255bc5eb6bd1552478a73b7
Alessandro Di Federico <ale@rev.ng>
2022-01-28 16:37:40 +0100

    Introduce model tools

---
+++ tools/model/dump/Main.cpp
@@ -0,0 +1,43 @@
+/// \file Main.cpp
+/// \brief
+
+//
+// This file is distributed under the MIT License. See LICENSE.md for details.
+//
+
+#include <string>
+
+#include "llvm/Support/CommandLine.h"
+
+#include "revng/Model/ToolHelpers.h"
+
+using namespace llvm;
+
+static cl::OptionCategory ThisToolCategory("Tool options", "");
+
+cl::opt<std::string> OutputFilename("o",
+                                    cl::cat(ThisToolCategory),
+                                    cl::desc("Override output filename"),
+                                    cl::init("-"),
+                                    cl::value_desc("filename"));
+
+static cl::opt<std::string> InputModulePath(cl::Positional,
+                                            cl::cat(ThisToolCategory),
+                                            cl::desc("<input module file>"),
+                                            cl::init("-"),
+                                            cl::value_desc("module"));
+
+int main(int Argc, char *Argv[]) {
+  cl::HideUnrelatedOptions({ &ThisToolCategory });
+  cl::ParseCommandLineOptions(Argc, Argv);
+
+  ExitOnError ExitOnError;
+
+  auto MaybeModel = ModelInModule::load(InputModulePath);
+  if (not MaybeModel)
+    ExitOnError(MaybeModel.takeError());
+
+  ExitOnError(MaybeModel->save(OutputFilename, ModelOutputType::YAML));
+
+  return EXIT_SUCCESS;
+}
