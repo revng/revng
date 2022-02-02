91fd04079aa8351e003312d22ad2f89315264aa5
Alessandro Di Federico <ale@rev.ng>
2022-02-02 14:38:17 +0100

    AllPasses.h: include Verify.h

--- include/revng/Model/Pass/AllPasses.h
+++ include/revng/Model/Pass/AllPasses.h
@@ -7,3 +7,4 @@
 #include "revng/Model/Pass/DeduplicateEquivalentTypes.h"
 #include "revng/Model/Pass/PromoteOriginalName.h"
 #include "revng/Model/Pass/PurgeUnnamedAndUnreachableTypes.h"
+#include "revng/Model/Pass/Verify.h"
