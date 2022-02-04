cafe44097b0596a6b804abd14a11bb47f5f22ec7
Ivan Krysak <ivan@rev.ng>
2022-02-04 16:27:41 +0200

    ABI: introduce default function type generation

---
+++ include/revng/ABI/DefaultFunctionPrototype.h
@@ -0,0 +1,25 @@
+#pragma once
+
+//
+// This file is distributed under the MIT License. See LICENSE.md for details.
+//
+
+#include <optional>
+
+#include "revng/Model/ABI.h"
+#include "revng/Model/Binary.h"
+
+namespace abi {
+
+/// Creates the "default" prototype for a function.
+///
+/// Such a prototype considers all the possible ways to pass
+/// an argument in or an return value out as used.
+///
+/// If `ABI` parameter was not provided, `BinaryToRecordTheTypeAt.DefaultABI`
+/// is used instead.
+model::TypePath
+defaultFunctionPrototype(model::Binary &BinaryToRecordTheTypeAt,
+                         std::optional<model::ABI::Values> ABI = std::nullopt);
+
+} // namespace abi
