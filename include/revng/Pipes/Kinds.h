fa8061b805571ef2408ae6f722ffb1bcc9ed0f21
Alessandro Di Federico <ale@rev.ng>
2022-02-08 16:04:49 +0100

    Drop dead `Kind`s

--- include/revng/Pipes/Kinds.h
+++ include/revng/Pipes/Kinds.h
@@ -7,16 +7,12 @@
 #include "revng/Pipeline/Kind.h"
 
 namespace revng::pipes {
+
 extern pipeline::Rank RootRank;
 
-extern pipeline::Kind CFepper;
 extern pipeline::Kind Binary;
 
 extern pipeline::Kind Object;
 extern pipeline::Kind Translated;
-
-extern pipeline::Kind ABIEnforced;
-
-extern pipeline::Kind Dead;
 
 } // namespace revng::pipes
--- lib/Pipes/Kinds.cpp
+++ lib/Pipes/Kinds.cpp
@@ -19,14 +19,7 @@ Kind revng::pipes::Binary("Binary", &RootRank);
 
 Kind revng::pipes::Object("Object", &RootRank);
 Kind revng::pipes::Translated("Translated", &RootRank);
-Kind revng::pipes::Dead("Dead", &RootRank);
-
-Kind revng::pipes::CFepper("CFepper", &FunctionsRank);
-Kind revng::pipes::ABIEnforced("ABIEnforced", Isolated, &FunctionsRank);
 
 static RegisterKind K1(Translated);
 static RegisterKind K2(Binary);
-static RegisterKind K3(Dead);
 static RegisterKind K4(Object);
-static RegisterKind K5(ABIEnforced);
-static RegisterKind K6(CFepper);
