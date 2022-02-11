806f07670989069f3dc375d163c651d652df9c4d
Alessandro Di Federico <ale@rev.ng>
2022-02-11 08:26:10 +0100

    Minor changes

--- include/revng/Pipes/Kinds.h
+++ include/revng/Pipes/Kinds.h
@@ -11,6 +11,7 @@
 namespace revng::pipes {
 
 inline pipeline::Rank RootRank("Root Rank");
+
 inline pipeline::Rank FunctionsRank("Function Rank", RootRank);
 
 inline pipeline::Kind Binary("Binary", &RootRank);
