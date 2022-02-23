9ed4347c9d37e9ee97be5ddf729d804b3caa1b58
Filippo Cremonese <filippocremonese@rev.ng>
2022-02-23 17:56:13 +0100

    PipelineC: Fix typo

--- include/revng/Pipeline/Container.h
+++ include/revng/Pipeline/Container.h
@@ -59,7 +59,7 @@ public:
   /// path that contains the serialized version of this object.
   virtual llvm::Error storeToDisk(llvm::StringRef Path) const = 0;
 
-  /// The implementation must esure that the content of this file will be
+  /// The implementation must ensure that the content of this file will be
   /// loaded from the provided path.
   virtual llvm::Error loadFromDisk(llvm::StringRef Path) = 0;
 };
