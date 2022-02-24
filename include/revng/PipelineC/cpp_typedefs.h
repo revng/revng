a4c3151b78bae9b043dcecb4ba0cf3e385d4e4b8
Filippo Cremonese <filippocremonese@rev.ng>
2022-02-24 11:28:29 +0100

    PipelineC: Refactor headers

---
+++ include/revng/PipelineC/cpp_typedefs.h
@@ -0,0 +1,12 @@
+#pragma once
+//
+// This file is distributed under the MIT License. See LICENSE.md for details.
+//
+
+typedef revng::pipes::PipelineManager rp_manager;
+typedef const pipeline::Kind rp_kind;
+typedef pipeline::Step rp_step;
+typedef pipeline::ContainerSet::value_type rp_container;
+typedef const pipeline::ContainerFactorySet::value_type rp_container_identifier;
+typedef const pipeline::Target rp_target;
+typedef const pipeline::TargetsList rp_targets_list;
