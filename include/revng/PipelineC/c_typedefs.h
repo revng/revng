bde14a10f4f49ffd0c6079cb68e05864cf9bc186
Filippo Cremonese <filippocremonese@rev.ng>
2022-02-24 11:28:29 +0100

    PipelineC: Refactor headers

---
+++ include/revng/PipelineC/c_typedefs.h
@@ -0,0 +1,13 @@
+#pragma once
+//
+// This file is distributed under the MIT License. See LICENSE.md for details.
+//
+
+typedef struct rp_manager rp_manager;
+typedef struct rp_kind rp_kind;
+typedef struct rp_step rp_step;
+typedef struct rp_container rp_container;
+typedef struct rp_container_identifier rp_container_identifier;
+typedef struct rp_target rp_target;
+typedef struct rp_targets_list rp_targets_list;
+typedef struct rp_model_diff rp_model_diff;
