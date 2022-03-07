b1dac5d17b209d903482e94c0935a6a8a9b5a4b9
Massimo Fioravanti <massimofioravanti@rev.ng>
2022-03-07 18:30:03 +0100

    ContainerBase: introduce serialization primitives

--- include/revng/Pipeline/Container.h
+++ include/revng/Pipeline/Container.h
@@ -6,6 +6,9 @@
 
 #include <memory>
 
+#include "llvm/Support/MemoryBuffer.h"
+#include "llvm/Support/raw_ostream.h"
+
 #include "revng/Pipeline/Target.h"
 #include "revng/Support/Concepts.h"
 
@@ -53,15 +56,28 @@ public:
 
   /// The implementation must ensure that
   /// not after(this)->enumerate().contains(Targets);
+  ///
+  /// returns false if nothing was removed
   virtual bool remove(const TargetsList &Targets) = 0;
+
+  /// The implementation for a Type T that extends ContainerBase must ensure
+  /// that a new instance of T, on which deserialize has been invoked with the
+  /// serialized content of a old instance, must be equal to the old instance.
+  virtual llvm::Error serialize(llvm::raw_ostream &OS) const = 0;
+
+  /// same as serialize
+  virtual llvm::Error deserialize(const llvm::MemoryBuffer &Buffer) = 0;
+
+  /// Must reset the state of the container to the just built state
+  virtual void clear() = 0;
 
   /// The implementation must ensure that there exists a file at the provided
   /// path that contains the serialized version of this object.
-  virtual llvm::Error storeToDisk(llvm::StringRef Path) const = 0;
+  virtual llvm::Error storeToDisk(llvm::StringRef Path) const;
 
   /// The implementation must esure that the content of this file will be
   /// loaded from the provided path.
-  virtual llvm::Error loadFromDisk(llvm::StringRef Path) = 0;
+  virtual llvm::Error loadFromDisk(llvm::StringRef Path);
 };
 
 /// CRTP class to be extended to implement a pipeline container.
