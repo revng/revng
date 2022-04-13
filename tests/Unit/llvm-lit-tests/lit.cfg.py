27195d903fa6da72b2b53ec8faf375bf130023fb
Giacomo Vercesi <giacomo.vercesi@rev.ng>
2022-04-13 18:00:54 +0200

    Sync with new revng-check-conventions script
    Fixes a couple of minor formatting errors
    Remove the use of tail in tests, use grep
    Disable checks on lit.cfg.py since it's a fragment

--- tests/Unit/llvm-lit-tests/lit.cfg.py
+++ tests/Unit/llvm-lit-tests/lit.cfg.py
@@ -1,8 +1,11 @@
+# flake8: noqa: F821
+# type: ignore
+
 import lit.formats
 
 config.name = "revng-c"
 config.test_format = lit.formats.ShTest(True)
-config.suffixes = ['.ll']
+config.suffixes = [".ll"]
 config.test_source_root = os.path.dirname(__file__)
 config.test_exec_root = config.my_obj_root
-config.substitutions.append(('%revngopt', 'revng --prefix=' + config.my_obj_root + ' opt '))
+config.substitutions.append(("%revngopt", "revng --prefix=" + config.my_obj_root + " opt "))
