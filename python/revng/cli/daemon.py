616eedaaa2e68231793e8bd75305b868aa682d11
Giacomo Vercesi <giacomo.vercesi@rev.ng>
2022-04-28 18:13:12 +0200

    Add extra command line options to 'revng daemon'
    --production runs the server on all interfaces and with debug options
    disabled
    --hypercorn-args allows to pass extra options to hypercorn when running
    revng daemon

--- python/revng/cli/daemon.py
+++ python/revng/cli/daemon.py
@@ -3,6 +3,8 @@
 #
 
 import os
+import shlex
+from argparse import ArgumentParser
 from sys import executable as py_executable
 
 from .commands_registry import Command, Options, commands_registry
@@ -13,37 +15,47 @@ class DaemonCommand(Command):
     def __init__(self):
         super().__init__(("daemon",), "Run revng GraphQL server")
 
-    def register_arguments(self, parser):
-        parser.add_argument("-p", "--port", type=str)
+    def register_arguments(self, parser: ArgumentParser):
+        parser.add_argument("-p", "--port", type=str, default="8000", help="Port to use")
+        parser.add_argument(
+            "--hypercorn-args", type=str, default="", help="Extra arguments to pass to hypercorn"
+        )
+        parser.add_argument(
+            "--production",
+            action="store_true",
+            help="Start server in production mode, "
+            + "this runs the server on all interfaces and disables debug information",
+        )
 
     def run(self, options: Options):
         libraries, _ = collect_libraries(options.search_prefixes)
         pipelines = collect_files(options.search_prefixes, ["share", "revng", "pipelines"], "*.yml")
 
-        if options.parsed_args.port is not None:
-            port = options.parsed_args.port
-        else:
-            port = "8000"
-
         env = {
-            **os.environ,
             "REVNG_ANALYSIS_LIBRARIES": ":".join(libraries),
             "REVNG_PIPELINES": ",".join(pipelines),
-            "STARLETTE_DEBUG": "1",
         }
-        return run(
+
+        args = ["-k", "asyncio", "-w", "1"]
+        extra_args = shlex.split(options.parsed_args.hypercorn_args)
+
+        if options.parsed_args.production:
+            args.extend(["-b", f"0.0.0.0:{options.parsed_args.port}"])
+        else:
+            args.extend(["-b", f"127.0.0.1:{options.parsed_args.port}", "--debug"])
+            env["STARLETTE_DEBUG"] = "1"
+
+        run(
             [
                 py_executable,
                 "-m",
                 "hypercorn",
-                "-b",
-                f"127.0.0.1:{port}",
-                "-k",
-                "asyncio",
+                *args,
+                *extra_args,
                 "revng.daemon:app",
             ],
             options,
-            env,
+            {**os.environ, **env},
             True,
         )
 
