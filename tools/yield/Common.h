8a07e6776715e2ce465f28c6f73f24d38e238a07
Ivan Krysak <ivan@rev.ng>
2022-05-03 13:18:49 +0300

    Add `revng yield assembly` tool

---
+++ tools/yield/Common.h
@@ -0,0 +1,35 @@
+#pragma once
+
+//
+// This file is distributed under the MIT License. See LICENSE.md for details.
+//
+
+#include <memory>
+
+#include "revng/Lift/LoadBinaryPass.h"
+#include "revng/Pipes/StringMapContainer.h"
+#include "revng/TupleTree/TupleTree.h"
+
+namespace llvm {
+class LLVMContext;
+class MemoryBuffer;
+class Module;
+} // namespace llvm
+
+namespace model {
+class Binary;
+}
+
+struct ObjectLifetimeController {
+  std::unique_ptr<llvm::LLVMContext> Context = {};
+  std::unique_ptr<llvm::Module> IR = {};
+  std::unique_ptr<TupleTree<model::Binary>> Model = {};
+  std::unique_ptr<llvm::MemoryBuffer> Binary = {};
+};
+
+using ReturnValueType = std::tuple<const llvm::Module &,
+                                   const model::Binary &,
+                                   RawBinaryView,
+                                   revng::pipes::StringMapContainer,
+                                   ObjectLifetimeController>;
+ReturnValueType parseCommandLineOptions(int Argc, char *Argv[]);
