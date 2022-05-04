540d5d8db35d6d259c6e91f3aea6895d6cc7608b
Pietro Fezzardi <pietro@rev.ng>
2022-05-04 16:00:57 +0200

    Add new tool revng-decompiled-yaml-to-c

---
+++ tools/decompiled-yaml-to-c/Main.cpp
@@ -0,0 +1,113 @@
+//
+// Copyright (c) rev.ng Labs Srl. See LICENSE.md for details.
+//
+
+#include "llvm/Support/CommandLine.h"
+#include "llvm/Support/Signals.h"
+
+#include "revng/Model/IRHelpers.h"
+#include "revng/Support/Debug.h"
+
+#include "revng-c/Backend/DecompileFunction.h"
+#include "revng-c/Pipes/Kinds.h"
+
+using namespace llvm::cl;
+
+using llvm::LLVMContext;
+using llvm::Module;
+
+static OptionCategory YAMLToCCategory("revng-decompiled-yaml-to-c options");
+
+static const char *Overview = "Ingests an YAML filed containing a mapping from "
+                              "Functions' MetaAddresses to the associated "
+                              "decompiled C code, and generates a valid file C "
+                              "containing all the Functions' bodies.";
+
+static opt<std::string> OutFile("o",
+                                Optional,
+                                init("-" /* for stdout */),
+                                desc("Output YAML file. It contains a mapping "
+                                     "from a Function's MetaAddress to the "
+                                     "decompiled C code generated for that "
+                                     "Function."),
+                                cat(YAMLToCCategory));
+
+static opt<std::string> InFile("i",
+                               Optional,
+                               init("-" /* for stdin */),
+                               desc("A YAML file containing a map. The key of "
+                                    "the map are Functions' MetaAddresses, and "
+                                    "the value is an escaped string containing "
+                                    "the C code of each decompiled function."),
+                               cat(YAMLToCCategory));
+
+static opt<std::string> TargetFunction("t",
+                                       Optional,
+                                       desc("MetaAddress of the target "
+                                            "function to decompile."),
+                                       cat(YAMLToCCategory));
+
+int main(int Argc, const char *Argv[]) {
+
+  // Enable LLVM stack trace
+  llvm::sys::PrintStackTraceOnErrorSignal(Argv[0]);
+
+  // Hide options not related to this tool
+  llvm::cl::HideUnrelatedOptions({ &YAMLToCCategory });
+
+  // Parse command line
+  bool Ok = llvm::cl::ParseCommandLineOptions(Argc,
+                                              Argv,
+                                              Overview,
+                                              &llvm::errs());
+  if (not Ok)
+    std::exit(EXIT_FAILURE);
+
+  // Open the output file.
+  std::error_code OutEC;
+  llvm::raw_fd_ostream OutputCCodeFile{ OutFile, OutEC };
+  if (OutEC)
+    revng_abort(OutEC.message().c_str());
+
+  // Read the input file into a buffer
+  auto InputBuffer = llvm::MemoryBuffer::getFile(InFile);
+  if (std::error_code InEC = InputBuffer.getError())
+    revng_abort(InEC.message().c_str());
+
+  using revng::pipes::FunctionStringMap;
+
+  model::Binary Model;
+  FunctionStringMap Functions("" /*Name*/,
+                              "application/"
+                              "x.yaml.c.decompiled",
+                              revng::pipes::DecompiledToC,
+                              Model);
+
+  // Deserialize the map
+  llvm::cantFail(Functions.deserialize(**InputBuffer));
+
+  OutputCCodeFile << "#include \"revng-model-types.h\"\n"
+                  << "#include \"revng-qemu-helpers-types.h\"\n\n";
+
+  if (TargetFunction.getNumOccurrences()) {
+    // If the TargetFunction option was passed, only print that function body
+    MetaAddress Target = MetaAddress::fromString(TargetFunction);
+
+    if (Target == MetaAddress::invalid())
+      revng_abort("MetaAddress of the function is invalid");
+
+    const auto &CCode = Functions[Target];
+    if (CCode.empty())
+      revng_abort("Cannot find target function");
+
+    OutputCCodeFile << CCode;
+  } else {
+    // Otherwise print all the Functions' bodies
+    for (const auto &[MetaAddress, CFunction] : Functions)
+      OutputCCodeFile << CFunction << '\n';
+  }
+
+  OutputCCodeFile.flush();
+
+  return 0;
+}
