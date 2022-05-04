13019a42902768348d76c9c676be85547062fcb8
Pietro Fezzardi <pietro@rev.ng>
2022-05-04 12:27:55 +0200

    Add -t TargetFunction option to revng-decompile
    This new option allows to decompile only a single function

--- tools/decompile/Main.cpp
+++ tools/decompile/Main.cpp
@@ -8,11 +8,13 @@
 #include "llvm/Support/CommandLine.h"
 #include "llvm/Support/Signals.h"
 
+#include "revng/Model/IRHelpers.h"
 #include "revng/Model/LoadModelPass.h"
 #include "revng/Support/Debug.h"
 
 #include "revng-c/Backend/DecompileFunction.h"
 #include "revng-c/Pipes/Kinds.h"
+#include "revng-c/Support/IRHelpers.h"
 
 using namespace llvm::cl;
 
@@ -50,6 +52,12 @@ static opt<std::string> ModelOverride("m",
                                            "or to ovverride the Model in the "
                                            "LLVM IR input file."),
                                       cat(DecompileCategory));
+
+static opt<std::string> TargetFunction("t",
+                                       Optional,
+                                       desc("MetaAddress of the target "
+                                            "function to decompile."),
+                                       cat(DecompileCategory));
 
 int main(int Argc, const char *Argv[]) {
 
@@ -102,6 +110,20 @@ int main(int Argc, const char *Argv[]) {
   llvm::raw_fd_ostream DecompiledOutFile{ OutFile, EC };
   if (EC)
     revng_abort(EC.message().c_str());
+
+  // If the TargetFunction option was passed, we look at the LLVM IR and strip
+  // away the bodies of all the functions that are not the target functions. In
+  // this way the decompiler does not emit them.
+  if (TargetFunction.getNumOccurrences()) {
+    MetaAddress Target = MetaAddress::fromString(TargetFunction);
+
+    if (Target == MetaAddress::invalid())
+      revng_abort("MetaAddress of the function is invalid");
+
+    for (llvm::Function &F : FunctionTags::Isolated.functions(Module.get()))
+      if (Target != getMetaAddressOfIsolatedFunction(F))
+        deleteOnlyBody(F);
+  }
 
   using revng::pipes::FunctionStringMap;
   FunctionStringMap DecompiledFunctions("" /*Name*/,
