da4bfb8644d4d6b82dbd359862753577ff0cae05
Ivan Krysak <ivan@rev.ng>
2022-05-11 15:43:32 +0300

    Implement CFG extraction

---
+++ include/revng/Yield/ControlFlow/Extraction.h
@@ -0,0 +1,24 @@
+#pragma once
+
+//
+// This file is distributed under the MIT License. See LICENSE.md for details.
+//
+
+namespace model {
+class Binary;
+}
+namespace yield {
+class Function;
+}
+namespace yield {
+class Graph;
+}
+
+namespace yield::cfg {
+
+struct Configuration;
+Graph extractFromInternal(const yield::Function &Function,
+                          const model::Binary &Binary,
+                          const Configuration &Configuration);
+
+} // namespace yield::cfg
