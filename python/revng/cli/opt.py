ccba62b13d2ffb11cf9e50df3414ede3a33ca7e7
Alessandro Di Federico <ale@rev.ng>
2022-05-12 21:46:53 +0200

    revng opt: do not add --help

--- python/revng/cli/opt.py
+++ python/revng/cli/opt.py
@@ -8,7 +8,7 @@ from .support import build_command_with_loads, run
 
 class IROptCommand(Command):
     def __init__(self):
-        super().__init__(("opt",), "LLVM's opt with rev.ng passes")
+        super().__init__(("opt",), "LLVM's opt with rev.ng passes", False)
 
     def register_arguments(self, parser):
         pass
