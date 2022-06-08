546175cc755cbe5a5fd80e428011fa1700383ab1
Pietro Fezzardi <pietro@rev.ng>
2022-06-08 07:52:14 +0200

    Fix typo in comment

--- lib/DataLayoutAnalysis/Middleend/RemoveInvalidPointers.cpp
+++ lib/DataLayoutAnalysis/Middleend/RemoveInvalidPointers.cpp
@@ -9,7 +9,7 @@ bool dla::RemoveInvalidPointers::runOnTypeSystem(LayoutTypeSystem &TS) {
 
   for (LayoutTypeSystemNode *PtrNode : llvm::nodes(&TS)) {
 
-    // Nodes whose size is the same of a pointer cannot have invalid outgoind
+    // Nodes whose size is the same of a pointer cannot have invalid outgoing
     // pointer edges.
     if (PtrNode->Size == PointerSize)
       continue;
