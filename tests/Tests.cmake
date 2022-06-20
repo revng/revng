9ce282865978207dc07fd412bbb40e4c310c8fe5
Alessandro Di Federico <ale@rev.ng>
2022-06-20 11:50:45 +0200

    Simplify CMake

--- tests/Tests.cmake
+++
@@ -1,8 +0,0 @@
-#
-# Copyright rev.ng Labs Srl. See LICENSE.md for details.
-#
-
-enable_testing()
-
-# Give control to the various subdirectories
-include(${CMAKE_SOURCE_DIR}/tests/unit/UnitTests.cmake)
