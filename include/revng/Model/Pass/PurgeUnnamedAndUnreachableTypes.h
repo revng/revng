1243d1061c453ed9298cd3943a5f6c9e8a69e315
Djordje Todorovic <dwarfyresearch@rev.ng>
2022-06-29 16:10:50 +0200

    Introduce `revng model hard-purge`
    In addition, introduce a new model Pass:
    
      revng model opt -prune-unsused-types

--- include/revng/Model/Pass/PurgeUnnamedAndUnreachableTypes.h
+++ include/revng/Model/Pass/PurgeUnnamedAndUnreachableTypes.h
@@ -12,4 +12,10 @@ namespace model {
 /// "outside" the type system itself.
 void purgeUnnamedAndUnreachableTypes(TupleTree<model::Binary> &Model);
 
+/// Remove all the types that cannot be reached from any type from `Functions`.
+void pruneUnusedTypes(TupleTree<model::Binary> &Model);
+
+/// Implement the purge logic.
+template<bool PruneAllUnusedTypes>
+void purgeTypesImpl(TupleTree<model::Binary> &Model);
 } // namespace model
