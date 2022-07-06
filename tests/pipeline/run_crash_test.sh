9f261cf3311d144bc9191a3f7fedab0616bb5fbd
Davide Depau <davide@rev.ng>
2022-07-06 16:19:02 +0200

    Add tests for stack trace signal handler
    This ensures that, on crash, revng-pipeline prints a stack trace

---
+++ tests/pipeline/run_crash_test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -o nounset
+# errexit and pipefail are intentionally missing
+
+CMAKE_BINARY_DIR="$1"
+EXPECTED_STATUS="$2"
+export REVNG_CRASH_SIGNAL="$3"
+
+"${CMAKE_BINARY_DIR}/libexec/revng/revng-pipeline" --load "${CMAKE_BINARY_DIR}/lib/librevngBadBehaviorLibrary.so" 2>&1 \
+    | tee -ip /dev/stderr \
+    | grep -q 'doCrash()\|WillCrash::WillCrash()\|librevngBadBehaviorLibrary.so'
+STATUS=("${PIPESTATUS[@]}")
+
+echo "Status: ${STATUS[*]}"
+
+if [[ "${STATUS[0]}" != "${EXPECTED_STATUS}" ]]; then
+  echo "revng-pipeline exited with incorrect exit status code ${STATUS[0]} (!= ${EXPECTED_STATUS})"
+  exit 1
+elif [[ "${STATUS[2]}" != "0" ]]; then
+  echo 'revng-pipeline did not output a stack trace'
+  exit 1
+else
+  exit 0
+fi
