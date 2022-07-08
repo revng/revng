165964bde10b076f3c4b479b2c63dc31c9ecb149
Ivan Krysak <ivan@rev.ng>
2022-07-08 18:16:15 +0300

    Add a `ConstexprString` container to ADT

---
+++ include/revng/ADT/ConstexprString.h
@@ -0,0 +1,48 @@
+#pragma once
+
+//
+// This file is distributed under the MIT License. See LICENSE.md for details.
+//
+
+#include <array>
+#include <string_view>
+
+/// Allows passing strings as template parameters
+///
+/// Objects of this type should only ever be constructed from a string literal.
+/// Internally, it stores the string (minus the last '\0' character as an array)
+template<size_t N>
+struct ConstexprString {
+  /// The data members have to be public for a literal type to be usable as
+  /// a template parameter :{
+  std::array<char, N - 1> String;
+
+  constexpr ConstexprString(const char (&Value)[N]) noexcept {
+    std::copy(Value, Value + N - 1, String.data());
+  }
+
+  constexpr ConstexprString(const ConstexprString &) = default;
+  constexpr ConstexprString(ConstexprString &&) = default;
+  constexpr ConstexprString &operator=(const ConstexprString &) = default;
+  constexpr ConstexprString &operator=(ConstexprString &&) = default;
+
+  constexpr size_t size() const noexcept { return String.size(); }
+  constexpr const char *begin() const noexcept { return String.begin(); }
+  constexpr const char *end() const noexcept { return String.end(); }
+  constexpr char operator[](size_t Idx) const noexcept { return String[Idx]; }
+
+  constexpr operator std::string_view() const noexcept {
+    return std::string_view{ String.data(), String.size() };
+  }
+};
+
+namespace examples {
+
+template<ConstexprString String>
+struct StringParametrizedTrait {
+  static constexpr std::string_view value = String;
+};
+
+static_assert(StringParametrizedTrait<"value">::value == "value");
+
+} // namespace examples
