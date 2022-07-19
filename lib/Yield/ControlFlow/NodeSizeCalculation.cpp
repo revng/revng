b94c23cf52fc7ea5fdbb013bdd071d4932b26fc3
Ivan Krysak <ivan@rev.ng>
2022-07-19 18:21:34 +0300

    Yield/ControlFlow: stop accounting for footer

--- lib/Yield/ControlFlow/NodeSizeCalculation.cpp
+++ lib/Yield/ControlFlow/NodeSizeCalculation.cpp
@@ -163,210 +163,21 @@ instructionSize(const yield::Instruction &Instruction,
                         Configuration.CommentFontSize,
                         Configuration));
 
-  // Instruction address.
+  // Annotation.
+  yield::Graph::Size RawBytesLengthWithOffsets{ 0, 0 };
+  RawBytesLengthWithOffsets.W += Instruction.RawBytes.size() * 3;
+  RawBytesLengthWithOffsets.W += CommentIndicatorSize + 5;
   appendSize(Result,
              fontSize(textSize(Instruction.Address.toString())
-                        + yield::Graph::Size(CommentIndicatorSize + 1, 0),
-                      Configuration.InstructionAddressFontSize,
+                        + RawBytesLengthWithOffsets,
+                      Configuration.AnnotationFontSize,
                       Configuration));
 
-  // Raw instruction bytes.
-  appendSize(Result,
-             fontSize(yield::Graph::Size(Instruction.RawBytes.size() * 3
-                                           + CommentIndicatorSize,
-                                         1),
-                      Configuration.InstructionBytesFontSize,
-                      Configuration));
-
-  // Account for the padding
+  // Account for the padding.
   Result.W += Configuration.HorizontalInstructionMarginSize * 2;
   Result.H += Configuration.VerticalInstructionMarginSize * 2;
 
   return Result;
-}
-
-using ParsedSuccessorVector = llvm::SmallVector<efa::ParsedSuccessor, 4>;
-
-class TargetSizeHelper {
-private:
-  const yield::BasicBlock &BasicBlock;
-  const yield::Function &Function;
-  const model::Binary &Binary;
-  const yield::Graph::Dimension PrefixSize;
-
-public:
-  TargetSizeHelper(const yield::BasicBlock &BasicBlock,
-                   const yield::Function &Function,
-                   const model::Binary &Binary,
-                   const yield::Graph::Dimension &PrefixSize) :
-    BasicBlock(BasicBlock),
-    Function(Function),
-    Binary(Binary),
-    PrefixSize(PrefixSize) {}
-
-  yield::Graph::Size singleTarget(const efa::ParsedSuccessor &Target) {
-    const auto &[NextAddress, CallAddress] = Target;
-    if (NextAddress.isValid()) {
-      if (CallAddress.isValid()) {
-        yield::Graph::Size Result{ PrefixSize, 1 };
-        Result.W += textSize("then goes to ").W + targetLinkWidth(NextAddress);
-        return appendSize(Result, call({ CallAddress }));
-      } else {
-        yield::Graph::Size Result{ PrefixSize, 1 };
-        Result.W += textSize("always goes to ").W
-                    + targetLinkWidth(NextAddress);
-        return Result;
-      }
-    } else {
-      if (CallAddress.isValid()) {
-        yield::Graph::Size Result{ PrefixSize, 1 };
-        Result.W += textSize("and does not return").W;
-        return appendSize(Result, call({ CallAddress }));
-      } else {
-        return { 0, 0 };
-      }
-    }
-  }
-
-  yield::Graph::Size twoTargets(const efa::ParsedSuccessor &First,
-                                const efa::ParsedSuccessor &Second) {
-    if (First.OptionalCallAddress.isValid()
-        || Second.OptionalCallAddress.isValid()) {
-      return multipleTargets({ First, Second });
-    }
-
-    MetaAddress FirstTarget = First.NextInstructionAddress;
-    MetaAddress SecondTarget = Second.NextInstructionAddress;
-    if (FirstTarget == SecondTarget)
-      return singleTarget(First);
-
-    if (FirstTarget == BasicBlock.End)
-      std::swap(FirstTarget, SecondTarget);
-
-    if (SecondTarget == BasicBlock.End) {
-      yield::Graph::Size R1{ PrefixSize, 1 };
-      R1.W += textSize("if taken, goes to ,").W + targetLinkWidth(FirstTarget);
-
-      yield::Graph::Size R2{ PrefixSize, 1 };
-      R2.W += textSize("otherwise, goes to ").W + targetLinkWidth(SecondTarget);
-
-      return appendSize(R1, R2);
-    } else {
-      return multipleTargets({ First, Second });
-    }
-  }
-
-  yield::Graph::Size multipleTargets(const ParsedSuccessorVector &Targets,
-                                     bool HasUnknownTargets = false) {
-    llvm::SmallVector<MetaAddress, 4> CallAddresses;
-    for (const auto &[_, Target] : Targets)
-      if (Target.isValid())
-        CallAddresses.emplace_back(Target);
-
-    yield::Graph::Size Result{ PrefixSize, 1 };
-    if (!CallAddresses.empty()) {
-      Result.W += textSize("then goes to one of: ").W;
-      appendSize(Result, call(CallAddresses));
-    } else {
-      Result.W += textSize("known targets include: ").W;
-    }
-
-    size_t ValidTargetCount = 0;
-    for (const auto &[Target, _] : Targets)
-      if (Target.isValid())
-        ++ValidTargetCount;
-    revng_assert(ValidTargetCount != 0);
-
-    for (size_t Counter = 0; const auto &[Target, _] : Targets) {
-      if (Target.isValid()) {
-        yield::Graph::Size Link{ PrefixSize, 1 };
-        Link.W += textSize("- ").W + targetLinkWidth(Target);
-        appendSize(Result, Link);
-      }
-    }
-
-    if (HasUnknownTargets == true) {
-      yield::Graph::Size AndMore{ PrefixSize, 1 };
-      AndMore.W += textSize("and more").W;
-      appendSize(Result, AndMore);
-    }
-
-    return Result;
-  }
-
-protected:
-  size_t targetLinkWidth(const MetaAddress &Target) {
-    return linkSize(Target, Function, Binary, 0, BasicBlock.End).W;
-  }
-
-  yield::Graph::Size
-  call(const llvm::SmallVector<MetaAddress, 4> &CallAddresses) {
-    revng_assert(!CallAddresses.empty());
-
-    yield::Graph::Size Result{ PrefixSize + textSize("calls ").W, 1 };
-    for (const MetaAddress &Address : CallAddresses)
-      Result.W += targetLinkWidth(Address);
-
-    Result.W += textSize(", ").W * (CallAddresses.size() - 1);
-
-    return Result;
-  }
-};
-
-static yield::Graph::Size
-targetFooterSize(const yield::BasicBlock &BasicBlock,
-                 const yield::Function &Function,
-                 const model::Binary &Binary,
-                 const yield::cfg::Configuration &Configuration) {
-  static const efa::ParsedSuccessor UnknownTarget{
-    .NextInstructionAddress = MetaAddress::invalid(),
-    .OptionalCallAddress = MetaAddress::invalid()
-  };
-
-  bool HasUnknown = false;
-  ParsedSuccessorVector Targets;
-  for (const auto &Edge : BasicBlock.Successors) {
-    auto TargetPair = efa::parseSuccessor(*Edge, BasicBlock.End, Binary);
-    if (TargetPair.NextInstructionAddress.isValid()
-        || TargetPair.OptionalCallAddress.isValid()) {
-      Targets.emplace_back(std::move(TargetPair));
-    } else {
-      HasUnknown = true;
-    }
-  }
-
-  namespace A = model::Architecture;
-  auto CommentIndicator = A::getAssemblyCommentIndicator(Binary.Architecture);
-
-  yield::Graph::Size Result = fontSize(textSize(CommentIndicator),
-                                       Configuration.InstructionFontSize,
-                                       Configuration);
-  auto LocalAppendHelper = [&](yield::Graph::Size &&Size) {
-    return appendSize(Result,
-                      fontSize(std::move(Size),
-                               Configuration.CommentFontSize,
-                               Configuration.InstructionFontSize,
-                               Configuration));
-  };
-
-  yield::Graph::Dimension PrefixSize = CommentIndicator.size() + 1;
-  TargetSizeHelper Helper(BasicBlock, Function, Binary, PrefixSize);
-  if (Targets.size() == 0) {
-    revng_assert(HasUnknown == true, "A basic block with no successors.");
-    return LocalAppendHelper(Helper.singleTarget(UnknownTarget));
-  } else if (Targets.size() == 1) {
-    const auto &First = Targets.front();
-    if (HasUnknown == false)
-      return LocalAppendHelper(Helper.singleTarget(First));
-    else
-      return LocalAppendHelper(Helper.twoTargets(First, UnknownTarget));
-  } else if (Targets.size() == 2 && HasUnknown == false) {
-    const auto &First = Targets.front();
-    const auto &Last = Targets.back();
-    return LocalAppendHelper(Helper.twoTargets(First, Last));
-  } else {
-    return LocalAppendHelper(Helper.multipleTargets(Targets, HasUnknown));
-  }
 }
 
 static yield::Graph::Size
@@ -402,11 +213,6 @@ basicBlockSize(const yield::BasicBlock &BasicBlock,
                              CommentIndicator.size(),
                              BasicBlock.HasDelaySlot));
   revng_assert(ToIterator == BasicBlock.Instructions.end());
-
-  // Account for the footer size.
-  // The footer is where target information is printed in the vertical layout.
-  appendSize(Result,
-             targetFooterSize(BasicBlock, Function, Binary, Configuration));
 
   return Result;
 }
