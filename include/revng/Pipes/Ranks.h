37caa91e6dfb5333d8748bafd29cec700c6f1fd5
Giacomo Vercesi <giacomo.vercesi@rev.ng>
2022-08-18 09:50:10 +0200

    Ranks: fix Rank keys
    Use model::Function::Key for the Function rank instead of the naked
    MetaAddress as it allows forward-compatibility if the model::Function
    key is ever changed.
    
    Use a std::tuple<uint64_t> for TypeField as it's the current type for
    {Struct,Enum,Union}Field.key()

--- include/revng/Pipes/Ranks.h
+++ include/revng/Pipes/Ranks.h
@@ -4,21 +4,23 @@
 // This file is distributed under the MIT License. See LICENSE.md for details.
 //
 
+#include "revng/Model/Function.h"
 #include "revng/Model/Type.h"
 #include "revng/Pipeline/Rank.h"
 #include "revng/Support/MetaAddress.h"
+#include "revng/Support/YAMLTraits.h"
 
 namespace revng::ranks {
 
 inline auto Binary = pipeline::defineRootRank<"binary">();
 
 using pipeline::defineRank;
-inline auto Function = defineRank<"function", MetaAddress>(Binary);
+inline auto Function = defineRank<"function", model::Function::Key>(Binary);
 inline auto BasicBlock = defineRank<"basic-block", MetaAddress>(Function);
 inline auto Instruction = defineRank<"instruction", MetaAddress>(BasicBlock);
 
 inline auto Type = defineRank<"type", model::Type::Key>(Binary);
-inline auto TypeField = defineRank<"type-field", uint64_t>(Type);
+inline auto TypeField = defineRank<"type-field", std::tuple<uint64_t>>(Type);
 
 inline auto RawByte = defineRank<"raw-byte", MetaAddress>(Binary);
 inline auto RawByteRange = defineRank<"raw-byte-range", MetaAddress>(RawByte);
