da993b3ceffb3b24c05ee410f45b50b8890353a9
Matteo Giordano <matteogiordano@rev.ng>
2022-12-16 15:03:01 +0100

    Pass: Visibility header file

---
+++ include/revng/Model/Pass/Visibility.h
@@ -0,0 +1,13 @@
+#pragma once
+
+//
+// This file is distributed under the MIT License. See LICENSE.md for details.
+//
+
+#include "revng/Model/Binary.h"
+
+namespace model {
+
+void calculateVisibility(TupleTree<model::Binary> &Model);
+
+} // namespace model
