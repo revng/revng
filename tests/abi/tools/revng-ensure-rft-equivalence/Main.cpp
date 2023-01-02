9973159af09794e10b76ada7eb5be53f0f2304a0
Ivan Krysak <ivan@rev.ng>
2023-01-02 14:12:03 +0100

    ABI-testing: make rtf-equivalence id-independent

--- tests/abi/tools/revng-ensure-rft-equivalence/Main.cpp
+++ tests/abi/tools/revng-ensure-rft-equivalence/Main.cpp
@@ -14,6 +14,7 @@
 /// specifically handles the IDs of `RawFunctionType`'s stack arguments.
 
 #include <string>
+#include <unordered_set>
 
 #include "llvm/Support/CommandLine.h"
 #include "llvm/Support/FileSystem.h"
@@ -51,6 +52,38 @@ static cl::opt<std::string> OutputFilename("o",
                                                           "filename"),
                                            llvm::cl::value_desc("filename"));
 
+static std::optional<std::pair<model::TypePath, model::TypePath>>
+ensureMatchingIDs(const model::Type::Key &Left,
+                  const model::Type::Key &Right,
+                  model::Binary &Model) {
+  auto &[LeftKind, LeftID] = Left;
+  auto &[RightKind, RightID] = Right;
+  revng_assert(LeftKind == RightKind);
+
+  if (LeftID != RightID) {
+    // Find the type
+    model::TypePath OldPath = Model.getTypePath(Right);
+    auto Iterator = Model.Types().find(OldPath.get()->key());
+
+    // Since the ID is a part of the key, we cannot just modify the type,
+    // we need to move the type out, and then reinsert it back in.
+    model::UpcastableType MovedOut = std::move(*Iterator);
+    Model.Types().erase(Iterator);
+
+    // Replace the ID of the moved out type.
+    MovedOut->ID() = LeftID;
+
+    // ReinsertTheType
+    model::TypePath NewPath = Model.recordNewType(std::move(MovedOut));
+
+    // Return the "replacement pair", so that the caller is able to gather them
+    // and invoke `TupleTree::replaceReferences` on all of them at once.
+    return std::make_pair(OldPath, NewPath);
+  } else {
+    return std::nullopt;
+  }
+}
+
 int main(int Argc, char *Argv[]) {
   revng::InitRevng X(Argc, Argv);
 
@@ -59,13 +92,15 @@ int main(int Argc, char *Argv[]) {
 
   ExitOnError ExitOnError;
 
-  auto LeftModel = ModelInModule::load(LeftModelPath);
-  if (not LeftModel)
-    ExitOnError(LeftModel.takeError());
+  auto LeftModule = ModelInModule::load(LeftModelPath);
+  if (not LeftModule)
+    ExitOnError(LeftModule.takeError());
+  TupleTree<model::Binary> &LeftModel = LeftModule->Model;
 
-  auto RightModel = ModelInModule::load(RightModelPath);
-  if (not RightModel)
-    ExitOnError(RightModel.takeError());
+  auto RightModule = ModelInModule::load(RightModelPath);
+  if (not RightModule)
+    ExitOnError(RightModule.takeError());
+  TupleTree<model::Binary> &RightModel = RightModule->Model;
 
   std::error_code EC;
   llvm::ToolOutputFile OutputFile(OutputFilename,
@@ -74,65 +109,130 @@ int main(int Argc, char *Argv[]) {
   if (EC)
     ExitOnError(llvm::createStringError(EC, EC.message()));
 
-  // Gather `RawFunctionType`s present in both the models based on their IDs.
+  // Introduce a function pair container to allow grouping them based on their
+  // `CustomName`.
   struct FunctionPair {
-    model::RawFunctionType *Left;
-    model::RawFunctionType *Right;
+    model::RawFunctionType *Left = nullptr;
+    model::RawFunctionType *Right = nullptr;
   };
-  std::map<model::Type::IDType, FunctionPair> Functions;
-  for (model::UpcastableType &LeftType : LeftModel->Model->Types())
-    if (auto *Left = llvm::dyn_cast<model::RawFunctionType>(LeftType.get()))
-      Functions[Left->ID()].Left = Left;
-  for (model::UpcastableType &RightType : RightModel->Model->Types())
-    if (auto *Right = llvm::dyn_cast<model::RawFunctionType>(RightType.get()))
-      Functions[Right->ID()].Right = Right;
+  struct TransparentComparator {
+    using is_transparent = std::true_type;
 
-  // Ensure their stack arguments have the same ID.
-  //
-  // If two `RawFunctionType`s have the same ID but their stack argument structs
-  // do not - replace the ID of the "Right" struct to be that of the "Left" one.
-  std::map<model::TypePath, model::TypePath> Replacements;
-  for (auto [_, Pair] : Functions) {
+    bool operator()(llvm::StringRef LHS, llvm::StringRef RHS) const {
+      return LHS < RHS;
+    }
+  };
+  std::map<model::Identifier, FunctionPair, TransparentComparator> Functions;
+  std::unordered_set<std::size_t> FunctionIDLookup;
+
+  // Make sure the default prototype is valid.
+  revng_assert(LeftModel->DefaultPrototype().isValid());
+  const auto &DefaultPrototype = *LeftModel->DefaultPrototype().get();
+
+  // Gather all the `RawFunctionType`s prototypes present in the first model.
+  for (model::Function &LF : LeftModel->Functions()) {
+    if (LF.Prototype().get() == nullptr)
+      continue; // Skip functions without prototypes.
+
+    revng_assert(LF.name() != "",
+                 "This test uses names to differentiate functions, as such "
+                 "having unnamed functions in the model would break it, "
+                 "hence it's not allowed.");
+
+    auto *Left = llvm::dyn_cast<model::RawFunctionType>(LF.Prototype().get());
+    revng_assert(Left != nullptr);
+    FunctionIDLookup.emplace(Left->ID());
+
+    if (Left->ID() == DefaultPrototype.ID())
+      continue; // Skip the default prototype.
+
+    auto [Iterator, Success] = Functions.try_emplace(LF.name());
+    revng_assert(Success);
+    Iterator->second.Left = Left;
+  }
+
+  // Gather all the `RawFunctionType`s prototypes present in the second model.
+  for (model::Function &RF : RightModel->Functions()) {
+    if (RF.Prototype().get() == nullptr)
+      continue; // Skip functions without prototypes.
+
+    revng_assert(RF.name() != "",
+                 "This test uses names to differentiate functions, as such "
+                 "having unnamed functions in the model would break it, "
+                 "hence it's not allowed.");
+
+    auto *Right = llvm::dyn_cast<model::RawFunctionType>(RF.Prototype().get());
+    revng_assert(Right != nullptr);
+
+    if (Right->ID() == DefaultPrototype.ID())
+      continue; // Skip the default prototype.
+
+    auto Iterator = Functions.find(RF.name());
+    if (Iterator == Functions.end()) {
+      std::string Error = "A function present in the right model is missing in "
+                          "the left one: "
+                          + RF.name().str().str();
+      revng_abort(Error.c_str());
+    }
+    revng_assert(Iterator->second.Right == nullptr);
+    Iterator->second.Right = Right;
+  }
+
+  // Ensure both the functions themselves and their stack argument structs have
+  // the same IDs. This makes it possible to use simple diff for comparing two
+  // models instead of doing that manually, since the ID is the only piece
+  // of the types that is allowed to change.
+  std::map<model::TypePath, model::TypePath> LeftReplacements;
+  std::map<model::TypePath, model::TypePath> RightReplacements;
+  for (auto [Name, Pair] : Functions) {
     auto [Left, Right] = Pair;
+
+    revng_assert(Left != nullptr,
+                 "This should never happen, something is VERY wrong.");
+    if (Right == nullptr) {
+      std::string Error = "A function present in the left model is missing in "
+                          "the right one: "
+                          + Name.str().str();
+      revng_abort(Error.c_str());
+    }
 
     // Try and access the argument struct.
     revng_check(Left->StackArgumentsType().Qualifiers().empty());
     revng_check(Right->StackArgumentsType().Qualifiers().empty());
-    model::Type
-      *LeftStackArguments = Left->StackArgumentsType().UnqualifiedType().get();
-    model::Type *
-      RightStackArguments = Right->StackArgumentsType().UnqualifiedType().get();
+    auto *LeftStack = Left->StackArgumentsType().UnqualifiedType().get();
+    auto *RightStack = Right->StackArgumentsType().UnqualifiedType().get();
 
     // XOR the `bool`eans - make sure that either both functions have stack
     // argument or neither one does.
-    revng_check(!LeftStackArguments == !RightStackArguments);
-
-    // Ignore function pairs without stack arguments.
-    if (LeftStackArguments == nullptr)
-      continue;
-
-    // If IDs differ - replace the ID.
-    if (LeftStackArguments->ID() != RightStackArguments->ID()) {
-      model::TypePath FromPath = Right->StackArgumentsType().UnqualifiedType();
-
-      RightModel->Model->Types().erase(LeftStackArguments->key());
-      auto *Struct = llvm::dyn_cast<model::StructType>(RightStackArguments);
-      revng_check(Struct != nullptr);
-      auto Copy = model::UpcastableType::make<model::StructType>(*Struct);
-      Copy->ID() = LeftStackArguments->ID();
-      auto ToPath = RightModel->Model->recordNewType(std::move(Copy));
-
-      Replacements.emplace(FromPath, ToPath);
+    revng_check(!LeftStack == !RightStack);
+    if (LeftStack != nullptr) {
+      model::Binary &RM = *RightModel;
+      if (auto R = ensureMatchingIDs(LeftStack->key(), RightStack->key(), RM))
+        RightReplacements.emplace(std::move(R.value()));
     }
+
+    if (auto R = ensureMatchingIDs(Left->key(), Right->key(), *RightModel))
+      RightReplacements.emplace(std::move(R.value()));
   }
 
   // Replace references to modified types.
-  RightModel->Model.replaceReferences(Replacements);
+  LeftModel.replaceReferences(LeftReplacements);
+  RightModel.replaceReferences(RightReplacements);
+
+  // Remove all the dynamic functions so that they don't interfere
+  LeftModel->ImportedDynamicFunctions().clear();
+  RightModel->ImportedDynamicFunctions().clear();
+
+  // Erase the default prototypes because they interfere with the test.
+  LeftModel->Types().erase(LeftModel->DefaultPrototype().get()->key());
+  RightModel->Types().erase(RightModel->DefaultPrototype().get()->key());
+  LeftModel->DefaultPrototype() = model::TypePath{};
+  RightModel->DefaultPrototype() = model::TypePath{};
 
   // Streamline both models and diff them
-  model::purgeUnnamedAndUnreachableTypes(LeftModel->Model);
-  model::purgeUnnamedAndUnreachableTypes(RightModel->Model);
-  auto Diff = diff(*LeftModel->Model, *RightModel->Model);
+  model::pruneUnusedTypes(LeftModel);
+  model::pruneUnusedTypes(RightModel);
+  TupleTreeDiff Diff = diff(*LeftModel, *RightModel);
 
   if (Diff.Changes.empty()) {
     return EXIT_SUCCESS;
