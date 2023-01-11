275e3943f7080294bb205566ecf3a0c9e7167874
Giacomo Vercesi <giacomo.vercesi@rev.ng>
2023-01-11 18:06:55 +0100

    daemon: rework starlette startup code
    Rework the startup code used to boot starlette, this allows removing the
    use of all globals, moreover it allow dropping the ManagerMiddleware
    class as it is no longer needed.

--- python/revng/daemon/__init__.py
+++ python/revng/daemon/__init__.py
@@ -7,16 +7,16 @@ import os
 import signal
 from datetime import timedelta
 from importlib import import_module
-from typing import List, Optional
+from typing import List
 
 from starlette.applications import Starlette
 from starlette.config import Config
 from starlette.middleware import Middleware
-from starlette.middleware.base import BaseHTTPMiddleware
 from starlette.middleware.cors import CORSMiddleware
 from starlette.middleware.gzip import GZipMiddleware
 from starlette.requests import Request
 from starlette.responses import PlainTextResponse
+from starlette.routing import Mount, Route
 
 from ariadne.asgi import GraphQL
 from ariadne.asgi.handlers import GraphQLHTTPHandler, GraphQLTransportWSHandler
@@ -26,87 +26,13 @@ from revng.api import Manager
 from revng.api._capi import initialize as capi_initialize
 from revng.api._capi import shutdown as capi_shutdown
 
-from .demo_webpage import demo_page, production_demo_page
+from .demo_webpage import generate_demo_page
 from .event_manager import EventManager
 from .schema_generator import SchemaGenerator
 from .util import project_workdir
 
-manager: Optional[Manager] = None
-event_manager: Optional[EventManager] = None
-startup_done = False
-
 config = Config()
 DEBUG = config("STARLETTE_DEBUG", cast=bool, default=False)
-
-
-class ManagerMiddleware(BaseHTTPMiddleware):
-    async def dispatch(self, request: Request, call_next):
-        assert manager, "Manager not initialized"
-        request.scope["manager"] = manager
-
-        # Safety checks
-        assert request.scope["manager"] is not None
-
-        return await call_next(request)
-
-
-async def status(request):
-    if startup_done:
-        return PlainTextResponse("OK")
-    else:
-        return PlainTextResponse("KO", 503)
-
-
-def generate_context(request: Request):
-    return {
-        "manager": manager,
-        "event_manager": event_manager,
-        "headers": request.headers,
-    }
-
-
-def startup():
-    global manager, event_manager, startup_done
-    capi_initialize(
-        signals_to_preserve=(
-            # Common terminal signals
-            signal.SIGINT,
-            signal.SIGTERM,
-            signal.SIGHUP,
-            signal.SIGCHLD,
-            # Issued by writing in closed sockets
-            signal.SIGPIPE,
-            # Used by uvicorn workers
-            signal.SIGUSR1,
-            signal.SIGUSR2,
-            signal.SIGQUIT,
-        )
-    )
-    manager = Manager(project_workdir())
-    event_manager = EventManager(manager)
-    app.mount(
-        "/graphql",
-        GraphQL(
-            SchemaGenerator().get_schema(manager),
-            context_value=generate_context,
-            http_handler=GraphQLHTTPHandler(extensions=[ApolloTracingExtension]),
-            websocket_handler=GraphQLTransportWSHandler(
-                connection_init_wait_timeout=timedelta(seconds=5)
-            ),
-            debug=DEBUG,
-        ),
-    )
-    startup_done = True
-
-
-def shutdown():
-    global manager
-    if manager is not None:
-        store_result = manager.save()
-        if not store_result:
-            logging.warning("Failed to store manager's containers")
-        del manager
-    capi_shutdown()
 
 
 def get_middlewares() -> List[Middleware]:
@@ -145,16 +71,73 @@ def parse_middleware_env(string: str | None) -> List[Middleware]:
     return result
 
 
-app = Starlette(
-    debug=DEBUG,
-    middleware=get_middlewares(),
-    on_startup=[startup],
-    on_shutdown=[shutdown],
-)
+def make_startlette() -> Starlette:
+    capi_initialize(
+        signals_to_preserve=(
+            # Common terminal signals
+            signal.SIGINT,
+            signal.SIGTERM,
+            signal.SIGHUP,
+            signal.SIGCHLD,
+            # Issued by writing in closed sockets
+            signal.SIGPIPE,
+            # Used by uvicorn workers
+            signal.SIGUSR1,
+            signal.SIGUSR2,
+            signal.SIGQUIT,
+        )
+    )
+    manager = Manager(project_workdir())
+    event_manager = EventManager(manager)
+    startup_done = False
+
+    async def status(request):
+        if startup_done:
+            return PlainTextResponse("OK")
+        else:
+            return PlainTextResponse("KO", 503)
+
+    def generate_context(request: Request):
+        return {
+            "manager": manager,
+            "event_manager": event_manager,
+            "headers": request.headers,
+        }
+
+    routes = [
+        Route("/", generate_demo_page(manager, DEBUG), methods=["GET"]),
+        Route("/status", status, methods=["GET"]),
+        Mount(
+            "/graphql",
+            GraphQL(
+                SchemaGenerator().get_schema(manager),
+                context_value=generate_context,
+                http_handler=GraphQLHTTPHandler(extensions=[ApolloTracingExtension]),
+                websocket_handler=GraphQLTransportWSHandler(
+                    connection_init_wait_timeout=timedelta(seconds=5)
+                ),
+                debug=DEBUG,
+            ),
+        ),
+    ]
+
+    def startup():
+        nonlocal startup_done
+        startup_done = True
+
+    def shutdown():
+        store_result = manager.save()
+        if not store_result:
+            logging.warning("Failed to store manager's containers")
+        capi_shutdown()
+
+    return Starlette(
+        debug=DEBUG,
+        middleware=get_middlewares(),
+        routes=routes,
+        on_startup=[startup],
+        on_shutdown=[shutdown],
+    )
 
 
-app.add_route("/status", status, ["GET"])
-if DEBUG:
-    app.add_route("/", demo_page, ["GET"])
-else:
-    app.add_route("/", production_demo_page, ["GET"])
+app = make_startlette()
