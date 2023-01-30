88d88a909d856757261f3514b5c6995924f7b652
Giacomo Vercesi <giacomo.vercesi@rev.ng>
2023-01-30 19:01:30 +0100

    daemon/test.py: Improve daemon log handling
    Improve the handling of the daemon process when running tests. This will
    print

--- share/revng/test/tests/daemon/conftest.py
+++ share/revng/test/tests/daemon/conftest.py
@@ -1,7 +1,7 @@
 #
 # This file is distributed under the MIT License. See LICENSE.md for details.
 #
-from pytest import Parser, hookimpl
+from pytest import Parser, TestReport, hookimpl
 
 
 def pytest_addoption(parser: Parser):
@@ -10,6 +10,5 @@ def pytest_addoption(parser: Parser):
 
 @hookimpl(tryfirst=True, hookwrapper=True)
 def pytest_runtest_makereport(item, call):
-    outcome = yield
-    rep = outcome.get_result()
+    rep: TestReport = (yield).get_result()
     setattr(item, "rep_" + rep.when, rep)
