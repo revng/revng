6f3dc255cf22f25c448c1fedc3e50cc2166beee2
Giacomo Vercesi <giacomo.vercesi@rev.ng>
2023-02-06 15:53:08 +0100

    revng.api: adopt rp_error
    Change Python's API to handle the new `rp_error` and its two variations:
    `DocumentError` and `SimpleError`.

--- python/revng/api/manager.py
+++ python/revng/api/manager.py
@@ -12,7 +12,7 @@ from revng.support.collect import collect_files
 from ._capi import ROOT, _api, ffi
 from .analysis import Analysis
 from .container import Container, ContainerIdentifier
-from .error_list import Expected, run_with_el
+from .errors import Error, Expected
 from .exceptions import RevngException
 from .invalidations import Invalidations, ResultWithInvalidations
 from .kind import Kind
@@ -434,29 +434,35 @@ class Manager:
         _name = make_c_string(name)
         _content = make_c_string(content)
         invalidations = Invalidations()
-        res = run_with_el(
-            _api.rp_manager_set_global, self._manager, _content, _name, invalidations._invalidations
+        error = Error()
+        res = _api.rp_manager_set_global(
+            self._manager, _content, _name, invalidations._invalidations, error._error
         )
-        return ResultWithInvalidations(res, invalidations)
+        return ResultWithInvalidations(Expected(res, error), invalidations)
 
     def verify_global(self, name, content) -> Expected[bool]:
         _name = make_c_string(name)
         _content = make_c_string(content)
-        return run_with_el(_api.rp_manager_verify_global, self._manager, _content, _name)
+        error = Error()
+        res = _api.rp_manager_verify_global(self._manager, _content, _name, error._error)
+        return Expected(res, error)
 
     def apply_diff(self, name, diff) -> ResultWithInvalidations[Expected[bool]]:
         _name = make_c_string(name)
         _diff = make_c_string(diff)
         invalidations = Invalidations()
-        res = run_with_el(
-            _api.rp_manager_apply_diff, self._manager, _diff, _name, invalidations._invalidations
+        error = Error()
+        res = _api.rp_manager_apply_diff(
+            self._manager, _diff, _name, invalidations._invalidations, error._error
         )
-        return ResultWithInvalidations(res, invalidations)
+        return ResultWithInvalidations(Expected(res, error), invalidations)
 
     def verify_diff(self, name, diff) -> Expected[bool]:
         _name = make_c_string(name)
         _diff = make_c_string(diff)
-        return run_with_el(_api.rp_manager_verify_diff, self._manager, _diff, _name)
+        error = Error()
+        res = _api.rp_manager_verify_diff(self._manager, _diff, _name, error._error)
+        return Expected(res, error)
 
     def get_model(self) -> str:
         return self.get_global("model.yml")
