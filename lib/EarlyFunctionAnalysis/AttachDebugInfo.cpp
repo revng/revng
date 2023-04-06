c16dc0a328a5e19690643948edf8f54844f4a984
Alessandro Di Federico <ale@rev.ng>
2023-04-06 08:58:15 +0200

    AttachDebugInfo: color instructions with a visit
    We used to only color instructions in the same BasicBlock as the call to
    `newpc`.  However, an instruction in the original binary can span
    multiple BasicBlocks.

--- lib/EarlyFunctionAnalysis/AttachDebugInfo.cpp
+++ lib/EarlyFunctionAnalysis/AttachDebugInfo.cpp
@@ -5,6 +5,7 @@
 // This file is distributed under the MIT License. See LICENSE.md for details.
 //
 
+#include "llvm/ADT/PostOrderIterator.h"
 #include "llvm/IR/DIBuilder.h"
 #include "llvm/IR/DebugInfoMetadata.h"
 #include "llvm/IR/Function.h"
@@ -24,67 +25,74 @@ using Register = RegisterPass<AttachDebugInfo>;
 static Register X("attach-debug-info", "Attach debug info metadata.");
 static Logger<> Log("attach-debug-info");
 
-static void handleBasicBlock(DIBuilder &DIB,
-                             llvm::BasicBlock &BB,
-                             DISubprogram *RootSubprogram,
-                             efa::FunctionMetadata &FM,
-                             GeneratedCodeBasicInfo &GCBI) {
+static bool isTrue(const llvm::Value *V) {
+  return getLimitedValue(V) != 0;
+}
+
+static void handleFunction(DIBuilder &DIB,
+                           llvm::Function &F,
+                           DISubprogram *RootSubprogram,
+                           efa::FunctionMetadata &FM,
+                           GeneratedCodeBasicInfo &GCBI) {
   namespace ranks = revng::ranks;
 
-  if (!isJumpTarget(&BB)) {
-    revng_log(Log, " Not a jump target " << BB.getName());
-    return;
-  }
-
-  const efa::BasicBlock *Block = FM.findBlock(GCBI, &BB);
-  if (!Block) {
-    revng_log(Log, " No metadata for " << BB.getName());
-    return;
-  }
-
-  revng_log(Log, " " << BB.getName() << ": " << Block->ID().toString());
-
-  llvm::Module &M = *BB.getParent()->getParent();
+  LLVMContext &Context = F.getParent()->getContext();
   DILocation *CurrentDebugLocation = nullptr;
-  for (auto &I : BB) {
-    if (auto *Call = getCallTo(&I, "newpc")) {
-      MetaAddress NewPC = blockIDFromNewPC(Call).start();
-      // We cloned `line:` only, but we need full MetaAddress that we keep
-      // as `DISubprogram`.
-      auto SPFlags = DISubprogram::toSPFlags(false, /* isLocalToUnit */
-                                             true, /* isDefinition*/
-                                             false /* isOptimized */);
-      auto Type = DIB.createSubroutineType(DIB.getOrCreateTypeArray({}));
-      // Let's make the debug location that points back to the binary.
-      std::string NewDebugLocation = serializedLocation(ranks::Instruction,
-                                                        FM.Entry(),
-                                                        Block->ID(),
-                                                        NewPC);
-      auto Subprogram = DIB.createFunction(RootSubprogram->getFile(), // Scope
-                                           NewDebugLocation, // Name
-                                           StringRef(), // LinkageName
-                                           RootSubprogram->getFile(), // File
-                                           1, // LineNo
-                                           Type, // Ty (subroutine type)
-                                           1, // ScopeLine
-                                           DINode::FlagPrototyped, // Flags
-                                           SPFlags);
-      DIB.finalizeSubprogram(Subprogram);
-      auto InlineLocationForMetaAddress = DILocation::get(M.getContext(),
-                                                          NewPC.address(),
-                                                          0,
-                                                          RootSubprogram,
-                                                          nullptr);
-      // Represent debug info for all the isolated functions as if they were
-      // inlined in the root.
-      auto InlineLoc = DILocation::get(M.getContext(),
-                                       NewPC.address(),
-                                       0,
-                                       Subprogram,
-                                       InlineLocationForMetaAddress);
-      CurrentDebugLocation = InlineLoc;
-      I.setDebugLoc(CurrentDebugLocation);
-    } else {
+  BasicBlockID LastJumpTarget;
+  for (auto *BB : ReversePostOrderTraversal(&F)) {
+    if (not GCBI.isTranslated(BB))
+      continue;
+
+    for (auto &I : *BB) {
+      if (auto *Call = getCallTo(&I, "newpc")) {
+        BasicBlockID Address = blockIDFromNewPC(Call);
+
+        if (isTrue(Call->getArgOperand(NewPCArguments::IsJumpTarget))) {
+          const auto &CFG = FM.ControlFlowGraph();
+          if (CFG.count(Address) != 0) {
+            LastJumpTarget = Address;
+          } else {
+            revng_assert(CFG.at(LastJumpTarget).contains(Address));
+          }
+        }
+
+        revng_assert(LastJumpTarget.isValid());
+        revng_assert(Address.inliningIndex() == LastJumpTarget.inliningIndex());
+
+        auto SPFlags = DISubprogram::toSPFlags(false, /* isLocalToUnit */
+                                               true, /* isDefinition*/
+                                               false /* isOptimized */);
+        auto Type = DIB.createSubroutineType(DIB.getOrCreateTypeArray({}));
+        // Let's make the debug location that points back to the binary.
+        std::string NewDebugLocation = serializedLocation(ranks::Instruction,
+                                                          FM.Entry(),
+                                                          LastJumpTarget,
+                                                          Address.start());
+        auto Subprogram = DIB.createFunction(RootSubprogram->getFile(), // Scope
+                                             NewDebugLocation, // Name
+                                             StringRef(), // LinkageName
+                                             RootSubprogram->getFile(), // File
+                                             1, // LineNo
+                                             Type, // Ty (subroutine type)
+                                             1, // ScopeLine
+                                             DINode::FlagPrototyped, // Flags
+                                             SPFlags);
+        DIB.finalizeSubprogram(Subprogram);
+        auto InlineLocationForMetaAddress = DILocation::get(Context,
+                                                            0,
+                                                            0,
+                                                            RootSubprogram,
+                                                            nullptr);
+        // Represent debug info for all the isolated functions as if they were
+        // inlined in the root.
+        auto InlineLoc = DILocation::get(Context,
+                                         0,
+                                         0,
+                                         Subprogram,
+                                         InlineLocationForMetaAddress);
+        CurrentDebugLocation = InlineLoc;
+      }
+
       I.setDebugLoc(CurrentDebugLocation);
     }
   }
@@ -139,8 +147,7 @@ bool AttachDebugInfo::runOnModule(llvm::Module &M) {
               "Metadata for Function " << F.getName() << ":"
                                        << FM.Entry().toString());
 
-    for (auto &BB : F)
-      handleBasicBlock(DIB, BB, RootSubprogram, FM, GCBI);
+    handleFunction(DIB, F, RootSubprogram, FM, GCBI);
   }
 
   return true;
