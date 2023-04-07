8860ea16e20877769a22ea65a695b35c3775ea8c
Alessandro Di Federico <ale@rev.ng>
2023-04-07 08:50:18 +0200

    Introduce HoistStructPhis
    This pass turns phis of invocations of pure functions into an invocation
    of the pure functions with one phis per argument of the original
    invocations.
    
    This makes handling phis easier in the backend and is necessary after
    the introduction of simplify-cfg with sinking enabled.

---
+++ lib/IRCanonicalization/HoistStructPhis.cpp
@@ -0,0 +1,94 @@
+//
+// Copyright (c) rev.ng Labs Srl. See LICENSE.md for details.
+//
+
+#include "llvm/Passes/PassBuilder.h"
+
+#include "revng/Support/Debug.h"
+
+using namespace llvm;
+
+static bool isLastBeforeTerminator(Instruction *I) {
+  auto It = I->getIterator();
+  ++It;
+  auto End = I->getParent()->end();
+  return It != End && ++It == End;
+}
+
+class HoistStructPhis : public llvm::FunctionPass {
+public:
+  static char ID;
+  HoistStructPhis() : llvm::FunctionPass(ID) {}
+
+  bool runOnFunction(llvm::Function &F) override {
+    llvm::SmallVector<PHINode *, 16> ToFix;
+
+    // Collect phis that need fixing
+    for (BasicBlock &BB : F)
+      for (Instruction &I : BB)
+        if (auto *Phi = dyn_cast<PHINode>(&I))
+          if (isa<StructType>(I.getType()))
+            ToFix.push_back(Phi);
+
+    if (ToFix.size() == 0)
+      return false;
+
+    for (PHINode *Phi : ToFix) {
+      auto PhiSize = Phi->getNumIncomingValues();
+
+      // Create all the phis
+      CallInst *FirstCall = nullptr;
+      Value *CalledValue = nullptr;
+      llvm::SmallVector<Value *, 2> Phis;
+      for (auto [V, Predecessor] : zip(Phi->incoming_values(), Phi->blocks())) {
+        // We only expect pure calls
+        auto *Call = cast<CallInst>(V);
+
+        if (CalledValue == nullptr) {
+          // This is the first call we see, make some extra checks
+          CalledValue = Call->getCalledOperand();
+          FirstCall = Call;
+
+          Function *Callee = Call->getCalledFunction();
+          revng_assert(Callee != nullptr);
+
+          revng_assert(isLastBeforeTerminator(Call)
+                       or Callee->onlyReadsMemory());
+
+          // First iteration, create phis
+          for (Type *ArgumentType : Call->getFunctionType()->params())
+            Phis.push_back(PHINode::Create(ArgumentType, PhiSize, "", Phi));
+
+        } else {
+          // Ensure all the incomings are calls to the same function
+          revng_assert(CalledValue == Call->getCalledOperand());
+        }
+
+        // Add an incoming
+        revng_assert(Call->arg_size() == Phis.size());
+        for (auto [Argument, Phi] : zip(Call->args(), Phis))
+          cast<PHINode>(Phi)->addIncoming(Argument, Predecessor);
+      }
+
+      // Create a new function call
+      Instruction *InsertionPoint = Phi->getParent()->getFirstNonPHI();
+      auto *NewCall = CallInst::Create({ FirstCall->getFunctionType(),
+                                         CalledValue },
+                                       ArrayRef<Value *>(Phis),
+                                       ArrayRef<OperandBundleDef>{},
+                                       "",
+                                       InsertionPoint);
+
+      // Steal metadata and replace original phi
+      NewCall->copyMetadata(*FirstCall);
+      revng_assert(NewCall->getType() == Phi->getType());
+      Phi->replaceAllUsesWith(NewCall);
+      Phi->eraseFromParent();
+    }
+
+    return true;
+  }
+};
+
+char HoistStructPhis::ID;
+static RegisterPass<HoistStructPhis> R("hoist-struct-phis", "", false, false);
