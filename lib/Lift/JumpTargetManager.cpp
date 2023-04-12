37ab35010222908f6e8d2ec7fdbe6c187ea5e66a
Djordje Todorovic <dwarfyresearch@rev.ng>
2023-04-12 22:39:18 +0200

    Minor changes

--- lib/Lift/JumpTargetManager.cpp
+++ lib/Lift/JumpTargetManager.cpp
@@ -1068,7 +1068,6 @@ void JumpTargetManager::setCFGForm(CFGForm::Values NewForm,
   revng_assert(CurrentCFGForm != NewForm);
   revng_assert(NewForm != CFGForm::UnknownForm);
 
-  std::set<BasicBlock *> Unreachable;
   static bool First = true;
   if (not First and VerifyLog.isEnabled()) {
     assertNoUnreachable();
