9bbafa9da4d2f495d6d9fb8796ed6de5510954b3
Pietro Fezzardi <pietro@rev.ng>
2023-04-18 23:55:36 +0200

    ExitSSA: group PHI in equivalence classes
    Each equivalence class informally represents a set of PHINodes for which
    we want to emit a single alloca.

--- lib/IRCanonicalization/ExitSSAPass.cpp
+++ lib/IRCanonicalization/ExitSSAPass.cpp
@@ -2,6 +2,7 @@
 // Copyright rev.ng Labs Srl. See LICENSE.md for details.
 //
 
+#include "llvm/ADT/EquivalenceClasses.h"
 #include "llvm/IR/BasicBlock.h"
 #include "llvm/IR/Function.h"
 #include "llvm/IR/IRBuilder.h"
@@ -20,10 +21,11 @@ using llvm::AnalysisUsage;
 using llvm::BasicBlock;
 using llvm::Function;
 using llvm::FunctionPass;
-using llvm::Instruction;
 using llvm::IRBuilder;
 using llvm::PHINode;
 using llvm::RegisterPass;
+using llvm::Use;
+using llvm::User;
 using llvm::Value;
 
 static Logger<> Log{ "exit-ssa" };
@@ -41,52 +43,161 @@ public:
   }
 };
 
+static std::vector<std::set<PHINode *>> getPHIEquivalenceClasses(Function &F) {
+
+  llvm::EquivalenceClasses<PHINode *> PHIEquivalenceClasses;
+
+  for (BasicBlock &BB : F) {
+    for (auto &PHI : BB.phis()) {
+
+      PHIEquivalenceClasses.insert(&PHI);
+
+      for (User *U : PHI.users()) {
+        if (auto *PHIUser = dyn_cast<PHINode>(U)) {
+          PHIEquivalenceClasses.insert(PHIUser);
+          PHIEquivalenceClasses.unionSets(&PHI, PHIUser);
+        }
+      }
+
+      for (Value *Incoming : PHI.incoming_values()) {
+        if (auto *PHIIncoming = dyn_cast<PHINode>(Incoming)) {
+          PHIEquivalenceClasses.insert(PHIIncoming);
+          PHIEquivalenceClasses.unionSets(&PHI, PHIIncoming);
+        }
+      }
+    }
+  }
+
+  std::vector<std::set<PHINode *>> Result;
+
+  auto I = PHIEquivalenceClasses.begin();
+  auto E = PHIEquivalenceClasses.end();
+  // Iterate over all of the members.
+  for (; I != E; ++I) {
+
+    // Ignore all the members that are not leaders of a class.
+    if (not I->isLeader())
+      continue;
+
+    // The interate all over the elements of a class, and build the set of
+    // PHINodes that represent that class.
+    std::set<PHINode *> PHISet;
+    auto PHIRange = llvm::make_range(PHIEquivalenceClasses.member_begin(I),
+                                     PHIEquivalenceClasses.member_end());
+    for (PHINode *PHI : PHIRange)
+      PHISet.insert(PHI);
+
+    Result.push_back(std::move(PHISet));
+  }
+
+  return Result;
+}
+
+static void
+replacePHIEquivalenceClass(const std::set<PHINode *> &PHIs, Function &F) {
+
+  revng_log(Log, "New PHIGroup ================");
+  LoggerIndent FirstIndent{ Log };
+
+  IRBuilder<> Builder(F.getContext());
+  Builder.SetInsertPointPastAllocas(&F);
+  AllocaInst *Alloca = Builder.CreateAlloca((*PHIs.begin())->getType());
+  revng_log(Log, "Created Alloca: " << dumpToString(Alloca));
+
+  {
+    revng_log(Log, "Replacing Incomings");
+    LoggerIndent IndentIncomings{ Log };
+
+    // First, we replace all the incoming that are not PHIs with stores of the
+    // incoming in the associated local variable.
+    for (auto *PHI : PHIs) {
+
+      revng_log(Log, "PHI: " << dumpToString(PHI));
+      LoggerIndent IndentPHI{ Log };
+
+      for (Use &IncomingUse : PHI->incoming_values()) {
+        Value *Incoming = IncomingUse.get();
+        revng_log(Log, "Incoming: " << dumpToString(Incoming));
+
+        // Skip PHINodes. They are already part of PHIs and they will be
+        // handled in consequent operations.
+        if (llvm::isa<PHINode>(Incoming))
+          continue;
+
+        llvm::BasicBlock *BB = PHI->getIncomingBlock(IncomingUse);
+        Builder.SetInsertPoint(BB->getTerminator());
+        auto *S = Builder.CreateStore(Incoming, Alloca);
+        revng_log(Log, dumpToString(S));
+      }
+    }
+  }
+
+  {
+    revng_log(Log, "Replacing Uses");
+    LoggerIndent IndentUses{ Log };
+
+    // Then, for all Uses whose Users are not also PHINodes we replace them with
+    // a load.
+    // All the uses that are PHIs are replaced with undef instead, and they will
+    // be cleaned up later.
+    for (auto *PHI : PHIs) {
+
+      revng_log(Log, "Use of PHI: " << dumpToString(PHI));
+      LoggerIndent IndentPHI{ Log };
+
+      Builder.SetInsertPoint(PHI);
+      auto *NewLoad = createLoad(Builder, Alloca);
+      revng_log(Log, "Create new load: " << dumpToString(NewLoad));
+
+      for (Use &U : llvm::make_early_inc_range(PHI->uses())) {
+        revng_log(Log, "in User: " << dumpToString(U.getUser()));
+
+        Value *NewOperand = nullptr;
+        if (isa<PHINode>(U.getUser()))
+          NewOperand = llvm::UndefValue::get(PHI->getType());
+        else
+          NewOperand = NewLoad;
+        revng_log(Log, "replaced with: " << dumpToString(NewOperand));
+        U.set(NewOperand);
+      }
+
+      if (not NewLoad->getNumUses()) {
+        revng_log(Log, "Erase new load since it has 0 uses");
+        NewLoad->eraseFromParent();
+      }
+    }
+  }
+
+  // Finally we remove all the PHIs
+  {
+    revng_log(Log, "Cleaning Up");
+    LoggerIndent IndentCleanup{ Log };
+    for (auto *PHI : PHIs) {
+      revng_log(Log, "Erasing: " << dumpToString(PHI));
+      PHI->eraseFromParent();
+    }
+  }
+}
+
 bool ExitSSAPass::runOnFunction(Function &F) {
 
   // Skip non-isolated functions
   if (not FunctionTags::Isolated.isTagOf(&F))
     return false;
 
-  IRBuilder<> Builder(F.getContext());
-  Builder.SetInsertPoint(&F.getEntryBlock().front());
+  bool Changed = false;
 
-  SmallMap<PHINode *, AllocaInst *, 8> PHIToAlloca;
-  for (BasicBlock &BB : F)
-    for (PHINode &PHI : BB.phis())
-      PHIToAlloca[&PHI] = Builder.CreateAlloca(PHI.getType());
+  // A vector containing sets of equivalence classes of PHINodes.
+  // Each equivalence class is composed of connected PHINodes that can form
+  // trees, a DAGs, or even loops.
+  // Informally, all the PHINodes in a group hold the same value, and we want to
+  // create a single local variable for each DAG.
+  const auto PHIClasses = getPHIEquivalenceClasses(F);
+  for (const auto &PHIGroup : PHIClasses)
+    replacePHIEquivalenceClass(PHIGroup, F);
+  Changed |= not PHIClasses.empty();
 
-  if (PHIToAlloca.empty())
-    return false;
-
-  for (auto &[PHI, Alloca] : PHIToAlloca) {
-    for (auto &IncomingUse : PHI->incoming_values()) {
-      llvm::BasicBlock *BB = PHI->getIncomingBlock(IncomingUse);
-      Builder.SetInsertPoint(BB->getTerminator());
-      Value *Incoming = IncomingUse.get();
-
-      BasicBlock *IncomingDefBB = &F.getEntryBlock();
-      if (auto *I = dyn_cast<Instruction>(Incoming))
-        IncomingDefBB = I->getParent();
-      revng_assert(IncomingDefBB);
-
-      revng_log(Log, "Incoming: " << dumpToString(Incoming));
-      auto *S = Builder.CreateStore(Incoming, PHIToAlloca.at(PHI));
-      revng_log(Log, dumpToString(S));
-    }
-  }
-
-  for (auto &[PHI, Alloca] : PHIToAlloca) {
-    Builder.SetInsertPoint(PHI);
-    auto *Load = createLoad(Builder, Alloca);
-    PHI->replaceAllUsesWith(Load);
-    PHI->eraseFromParent();
-  }
-
-  for (BasicBlock &BB : F)
-    for (Instruction &I : BB)
-      revng_assert(not llvm::isa<PHINode>(I));
-
-  return not PHIToAlloca.empty();
+  return Changed;
 }
 
 char ExitSSAPass::ID = 0;
