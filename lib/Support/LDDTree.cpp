b112d511543bca10b9c56cf8067f877d235cc23e
Alessandro Di Federico <ale@rev.ng>
2023-05-09 17:36:50 +0200

    LDDTree: rewrite findLibrary
    `findLibrary` has been rewritten to adhere more strictly to `man ld.so`.
    Also, `lddtree` no longer relies on sections headers.

--- lib/Support/LDDTree.cpp
+++ lib/Support/LDDTree.cpp
@@ -18,123 +18,106 @@
 #include "llvm/Support/Process.h"
 
 #include "revng/ADT/RecursiveCoroutine.h"
+#include "revng/ADT/STLExtras.h"
 #include "revng/Support/Debug.h"
 #include "revng/Support/LDDTree.h"
+#include "revng/Support/OverflowSafeInt.h"
 
 Logger<> Log("lddtree");
 
 using namespace llvm;
 
-static llvm::Expected<std::string>
-findInDir(llvm::StringRef Lib, llvm::StringRef WhereToLook) {
-  llvm::SmallString<128> ResultPath;
-  llvm::sys::path::append(ResultPath, WhereToLook, Lib);
-
-  if (llvm::sys::fs::exists(ResultPath.str()))
-    return std::string(ResultPath.str());
-
-  std::error_code EC;
-  llvm::sys::fs::directory_iterator DirIt(WhereToLook, EC);
-  llvm::sys::fs::directory_iterator DirEnd;
-  if (EC) {
-    revng_log(Log,
-              "Unable to find library: "
-                << toString(llvm::errorCodeToError(EC)));
-    return llvm::errorCodeToError(EC);
-  }
-  while (DirIt != DirEnd) {
-    ResultPath.clear();
-    llvm::sys::path::append(ResultPath, DirIt->path(), Lib);
-
-    DirIt.increment(EC);
-    if (EC) {
-      revng_log(Log,
-                "Unable to find library: "
-                  << toString(llvm::errorCodeToError(EC)));
-      return llvm::errorCodeToError(EC);
-    }
-
-    if (llvm::sys::fs::exists(ResultPath.str()))
-      return std::string(ResultPath.str());
-  }
-
-  revng_log(Log, "Unable to find " << Lib);
-  return std::string();
-}
-
+/// \see man ld.so
 static std::optional<std::string>
-findLibrary(llvm::StringRef Lib, std::optional<llvm::StringRef> RunPath) {
-  llvm::SmallString<128> LibraryPath;
+findLibrary(StringRef ToImport,
+            StringRef ImporterPath,
+            bool Is64,
+            bool NoDefault,
+            uint16_t EMachine,
+            std::optional<StringRef> RPath,
+            std::optional<StringRef> RunPath) {
+  revng_log(Log, "Looking for " << ToImport);
+  LoggerIndent<> Indent(Log);
 
-  // First process the `LD_LIBRARY_PATH`.
-  auto LDLibraryPath = llvm::sys::Process::GetEnv("LD_LIBRARY_PATH");
-  if (LDLibraryPath) {
-    StringRef TheLDLibraryPath(*LDLibraryPath);
-    SmallVector<StringRef, 16> LibraryPaths;
-    TheLDLibraryPath.split(LibraryPaths, ":");
-    for (auto Path : LibraryPaths) {
-      llvm::sys::path::append(LibraryPath, Path, Lib);
-      if (llvm::sys::fs::exists(LibraryPath))
-        return std::string(LibraryPath.str());
-      LibraryPath.clear();
+  SmallVector<std::string, 16> SearchPaths;
+
+  // Process DT_RPATH
+  if (RPath and RPath->size())
+    for (StringRef Path : split(*RPath, ":"))
+      SearchPaths.push_back(Path.str());
+
+  // Process the `LD_LIBRARY_PATH`
+  if (auto MaybeLibraryPath = llvm::sys::Process::GetEnv("LD_LIBRARY_PATH"))
+    for (StringRef Path : split(*MaybeLibraryPath, ":"))
+      SearchPaths.push_back(Path.str());
+
+  // Process DT_RUNPATH
+  std::string Origin = llvm::sys::path::parent_path(ImporterPath).str();
+  std::string LibName = Is64 ? "lib64" : "lib";
+  if (RunPath and RunPath->size()) {
+    for (StringRef Path : split(*RunPath, ":")) {
+      std::string PathString = Path.str();
+      replaceAll(PathString, "$ORIGIN", Origin);
+      replaceAll(PathString, "${ORIGIN}", Origin);
+      replaceAll(PathString, "$LIB", LibName);
+      replaceAll(PathString, "${LIB}", LibName);
+      // TODO: handle PLATFORM
+      SearchPaths.push_back(PathString);
     }
   }
 
-  // Process -rpath.
-  if (RunPath and (*RunPath).size()) {
-    SmallVector<StringRef, 16> LibraryRunPaths;
-    (*RunPath).split(LibraryRunPaths, ":");
-    for (auto Path : LibraryRunPaths) {
-      llvm::sys::path::append(LibraryPath, Path, Lib);
+  if (not NoDefault) {
+    SearchPaths.push_back("/" + LibName);
+    SearchPaths.push_back("/usr/" + LibName);
+  }
 
-      if (llvm::sys::fs::exists(LibraryPath))
-        return std::string(LibraryPath.str());
-      LibraryPath.clear();
+  for (std::string SearchPath : SearchPaths) {
+    SmallString<128> Candidate;
+    sys::path::append(Candidate, SearchPath, ToImport);
+
+    if (not sys::fs::exists(Candidate)) {
+      revng_log(Log, Candidate.str() << " does not exist");
+      continue;
     }
+
+    // Parse the binary
+    auto BinaryOrErr = object::createBinary(Candidate);
+    if (not BinaryOrErr) {
+      revng_log(Log,
+                "Can't create binary: " << toString(BinaryOrErr.takeError()));
+      llvm::consumeError(BinaryOrErr.takeError());
+      continue;
+    }
+
+    // Ensure it's an ELF
+    using namespace object;
+    auto *Elf = dyn_cast<ELFObjectFileBase>(BinaryOrErr->getBinary());
+    if (Elf == nullptr) {
+      revng_log(Log, "Found " << Candidate.str() << " but it's not an ELF.");
+      continue;
+    }
+
+    // Ensure it's the right machine
+    if (Elf->getEMachine() != EMachine) {
+      revng_log(Log,
+                "Found " << Candidate.str()
+                         << " but it has the wrong e_machine: "
+                         << Elf->getEMachine() << " (expected " << EMachine
+                         << ").");
+      continue;
+    }
+
+    revng_log(Log, "Found: " << Candidate.str());
+
+    return { Candidate.str().str() };
   }
 
-  // Try in /lib/.
-  auto LibraryAsCanonical = findInDir(Lib, "/lib/");
-  if (!LibraryAsCanonical) {
-    revng_log(Log,
-              "Unable to find library: "
-                << toString(LibraryAsCanonical.takeError()));
-    llvm::consumeError(LibraryAsCanonical.takeError());
-    return std::nullopt;
-  }
-
-  if ((*LibraryAsCanonical).size())
-    return *LibraryAsCanonical;
-
-  LibraryAsCanonical = findInDir(Lib, "/usr/lib/");
-  if (!LibraryAsCanonical) {
-    revng_log(Log,
-              "Unable to find library: "
-                << toString(LibraryAsCanonical.takeError()));
-    llvm::consumeError(LibraryAsCanonical.takeError());
-    return std::nullopt;
-  }
-
-  if ((*LibraryAsCanonical).size())
-    return *LibraryAsCanonical;
-
-  LibraryAsCanonical = findInDir(Lib, "/usr/local/lib/");
-  if (!LibraryAsCanonical) {
-    revng_log(Log,
-              "Unable to find library: "
-                << toString(LibraryAsCanonical.takeError()));
-    llvm::consumeError(LibraryAsCanonical.takeError());
-    return std::nullopt;
-  }
-
-  if ((*LibraryAsCanonical).size())
-    return *LibraryAsCanonical;
-
+  revng_log(Log, ToImport << " not found");
   return std::nullopt;
 }
 
 template<class ELFT>
-std::optional<llvm::StringRef>
+std::optional<StringRef>
 getDynamicString(const llvm::object::ELFFile<ELFT> &TheELF,
                  StringRef DynamicStringTable,
                  uint64_t Value) {
@@ -153,55 +136,134 @@ getDynamicString(const llvm::object::ELFFile<ELFT> &TheELF,
 }
 
 template<class ELFT>
-void lddtreeResolve(LDDTree &Dependencies,
-                    StringRef FileName,
-                    const llvm::object::ELFFile<ELFT> &TheELF) {
-  llvm::StringRef DynamicStringTable;
+static std::optional<StringRef>
+getDynamicStringTable(const ELFT &ELFObjectFile,
+                      typename ELFT::Elf_Dyn_Range &DynamicEntries) {
+  // Find address and size of .dynstr using dynamic entries
+  uint64_t DynstrAddress = 0;
+  uint64_t DynstrSize = 0;
+  for (const auto &DynamicTag : DynamicEntries) {
+    auto TheTag = DynamicTag.getTag();
+    auto TheVal = DynamicTag.getVal();
+    if (TheTag == llvm::ELF::DT_STRTAB) {
+      DynstrAddress = TheVal;
+    } else if (TheTag == llvm::ELF::DT_STRSZ) {
+      DynstrSize = TheVal;
+    }
+  }
 
-  using Elf_Shdr = const typename llvm::object::ELFFile<ELFT>::Elf_Shdr;
-  typename ELFT::ShdrRange Sections = cantFail(TheELF.sections());
-  for (const Elf_Shdr &Sec : Sections) {
-    if (Sec.sh_type == llvm::ELF::SHT_DYNSYM) {
-      if (auto E = TheELF.getStringTableForSymtab(Sec)) {
-        DynamicStringTable = *E;
-        break;
+  // Compute end address
+  auto MaybeEndAddress = OverflowSafeInt(DynstrAddress) + DynstrSize;
+  if (DynstrAddress == 0 or DynstrSize == 0 or not MaybeEndAddress)
+    return {};
+  uint64_t EndAddress = *MaybeEndAddress;
+
+  //
+  // Convert address to offset
+  //
+
+  // Collect program headers
+  auto ProgramHeadersOrError = ELFObjectFile.getELFFile().program_headers();
+  if (not ProgramHeadersOrError) {
+    revng_log(Log, "No valid program headers available");
+    llvm::consumeError(ProgramHeadersOrError.takeError());
+    return {};
+  }
+
+  // Find the correct program header
+  using Elf_Phdr = ELFT::Elf_Phdr;
+  for (const Elf_Phdr &Phdr : *ProgramHeadersOrError) {
+    if (Phdr.p_type != llvm::ELF::PT_LOAD)
+      continue;
+
+    uint64_t StartAddress = Phdr.p_vaddr;
+    uint64_t FileSize = Phdr.p_filesz;
+    auto SegmentEndAddress = OverflowSafeInt(StartAddress) + FileSize;
+    if (SegmentEndAddress and DynstrAddress >= Phdr.p_vaddr
+        and EndAddress <= *SegmentEndAddress) {
+      uint64_t SegmentStartOffset = Phdr.p_offset;
+      auto MaybeSegmentEndOffset = OverflowSafeInt(SegmentStartOffset)
+                                   + FileSize;
+      auto MaybeDynstrOffset = OverflowSafeInt(DynstrAddress) - StartAddress;
+      auto MaybeDynstrEnd = MaybeDynstrOffset + DynstrSize;
+
+      StringRef RawData = ELFObjectFile.getData();
+      if (MaybeSegmentEndOffset and MaybeDynstrOffset and MaybeDynstrEnd
+          and SegmentStartOffset <= *MaybeSegmentEndOffset
+          and SegmentStartOffset <= RawData.size()
+          and *MaybeSegmentEndOffset <= RawData.size()) {
+        return RawData.slice(SegmentStartOffset, *MaybeSegmentEndOffset)
+          .slice(*MaybeDynstrOffset, *MaybeDynstrEnd);
       }
     }
   }
 
-  auto DynamicEntries = TheELF.dynamicEntries();
-  if (!DynamicEntries)
+  return {};
+}
+
+template<class ELFT>
+void lddtreeResolve(LDDTree &Dependencies,
+                    StringRef FileName,
+                    const ELFT &ELFObjectFile) {
+  const auto &TheELF = ELFObjectFile.getELFFile();
+
+  auto MaybeDynamicEntries = TheELF.dynamicEntries();
+  if (not MaybeDynamicEntries) {
+    revng_log(Log, "No dynamic entries");
     return;
+  }
+  using Elf_Dyn_Range = ELFT::Elf_Dyn_Range;
+  Elf_Dyn_Range DynamicEntries = *MaybeDynamicEntries;
 
   std::optional<StringRef> RunPath;
+  std::optional<StringRef> RPath;
+  bool NoDefault = false;
 
-  using Elf_Dyn = const typename llvm::object::ELFFile<ELFT>::Elf_Dyn;
+  // Look for .dynstr
+  StringRef DynamicStringTable;
+  if (auto MaybeDynamicStringTable = getDynamicStringTable(ELFObjectFile,
+                                                           DynamicEntries))
+    DynamicStringTable = *MaybeDynamicStringTable;
 
-  // Find RUNPATH generated with -rpath.
-  for (Elf_Dyn &DynamicTag : *DynamicEntries) {
+  if (DynamicStringTable.empty()) {
+    revng_log(Log, "Cannot find .dynstr");
+    return;
+  }
+
+  // Look for DT_RPATH and DT_RUNPATH
+  using Elf_Dyn = ELFT::Elf_Dyn;
+  for (const Elf_Dyn &DynamicTag : DynamicEntries) {
     auto TheTag = DynamicTag.getTag();
     auto TheVal = DynamicTag.getVal();
     if (TheTag == llvm::ELF::DT_RUNPATH) {
-      RunPath = getDynamicString<ELFT>(TheELF, DynamicStringTable, TheVal);
-      if (RunPath)
-        revng_log(Log, "  RUNPATH: " << *RunPath);
-      else
-        revng_log(Log, "  Unable to parse RUNPATH");
-      break;
+      RunPath = getDynamicString(TheELF, DynamicStringTable, TheVal);
+    } else if (TheTag == llvm::ELF::DT_RPATH) {
+      RPath = getDynamicString(TheELF, DynamicStringTable, TheVal);
+    } else if (TheTag == llvm::ELF::DT_FLAGS_1) {
+      NoDefault = (TheVal & llvm::ELF::DF_1_NODEFLIB) != 0;
     }
   }
 
-  for (Elf_Dyn &DynamicTag : *DynamicEntries) {
+  bool Is64 = (std::is_same_v<ELFT, object::ELF64LEObjectFile>
+               or std::is_same_v<ELFT, object::ELF64BEObjectFile>);
+
+  for (const Elf_Dyn &DynamicTag : DynamicEntries) {
     auto TheTag = DynamicTag.getTag();
     auto TheVal = DynamicTag.getVal();
     if (TheTag == llvm::ELF::DT_NEEDED) {
-      auto LibName = getDynamicString<ELFT>(TheELF, DynamicStringTable, TheVal);
+      auto LibName = getDynamicString(TheELF, DynamicStringTable, TheVal);
       if (!LibName) {
-        revng_log(Log, "  Unable to parse needed library name");
+        revng_log(Log, "Unable to parse needed library name");
         continue;
       }
-      revng_log(Log, "  - needeed library: " << *LibName << "\n");
-      if (auto LocOfLib = findLibrary(*LibName, RunPath))
+
+      if (auto LocOfLib = findLibrary(*LibName,
+                                      FileName,
+                                      Is64,
+                                      NoDefault,
+                                      TheELF.getHeader().e_machine,
+                                      RPath,
+                                      RunPath))
         Dependencies[FileName.str()].push_back(*LocOfLib);
     }
   }
@@ -212,22 +274,26 @@ static RecursiveCoroutine<void> lddtreeHelper(LDDTree &Dependencies,
                                               unsigned CurrentLevel,
                                               unsigned DepthLevel) {
   revng_log(Log, "lddtree for " << Path << "\n");
-  auto BinaryOrErr = object::createBinary(Path);
+  LoggerIndent<> Ident(Log);
+
+  using namespace object;
+  auto BinaryOrErr = createBinary(Path);
   if (not BinaryOrErr) {
     revng_log(Log,
               "Can't create binary: " << toString(BinaryOrErr.takeError()));
     llvm::consumeError(BinaryOrErr.takeError());
     rc_return;
   }
-  auto &Obj = *cast<object::ObjectFile>(BinaryOrErr->getBinary());
-  if (auto *ELFObj = dyn_cast<object::ELF32LEObjectFile>(&Obj))
-    lddtreeResolve(Dependencies, Path, ELFObj->getELFFile());
-  else if (auto *ELFObj = dyn_cast<object::ELF32BEObjectFile>(&Obj))
-    lddtreeResolve(Dependencies, Path, ELFObj->getELFFile());
-  else if (auto *ELFObj = dyn_cast<object::ELF64LEObjectFile>(&Obj))
-    lddtreeResolve(Dependencies, Path, ELFObj->getELFFile());
-  else if (auto *ELFObj = dyn_cast<object::ELF64BEObjectFile>(&Obj))
-    lddtreeResolve(Dependencies, Path, ELFObj->getELFFile());
+
+  auto *Binary = BinaryOrErr->getBinary();
+  if (auto *ELFObjectFile = dyn_cast<ELF32LEObjectFile>(Binary))
+    lddtreeResolve(Dependencies, Path, *ELFObjectFile);
+  else if (auto *ELFObjectFile = dyn_cast<ELF32BEObjectFile>(Binary))
+    lddtreeResolve(Dependencies, Path, *ELFObjectFile);
+  else if (auto *ELFObjectFile = dyn_cast<ELF64LEObjectFile>(Binary))
+    lddtreeResolve(Dependencies, Path, *ELFObjectFile);
+  else if (auto *ELFObjectFile = dyn_cast<ELF64BEObjectFile>(Binary))
+    lddtreeResolve(Dependencies, Path, *ELFObjectFile);
   else
     revng_log(Log, "Not an ELF.");
 
