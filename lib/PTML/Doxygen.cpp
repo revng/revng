ed4efb0bd972eecc24c50d14aa8736b999600790
Ivan Krysak <ivan@rev.ng>
2023-05-26 17:46:36 +0300

    yield::ptml: add `functionComment` function

--- lib/PTML/Doxygen.cpp
+++ lib/PTML/Doxygen.cpp
@@ -5,9 +5,12 @@
 /// \file Doxygen.cpp
 /// \brief
 
+#include "revng/ABI/FunctionType/Layout.h"
 #include "revng/Model/Binary.h"
 #include "revng/PTML/Constants.h"
 #include "revng/PTML/Doxygen.h"
+#include "revng/Pipeline/Location.h"
+#include "revng/Pipes/Ranks.h"
 
 namespace ptml::tokens {
 
@@ -248,4 +251,249 @@ std::string ptml::freeFormComment(const ::ptml::PTMLBuilder &PTML,
   CommentBuilder Builder(PTML, CommentIndicator, Indentation, WrapAt);
   auto Result = Builder.emit(Text.str());
   return Result.empty() ? Result : '\n' + Result;
+}
+
+using pipeline::serializedLocation;
+namespace ranks = revng::ranks;
+
+static llvm::SmallVector<DoxygenLine, 16>
+gatherArgumentComments(const model::Function &Function) {
+  llvm::SmallVector<DoxygenLine, 16> Result;
+
+  static constexpr std::string_view Keyword = "\\param ";
+
+  const model::Type *Prototype = Function.Prototype().get();
+  if (auto *FT = llvm::dyn_cast<model::CABIFunctionType>(Prototype)) {
+    abi::FunctionType::Layout Layout(*FT);
+
+    std::size_t IndOffset = Layout.returnsAggregateType() ? 1 : 0;
+    revng_assert(FT->Arguments().size() + IndOffset == Layout.Arguments.size());
+    for (std::size_t Index = 0; Index < FT->Arguments().size(); ++Index) {
+      DoxygenLine &Line = Result.emplace_back();
+      Line->emplace_back(DoxygenToken::Types::Keyword, std::string(Keyword));
+      Line.InternalIndentation = Keyword.size();
+
+      const model::Argument &Argument = FT->Arguments().at(Index);
+      auto &Name = Line->emplace_back(DoxygenToken::Types::Identifier,
+                                      Argument.name().str().str());
+      Name.ExtraAttributes.emplace_back(ptml::attributes::ModelEditPath,
+                                        model::editPath::customName(*FT,
+                                                                    Argument));
+      Name.ExtraAttributes.emplace_back(ptml::attributes::LocationReferences,
+                                        serializedLocation(ranks::CABIArgument,
+                                                           FT->key(),
+                                                           Argument.key()));
+
+      const auto CurrentArgument = Layout.Arguments[Index + IndOffset];
+      if (!CurrentArgument.Registers.empty()) {
+        Line->emplace_back(DoxygenToken::Types::Untagged, " (in ");
+        for (auto Register : skip_back(CurrentArgument.Registers)) {
+          Line->emplace_back(DoxygenToken::Types::Identifier,
+                             model::Register::getRegisterName(Register).str());
+          Line->emplace_back(DoxygenToken::Types::Untagged, ", ");
+        }
+        auto Last = CurrentArgument.Registers.back();
+        Line->emplace_back(DoxygenToken::Types::Identifier,
+                           model::Register::getRegisterName(Last).str());
+        Line->emplace_back(DoxygenToken::Types::Untagged, ") ");
+      } else {
+        revng_assert(CurrentArgument.Stack && CurrentArgument.Stack->Size != 0);
+        Line->emplace_back(DoxygenToken::Types::Untagged, " (on the stack) ");
+      }
+
+      auto &Tag = Line->emplace_back(DoxygenToken::Types::Untagged,
+                                     FT->Arguments().at(Index).Comment());
+      Tag.ExtraAttributes.emplace_back(ptml::attributes::ModelEditPath,
+                                       model::editPath::comment(*FT, Argument));
+    }
+  } else if (auto *FT = llvm::dyn_cast<model::RawFunctionType>(Prototype)) {
+    for (const model::NamedTypedRegister &Argument : FT->Arguments()) {
+      model::Register::Values Register = Argument.Location();
+
+      DoxygenLine &Line = Result.emplace_back();
+      Line->emplace_back(DoxygenToken::Types::Keyword, std::string(Keyword));
+      Line.InternalIndentation = Keyword.size();
+
+      auto &Name = Line->emplace_back(DoxygenToken::Types::Identifier,
+                                      Argument.name().str().str());
+      Name.ExtraAttributes.emplace_back(ptml::attributes::ModelEditPath,
+                                        model::editPath::customName(*FT,
+                                                                    Argument));
+      Name.ExtraAttributes.emplace_back(ptml::attributes::LocationReferences,
+                                        serializedLocation(ranks::RawArgument,
+                                                           FT->key(),
+                                                           Argument.key()));
+      Line->emplace_back(DoxygenToken::Types::Untagged, " (in ");
+      Line->emplace_back(DoxygenToken::Types::Identifier,
+                         model::Register::getRegisterName(Register).str());
+      Line->emplace_back(DoxygenToken::Types::Untagged, ") ");
+      auto &Tag = Line->emplace_back(DoxygenToken::Types::Untagged,
+                                     Argument.Comment());
+      Tag.ExtraAttributes.emplace_back(ptml::attributes::ModelEditPath,
+                                       model::editPath::comment(*FT, Argument));
+    }
+
+    if (FT->StackArgumentsType().UnqualifiedType().isValid()) {
+      revng_assert(FT->StackArgumentsType().Qualifiers().empty());
+      auto *Unqualified = FT->StackArgumentsType().UnqualifiedType().get();
+      const auto &Stack = *llvm::cast<model::StructType>(Unqualified);
+
+      struct FieldMapEntry {
+        std::string Name;
+        llvm::StringRef Comment;
+        const model::StructField &Field;
+      };
+      llvm::SmallVector<FieldMapEntry> Comments;
+      for (const model::StructField &Field : Stack.Fields())
+        if (!Field.Comment().empty())
+          Comments.emplace_back(Field.name().str().str(),
+                                Field.Comment(),
+                                Field);
+
+      if (!Comments.empty()) {
+        static constexpr std::string_view FirstLine = "stack_args ";
+
+        bool IsFirst = true;
+        for (auto [Name, Comment, Field] : Comments) {
+          DoxygenLine &Line = Result.emplace_back();
+          if (IsFirst) {
+            Line->emplace_back(DoxygenToken::Types::Keyword,
+                               std::string(Keyword));
+            Line.InternalIndentation = Keyword.size();
+
+            Line->emplace_back(DoxygenToken::Types::Untagged,
+                               std::string(FirstLine));
+            Line.InternalIndentation += FirstLine.size();
+
+            IsFirst = false;
+          } else {
+            Line.InternalIndentation = Keyword.size() + FirstLine.size();
+            Line->emplace_back(DoxygenToken::Types::Indentation,
+                               std::string(Line.InternalIndentation, ' '));
+          }
+
+          auto &L = Line->emplace_back(DoxygenToken::Types::Identifier, Name);
+          L.ExtraAttributes.emplace_back(ptml::attributes::ModelEditPath,
+                                         model::editPath::customName(Stack,
+                                                                     Field));
+          L.ExtraAttributes.emplace_back(ptml::attributes::LocationReferences,
+                                         serializedLocation(ranks::StructField,
+                                                            Stack.key(),
+                                                            Field.key()));
+
+          auto &Tag = Line->emplace_back(DoxygenToken::Types::Untagged,
+                                         ": " + Comment.str());
+          Tag.ExtraAttributes.emplace_back(ptml::attributes::ModelEditPath,
+                                           model::editPath::comment(Stack,
+                                                                    Field));
+        }
+      }
+    }
+  } else {
+    revng_abort("Function prototype must be a function type.");
+  }
+
+  return Result;
+}
+
+static llvm::SmallVector<DoxygenLine, 16>
+gatherReturnValueComments(const model::Function &Function) {
+  llvm::SmallVector<DoxygenLine, 16> Result;
+
+  static constexpr std::string_view Keyword = "\\returns ";
+
+  const model::Type *Prototype = Function.Prototype().get();
+  if (auto *F = llvm::dyn_cast<model::CABIFunctionType>(Prototype)) {
+    if (!F->ReturnValueComment().empty()) {
+      DoxygenLine &Line = Result.emplace_back();
+      Line->emplace_back(DoxygenToken::Types::Keyword, std::string(Keyword));
+      Line.InternalIndentation = Keyword.size();
+
+      DoxygenToken &Tag = Line->emplace_back(DoxygenToken::Types::Untagged,
+                                             F->ReturnValueComment());
+      Tag.ExtraAttributes.emplace_back(ptml::attributes::ModelEditPath,
+                                       model::editPath::returnValueComment(*F));
+    }
+  } else if (auto *FT = llvm::dyn_cast<model::RawFunctionType>(Prototype)) {
+    if (!FT->ReturnValueComment().empty()) {
+      DoxygenLine &Line = Result.emplace_back();
+      Line->emplace_back(DoxygenToken::Types::Keyword, std::string(Keyword));
+      Line.InternalIndentation = Keyword.size();
+
+      DoxygenToken &T = Line->emplace_back(DoxygenToken::Types::Untagged,
+                                           FT->ReturnValueComment());
+      T.ExtraAttributes.emplace_back(ptml::attributes::ModelEditPath,
+                                     model::editPath::returnValueComment(*FT));
+    }
+
+    for (const model::TypedRegister &ReturnValue : FT->ReturnValues()) {
+      model::Register::Values Register = ReturnValue.Location();
+
+      if (!ReturnValue.Comment().empty()) {
+        if (Result.empty())
+          Result.emplace_back();
+
+        auto &Line = Result.emplace_back();
+        Line.InternalIndentation = Keyword.size();
+
+        if (Result.size() == 1) {
+          Line->emplace_back(DoxygenToken::Types::Keyword,
+                             std::string(Keyword));
+        } else {
+          Line->emplace_back(DoxygenToken::Types::Indentation,
+                             std::string(Keyword.size(), ' '));
+        }
+
+        Line->emplace_back(DoxygenToken::Types::Identifier,
+                           model::Register::getRegisterName(Register).str());
+        auto &Tag = Line->emplace_back(DoxygenToken::Types::Untagged,
+                                       ": " + ReturnValue.Comment());
+        Tag.ExtraAttributes.emplace_back(ptml::attributes::ModelEditPath,
+                                         model::editPath::comment(*FT,
+                                                                  ReturnValue));
+      }
+    }
+
+  } else {
+    revng_abort("Function prototype must be a function type.");
+  }
+
+  return Result;
+}
+
+std::string ptml::functionComment(const ::ptml::PTMLBuilder &PTML,
+                                  const model::Function &Function,
+                                  const model::Binary &Binary,
+                                  llvm::StringRef CommentIndicator,
+                                  std::size_t Indentation,
+                                  std::size_t WrapAt) {
+  llvm::SmallVector<DoxygenLine, 16> Result;
+  if (!Function.Comment().empty()) {
+    DoxygenToken Tag{ .Type = DoxygenToken::Types::Untagged,
+                      .Value = Function.Comment() };
+    Tag.ExtraAttributes.emplace_back(::ptml::attributes::ModelEditPath,
+                                     model::editPath::comment(Function));
+    Result.emplace_back(DoxygenLine{ .Tags = { std::move(Tag) } });
+  }
+
+  auto ArgumentComments = gatherArgumentComments(Function);
+  if (!ArgumentComments.empty()) {
+    if (!Result.empty())
+      Result.emplace_back();
+
+    Result.reserve(Result.size() + ArgumentComments.size());
+    std::ranges::move(ArgumentComments, std::back_inserter(Result));
+  }
+
+  auto ReturnValueComments = gatherReturnValueComments(Function);
+  if (!ReturnValueComments.empty()) {
+    if (!Result.empty())
+      Result.emplace_back();
+
+    Result.reserve(Result.size() + ReturnValueComments.size());
+    std::ranges::move(ReturnValueComments, std::back_inserter(Result));
+  }
+
+  CommentBuilder Builder(PTML, CommentIndicator, Indentation, WrapAt);
+  return Builder.emit(std::move(Result));
 }
