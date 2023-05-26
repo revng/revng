62a37ba6f476d284cd789328e4c5fdbd7a9239c1
Ivan Krysak <ivan@rev.ng>
2023-05-26 17:46:36 +0300

    yield::ptml: add `functionComment` function

--- include/revng/PTML/Doxygen.h
+++ include/revng/PTML/Doxygen.h
@@ -30,4 +30,30 @@ std::string comment(const ::ptml::PTMLBuilder &Builder,
   return freeFormComment(Builder, V.Comment(), Indicator, Indent, WrapAt);
 }
 
+/// Emits PTML containing a comment for a function constructed based on
+/// the model representation of both the function and its type.
+///
+/// \param Function the function to gather comment information from.
+/// \param Binary the model of the binary the function belongs to.
+/// \param CommentIndicator the indicator used at the start of commented lines.
+///        If no custom value is specified, the value obtained from
+///        `model::Architecture::getAssemblyCommentIndicator` is used instead,
+///        which is the preferred option for the assembly (see the inline
+///        overload calling this function).
+///        For C code, the recommended indicator is either `'///'` or `'  *'`.
+///        With second option, the opening (`'/**'`) and closing (`' */'`)
+///        brackets are not provided by this function and need to be added
+///        by the caller.
+/// \param WrapAt the expected width of a line within the emitted comment.
+/// \param Indentation the number of spaces appended **before** the comment as
+///        an indentation token (see `ptml::tokens::Indentation`).
+///
+/// \returns a serialized PTML string containing the comment.
+std::string functionComment(const ::ptml::PTMLBuilder &ThePTMLBuilder,
+                            const model::Function &Function,
+                            const model::Binary &Binary,
+                            llvm::StringRef CommentIndicator,
+                            std::size_t Indentation,
+                            std::size_t WrapAt);
+
 } // namespace ptml
