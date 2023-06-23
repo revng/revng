559cc7d699eb56d2769fedd38283e718a9077f5e
Alessandro Di Federico <ale@rev.ng>
2023-06-23 18:05:41 +0200

    HoistStructPhis: various improvements
    HoistStructPhis now:
    
    1. Ignores calls to isolated functions
    2. Works only on isolated functions
    3. Drop the old calls which were otherwise surviving

--- lib/IRCanonicalization/HoistStructPhis.cpp
+++ lib/IRCanonicalization/HoistStructPhis.cpp
@@ -5,6 +5,7 @@
 #include "llvm/Passes/PassBuilder.h"
 
 #include "revng/Support/Debug.h"
+#include "revng/Support/FunctionTags.h"
 
 using namespace llvm;
 
@@ -21,6 +22,9 @@ public:
   HoistStructPhis() : llvm::FunctionPass(ID) {}
 
   bool runOnFunction(llvm::Function &F) override {
+    if (not FunctionTags::Isolated.isTagOf(&F))
+      return false;
+
     llvm::SmallVector<PHINode *, 16> ToFix;
 
     // Collect phis that need fixing
@@ -33,60 +37,71 @@ public:
     if (ToFix.size() == 0)
       return false;
 
-    for (PHINode *Phi : ToFix) {
-      auto PhiSize = Phi->getNumIncomingValues();
-
-      // Create all the phis
-      CallInst *FirstCall = nullptr;
-      Value *CalledValue = nullptr;
-      llvm::SmallVector<Value *, 2> Phis;
-      for (auto [V, Predecessor] : zip(Phi->incoming_values(), Phi->blocks())) {
-        // We only expect pure calls
-        auto *Call = cast<CallInst>(V);
-
-        if (CalledValue == nullptr) {
-          // This is the first call we see, make some extra checks
-          CalledValue = Call->getCalledOperand();
-          FirstCall = Call;
-
-          Function *Callee = Call->getCalledFunction();
-          revng_assert(Callee != nullptr);
-
-          revng_assert(isLastBeforeTerminator(Call)
-                       or Callee->onlyReadsMemory());
-
-          // First iteration, create phis
-          for (Type *ArgumentType : Call->getFunctionType()->params())
-            Phis.push_back(PHINode::Create(ArgumentType, PhiSize, "", Phi));
-
-        } else {
-          // Ensure all the incomings are calls to the same function
-          revng_assert(CalledValue == Call->getCalledOperand());
-        }
-
-        // Add an incoming
-        revng_assert(Call->arg_size() == Phis.size());
-        for (auto [Argument, Phi] : zip(Call->args(), Phis))
-          cast<PHINode>(Phi)->addIncoming(Argument, Predecessor);
-      }
-
-      // Create a new function call
-      Instruction *InsertionPoint = Phi->getParent()->getFirstNonPHI();
-      auto *NewCall = CallInst::Create({ FirstCall->getFunctionType(),
-                                         CalledValue },
-                                       ArrayRef<Value *>(Phis),
-                                       ArrayRef<OperandBundleDef>{},
-                                       "",
-                                       InsertionPoint);
-
-      // Steal metadata and replace original phi
-      NewCall->copyMetadata(*FirstCall);
-      revng_assert(NewCall->getType() == Phi->getType());
-      Phi->replaceAllUsesWith(NewCall);
-      Phi->eraseFromParent();
-    }
+    for (PHINode *Phi : ToFix)
+      handlePhi(Phi);
 
     return true;
+  }
+
+  void handlePhi(PHINode *Phi) {
+    auto PhiSize = Phi->getNumIncomingValues();
+
+    // Create all the phis
+    CallInst *FirstCall = nullptr;
+    Value *CalledValue = nullptr;
+    llvm::SmallVector<Value *, 2> Phis;
+    llvm::SmallVector<CallInst *, 2> Calls;
+    for (auto [V, Predecessor] : zip(Phi->incoming_values(), Phi->blocks())) {
+      // We only expect pure calls
+      auto *Call = cast<CallInst>(V);
+      Calls.push_back(Call);
+
+      if (CalledValue == nullptr) {
+        // This is the first call we see, make some extra checks
+        CalledValue = Call->getCalledOperand();
+        FirstCall = Call;
+
+        Function *Callee = Call->getCalledFunction();
+        revng_assert(Callee != nullptr);
+
+        revng_assert(isLastBeforeTerminator(Call) or Callee->onlyReadsMemory());
+
+        // Ignore isolated functions
+        if (FunctionTags::Isolated.isTagOf(Callee))
+          return;
+
+        // First iteration, create phis
+        for (Type *ArgumentType : Call->getFunctionType()->params())
+          Phis.push_back(PHINode::Create(ArgumentType, PhiSize, "", Phi));
+
+      } else {
+        // Ensure all the incomings are calls to the same function
+        revng_assert(CalledValue == Call->getCalledOperand());
+      }
+
+      // Add an incoming
+      revng_assert(Call->arg_size() == Phis.size());
+      for (auto [Argument, Phi] : zip(Call->args(), Phis))
+        cast<PHINode>(Phi)->addIncoming(Argument, Predecessor);
+    }
+
+    // Create a new function call
+    Instruction *InsertionPoint = Phi->getParent()->getFirstNonPHI();
+    auto *NewCall = CallInst::Create({ FirstCall->getFunctionType(),
+                                       CalledValue },
+                                     ArrayRef<Value *>(Phis),
+                                     ArrayRef<OperandBundleDef>{},
+                                     "",
+                                     InsertionPoint);
+
+    // Steal metadata and replace original phi
+    NewCall->copyMetadata(*FirstCall);
+    revng_assert(NewCall->getType() == Phi->getType());
+    Phi->replaceAllUsesWith(NewCall);
+    Phi->eraseFromParent();
+
+    for (CallInst *Call : Calls)
+      Call->eraseFromParent();
   }
 };
 
