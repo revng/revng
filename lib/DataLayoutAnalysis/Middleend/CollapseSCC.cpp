5c132e795643dfd18e39f5d778894c227c1dfb4d
Pietro Fezzardi <pietro@rev.ng>
2023-08-17 12:08:01 +0200

    DLA: CollapseSCC preserves NonScalar nodes

--- lib/DataLayoutAnalysis/Middleend/CollapseSCC.cpp
+++ lib/DataLayoutAnalysis/Middleend/CollapseSCC.cpp
@@ -8,6 +8,8 @@
 
 #include "llvm/ADT/GraphTraits.h"
 #include "llvm/ADT/SCCIterator.h"
+#include "llvm/ADT/STLExtras.h"
+#include "llvm/ADT/SetVector.h"
 
 #include "revng/ADT/FilteredGraphTraits.h"
 #include "revng/Support/Debug.h"
@@ -30,6 +32,10 @@ using InstanceNodeT = EdgeFilteredGraph<GraphNodeT, isInstanceEdge>;
 using EqualityNodeT = EdgeFilteredGraph<GraphNodeT, isEqualityEdge>;
 using InstanceOffset0EdgeT = EdgeFilteredGraph<GraphNodeT, isInstanceOff0>;
 using NonPointerNodeT = EdgeFilteredGraph<GraphNodeT, isNotPointerEdge>;
+
+static bool sizeGreater(const LTSN *LHS, const LTSN *RHS) {
+  return LHS->Size > RHS->Size;
+}
 
 template<typename NodeT>
 static bool collapseSCCs(LayoutTypeSystem &TS) {
@@ -76,7 +82,86 @@ static bool collapseSCCs(LayoutTypeSystem &TS) {
 
   for (scc_t &SCC : ToCollapse) {
     revng_log(LogVerbose, "# SCC with " << SCC.size() << " elements");
-    TS.mergeNodes(SCC);
+    revng_assert(not SCC.empty());
+    // Reorder the SCC so that nodes with the same size are adjacent.
+    // Nodes with larger sizes come first.
+    llvm::stable_sort(SCC, sizeGreater);
+
+    // Now, merge all the nodes that have the same size, while noting the
+    // leaders.
+    llvm::SmallSetVector<LayoutTypeSystemNode *, 8> Leaders;
+    {
+      auto LeaderIt = SCC.begin();
+      auto CurrentSize = (*LeaderIt)->Size;
+      auto End = SCC.end();
+
+      while (LeaderIt != End) {
+
+        Leaders.insert(*LeaderIt);
+
+        // Find the first node with a size different from CurrentSize.
+        auto EndCurrentSize = std::next(LeaderIt);
+        while (EndCurrentSize != End and (*EndCurrentSize)->Size == CurrentSize)
+          ++EndCurrentSize;
+
+        // Merge all the nodes with the same size.
+        TS.mergeNodes({ &*LeaderIt, &*EndCurrentSize });
+
+        // Go to the next size.
+        LeaderIt = EndCurrentSize;
+      }
+    }
+
+    // Now, if the last Leader has Size 0, we merge it with the first Leader,
+    // which represents the largest type.
+    if (Leaders.size() > 1 and Leaders.back()->Size == 0) {
+      TS.mergeNodes(llvm::SmallVector{ Leaders.front(), Leaders.back() });
+      Leaders.pop_back();
+    }
+
+    // If a single leader is left, we're done, all the nodes have been merged
+    // together.
+    if (Leaders.size() < 2)
+      continue;
+
+    // If we're left with more than one leader, we just want to remove all edges
+    // between them and replace them with offset-at-0 edges, from the larger
+    // leader to the smaller.
+    auto LeaderIt = Leaders.begin();
+    auto LeaderEnd = Leaders.end();
+    for (; LeaderIt != LeaderEnd; ++LeaderIt) {
+
+      // For each leader we erase all the non-pointer edges going to other
+      // leaders.
+      auto *Leader = *LeaderIt;
+      using DLAGraph = llvm::GraphTraits<LayoutTypeSystemNode *>;
+      auto EdgeIt = DLAGraph::child_edge_begin(Leader);
+      auto EdgeEnd = DLAGraph::child_edge_end(Leader);
+      auto EdgeNext = DLAGraph::child_edge_end(Leader);
+
+      for (; EdgeIt != EdgeEnd; EdgeIt = EdgeNext) {
+
+        EdgeNext = std::next(EdgeIt);
+        auto &Edge = *EdgeIt;
+
+        // Skip pointers
+        if (isPointerEdge(Edge))
+          continue;
+
+        const auto &[Child, Tag] = Edge;
+
+        // Skip edges that go to non-leaders
+        if (not Leaders.contains(Child))
+          continue;
+
+        TS.eraseEdge(Leader, EdgeIt);
+      }
+
+      // Then, for each leader, we add an instance-at-offset-0 edge to the next
+      // leader, which is the one with immediately lower size.
+      if (auto NextLeaderIt = std::next(LeaderIt); NextLeaderIt != LeaderEnd)
+        TS.addInstanceLink(*LeaderIt, *NextLeaderIt, dla::OffsetExpression{});
+    }
   }
 
   return ToCollapse.size();
