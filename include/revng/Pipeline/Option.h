9886a9bcab555305d1d7d7929f04a89bcf3bfe12
Giacomo Vercesi <giacomo.vercesi@rev.ng>
2023-08-25 16:52:15 +0200

    pipeline::Option: fix TypeName
    Fix the `getTypeName` function needed to report the type of an Analysis'
    option.

--- include/revng/Pipeline/Option.h
+++ include/revng/Pipeline/Option.h
@@ -39,6 +39,24 @@ llvm::StringRef getOptionName() {
   return getOptionInfo<Invokable, Index>().Name;
 }
 
+template<typename T>
+llvm::StringRef typeNameImpl();
+
+template<>
+inline llvm::StringRef typeNameImpl<std::string>() {
+  return "string";
+}
+
+template<>
+inline llvm::StringRef typeNameImpl<int>() {
+  return "int";
+}
+
+template<>
+inline llvm::StringRef typeNameImpl<uint64_t>() {
+  return "uint64_t";
+}
+
 template<typename T, size_t Index>
 using ArgTypeImpl = std::decay_t<decltype(getOptionInfo<T, Index>())>;
 
@@ -57,12 +75,13 @@ using OptionType = std::conditional_t<IsConstCharPtr<InvokableType, Index>,
 
 template<typename Invokable, size_t Index>
 llvm::StringRef getTypeName() {
-  return typeNameImpl<ArgTypeImpl<Invokable, Index>>();
+  return typeNameImpl<OptionType<Invokable, Index>>();
 }
 
 template<typename InvokableType, size_t Index>
 OptionType<InvokableType, Index> getOptionDefault() {
   return getOptionInfo<InvokableType, Index>().Default;
 }
+
 } // namespace detail
 } // namespace pipeline
