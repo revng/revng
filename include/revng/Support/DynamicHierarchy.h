6addadcda7c02456d43de09aceb38117cac2efe6
Pietro Fezzardi <pietro@rev.ng>
2023-08-28 14:02:35 +0200

    DynamicHierarchy.h: drop unused include

--- include/revng/Support/DynamicHierarchy.h
+++ include/revng/Support/DynamicHierarchy.h
@@ -10,7 +10,6 @@
 #include "llvm/ADT/ArrayRef.h"
 #include "llvm/ADT/StringRef.h"
 #include "llvm/Support/Debug.h"
-#include "llvm/Support/ManagedStatic.h"
 #include "llvm/Support/raw_ostream.h"
 
 #include "revng/Support/Debug.h"
