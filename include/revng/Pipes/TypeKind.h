11938e40da0342dbbf509dd268ea55dfec019625
Giacomo Vercesi <giacomo.vercesi@rev.ng>
2023-12-05 11:32:44 +0100

    Introduce `TypeStringMap`
    Building upon the previous commit, introduce `TypeStringMap` which is a
    container for type-wise strings.

---
+++ include/revng/Pipes/TypeKind.h
@@ -0,0 +1,28 @@
+#pragma once
+
+//
+// This file is distributed under the MIT License. See LICENSE.md for details.
+//
+
+#include "revng/Model/Binary.h"
+#include "revng/Pipeline/Kind.h"
+#include "revng/Pipeline/RegisterKind.h"
+#include "revng/Pipeline/Target.h"
+#include "revng/Pipes/ModelGlobal.h"
+
+namespace revng::kinds {
+
+class TypeKind : public pipeline::Kind {
+public:
+  using pipeline::Kind::Kind;
+  void appendAllTargets(const pipeline::Context &Ctx,
+                        pipeline::TargetsList &Out) const override {
+    using namespace pipeline;
+    const auto &Model = getModelFromContext(Ctx);
+    for (const auto &Type : Model->Types()) {
+      Out.push_back(Target(serializeToString(Type->key()), *this));
+    }
+  }
+};
+
+} // namespace revng::kinds
