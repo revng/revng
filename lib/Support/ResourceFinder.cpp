06f61370040f66bcdb97ed0fcc62f88e574737ee
Giacomo Vercesi <giacomo.vercesi@rev.ng>
2023-12-18 09:21:58 +0100

    ResourceFinder: implement getComponentsHash
    Implement the `getComponentsHash` function which returns a hash string
    for all the components currently installed in revng.

--- lib/Support/ResourceFinder.cpp
+++ lib/Support/ResourceFinder.cpp
@@ -2,8 +2,12 @@
 // This file is distributed under the MIT License. See LICENSE.md for details.
 //
 
+#include "llvm/ADT/STLExtras.h"
+#include "llvm/Support/Error.h"
+#include "llvm/Support/MemoryBuffer.h"
 #include "llvm/Support/Path.h"
 
+#include "revng/Support/Assert.h"
 #include "revng/Support/ResourceFinder.h"
 
 namespace revng {
@@ -16,5 +20,21 @@ PathList ResourceFinder({
 #endif
 
 });
+
+std::string getComponentsHash() {
+  std::string Directory = "share/revng/component-hashes";
+  std::vector<std::string> Files = ResourceFinder.list(Directory, "");
+  llvm::sort(Files);
+
+  std::string Result;
+  for (std::string &File : Files) {
+    auto MaybeBuffer = llvm::MemoryBuffer::getFile(File);
+    revng_assert(MaybeBuffer);
+    llvm::StringRef Contents = MaybeBuffer->get()->getBuffer().trim();
+    Result.append(Contents.begin(), Contents.end());
+  }
+
+  return Result;
+}
 
 } // namespace revng
