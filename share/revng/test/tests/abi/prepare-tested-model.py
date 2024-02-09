34b46239d1de9cd24d4782b1aef38b705bbcfdb4
Ivan Krysak <ivan@rev.ng>
2024-02-09 09:43:43 +0100

    ABI-tests: strip model of irrelevant functions

--- share/revng/test/tests/abi/prepare-tested-model.py
+++ share/revng/test/tests/abi/prepare-tested-model.py
@@ -5,9 +5,11 @@
 #
 
 # This script is used for model preparation for the tests using runtime-abi
-# artifacts. It ensures that the prototypes of all the functions have their
-# ABI set correctly to work around the limitations of ABI detection when
-# importing from dwarf.
+# artifacts. It removes all the function not explicitly relevant to the test
+# so only `test_X` function plus a couple of helpers (like `main` and `memcpy`)
+# remain. After which, it ensures that the prototypes of all the remaining
+# functions have their ABI set correctly to work around the limitations of
+# ABI detection when importing from dwarf.
 
 import argparse
 import sys
@@ -26,6 +28,24 @@ def main():
 
     with open(args.input_model_path, "r") as f:
         binary = yaml.load(f, Loader=model.YamlLoader)
+
+    #
+    # Remove any function that is not explicitly relevant to the test.
+    #
+
+    def can_drop(function):
+        return not (
+            function.OriginalName.startswith("test_")
+            or function.OriginalName == "main"
+            or function.OriginalName == "memcpy"
+        )
+
+    for function in [f for f in binary.Functions if can_drop(f)]:
+        binary.Functions.remove(function)
+
+    #
+    # Ensure the ABI of all remaining functions is what we expect it to be.
+    #
 
     prototypes_to_fix = [str(f.Prototype) for f in binary.Functions if f.Prototype.is_valid()]
     for current_type in binary.Types:
