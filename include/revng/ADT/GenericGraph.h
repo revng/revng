6a1018b8604f69ecdf5abb8bf3169ca23d6d288d
Ivan Krysak <ivan@rev.ng>
2024-02-21 08:12:50 +0100

    Remove unused linter suppressors

--- include/revng/ADT/GenericGraph.h
+++ include/revng/ADT/GenericGraph.h
@@ -365,7 +365,6 @@ class BidirectionalNode
                                       SmallSize,
                                       BidirectionalNode>::Result {
 public:
-  // NOLINTNEXTLINE
   static const bool is_bidirectional_node = true;
 
 public:
@@ -1298,7 +1297,6 @@ template<typename NodeT, size_t SmallSize, bool HasEntryNode>
 class GenericGraph
   : public std::conditional_t<HasEntryNode, EntryNode<NodeT>, Empty> {
 public:
-  // NOLINTNEXTLINE
   static const bool is_generic_graph = true;
   using NodesContainer = llvm::SmallVector<std::unique_ptr<NodeT>, SmallSize>;
   using Node = NodeT;
