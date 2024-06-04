f4001063a7f5d5411d49493e2de6d04d8578bdae
Lauri Vasama <vasama@rev.ng>
2024-06-04 13:49:53 +0300

    Add note on temporary nature of scalar tuple

--- include/revng-c/mlir/Dialect/Clift/IR/CliftTypes.h
+++ include/revng-c/mlir/Dialect/Clift/IR/CliftTypes.h
@@ -29,6 +29,9 @@ struct ScalarTupleTypeStorage;
 /// Scalar tuples are used to represent the multiple register return values of
 /// raw function types. They are much like structs, but permit only scalar
 /// element types and have no explicit offsets for their elements.
+///
+/// The scalar tuple type is intended as a temporary solution to be replaced in
+/// the future by auto-generated struct types.
 class ScalarTupleType : public Type::TypeBase<ScalarTupleType,
                                               Type,
                                               ScalarTupleTypeStorage,
