d72139357fadb6276bd2a79f842566046a2b98d3
Pietro Fezzardi <pietro@rev.ng>
2024-06-19 18:21:07 +0200

    Add DecompileToDirectoryPipe
    This pipe decompiles all the C code in a single directory, packaged up
    as a .tar.gz file.

---
+++ include/revng-c/Backend/DecompileToDirectoryPipe.h
@@ -0,0 +1,64 @@
+#pragma once
+
+//
+// This file is distributed under the MIT License. See LICENSE.md for details.
+//
+
+#include <array>
+#include <string>
+
+#include "llvm/ADT/ArrayRef.h"
+#include "llvm/Support/raw_ostream.h"
+
+#include "revng/Pipeline/Contract.h"
+#include "revng/Pipes/FileContainer.h"
+#include "revng/Pipes/Kinds.h"
+
+#include "revng-c/Pipes/Kinds.h"
+
+namespace revng::pipes {
+
+inline constexpr char RecompilableArchiveMime[] = "application/x-tar";
+inline constexpr char RecompilableArchiveName[] = "recompilable-archive";
+inline constexpr char RecompilableArchiveExtension[] = ".tar.gz";
+using RecompilableArchiveContainer = FileContainer<
+  &kinds::RecompilableArchive,
+  RecompilableArchiveName,
+  RecompilableArchiveMime,
+  RecompilableArchiveExtension>;
+
+class DecompileToDirectory {
+public:
+  static constexpr auto Name = "decompile-to-directory";
+
+  std::array<pipeline::ContractGroup, 1> getContract() const {
+    using namespace pipeline;
+    using namespace revng::kinds;
+
+    return { ContractGroup({ Contract(StackAccessesSegregated,
+                                      0,
+                                      RecompilableArchive,
+                                      2,
+                                      InputPreservation::Preserve),
+                             Contract(CFG,
+                                      1,
+                                      RecompilableArchive,
+                                      2,
+                                      InputPreservation::Preserve) }) };
+  }
+
+  void run(const pipeline::ExecutionContext &Ctx,
+           pipeline::LLVMContainer &IRContainer,
+           const revng::pipes::CFGMap &CFGMap,
+           RecompilableArchiveContainer &OutTarFile);
+
+  llvm::Error checkPrecondition(const pipeline::Context &Ctx) const {
+    return llvm::Error::success();
+  }
+
+  void print(const pipeline::Context &Ctx,
+             llvm::raw_ostream &OS,
+             llvm::ArrayRef<std::string> ContainerNames) const {}
+};
+
+} // end namespace revng::pipes
---
+++ lib/Backend/DecompileToDirectoryPipe.cpp
@@ -0,0 +1,128 @@
+//
+// This file is distributed under the MIT License. See LICENSE.md for details.
+//
+
+#include "llvm/ADT/ArrayRef.h"
+#include "llvm/Support/raw_ostream.h"
+
+#include "revng/EarlyFunctionAnalysis/ControlFlowGraphCache.h"
+#include "revng/Pipeline/AllRegistries.h"
+#include "revng/Support/GzipTarFile.h"
+#include "revng/Support/ResourceFinder.h"
+
+#include "revng-c/Backend/DecompileFunction.h"
+#include "revng-c/Backend/DecompileToDirectoryPipe.h"
+#include "revng-c/Backend/DecompileToSingleFile.h"
+#include "revng-c/HeadersGeneration/HelpersToHeader.h"
+#include "revng-c/HeadersGeneration/ModelToHeader.h"
+#include "revng-c/Support/PTMLC.h"
+
+namespace revng::pipes {
+
+using namespace pipeline;
+
+static RegisterDefaultConstructibleContainer<RecompilableArchiveContainer> Reg;
+
+void DecompileToDirectory::run(const pipeline::ExecutionContext &Ctx,
+                               pipeline::LLVMContainer &IRContainer,
+                               const revng::pipes::CFGMap &CFGMap,
+                               RecompilableArchiveContainer &OutTarFile) {
+
+  std::error_code EC;
+  llvm::raw_fd_ostream OutputStream{ OutTarFile.getOrCreatePath(), EC };
+  if (EC)
+    revng_abort(EC.message().c_str());
+
+  GzipTarWriter TarWriter{ OutputStream };
+
+  llvm::Module &Module = IRContainer.getModule();
+  const model::Binary &Model = *getModelFromContext(Ctx);
+  {
+
+    ControlFlowGraphCache Cache{ CFGMap };
+
+    std::string DecompiledC;
+
+    llvm::raw_string_ostream Out{ DecompiledC };
+
+    DecompileStringMap DecompiledFunctions("tmp");
+    decompile(Cache,
+              Module,
+              Model,
+              DecompiledFunctions,
+              /* GeneratePlainC = */ true);
+
+    ptml::PTMLCBuilder B{ /* GeneratePlainC = */ true };
+    printSingleCFile(Out, B, DecompiledFunctions, {} /* Targets */);
+
+    Out.flush();
+
+    TarWriter.append("decompiled/decompiled.c",
+                     llvm::ArrayRef{ DecompiledC.data(),
+                                     DecompiledC.length() });
+  }
+
+  {
+    std::string ModelHeader;
+    llvm::raw_string_ostream Out{ ModelHeader };
+
+    dumpModelToHeader(Model,
+                      Out,
+                      ModelToHeaderOptions{ .GeneratePlainC = true });
+
+    Out.flush();
+
+    TarWriter.append("decompiled/types-and-globals.h",
+                     llvm::ArrayRef{ ModelHeader.data(),
+                                     ModelHeader.length() });
+  }
+
+  {
+    std::string HelpersHeader;
+    llvm::raw_string_ostream Out{ HelpersHeader };
+
+    dumpHelpersToHeader(Module,
+                        Out,
+                        /* GeneratePlainC = */ true);
+
+    Out.flush();
+
+    TarWriter.append("decompiled/helpers.h",
+                     llvm::ArrayRef{ HelpersHeader.data(),
+                                     HelpersHeader.length() });
+  }
+
+  {
+    auto Path = revng::ResourceFinder.findFile("share/revng-c/include/"
+                                               "revng-attributes.h");
+
+    if (not Path or Path->empty())
+      revng_abort("can't find revng-attributes.h");
+
+    auto BufferOrError = llvm::MemoryBuffer::getFileOrSTDIN(*Path);
+    auto Buffer = cantFail(errorOrToExpected(std::move(BufferOrError)));
+
+    TarWriter.append("decompiled/revng-attributes.h",
+                     { Buffer->getBufferStart(), Buffer->getBufferSize() });
+  }
+
+  {
+    auto Path = revng::ResourceFinder.findFile("share/revng-c/include/"
+                                               "revng-primitive-types.h");
+
+    if (not Path or Path->empty())
+      revng_abort("can't find revng-primitive-types.h");
+
+    auto BufferOrError = llvm::MemoryBuffer::getFileOrSTDIN(*Path);
+    auto Buffer = cantFail(errorOrToExpected(std::move(BufferOrError)));
+
+    TarWriter.append("decompiled/revng-primitive-types.h",
+                     { Buffer->getBufferStart(), Buffer->getBufferSize() });
+  }
+
+  TarWriter.close();
+}
+
+} // end namespace revng::pipes
+
+static pipeline::RegisterPipe<revng::pipes::DecompileToDirectory> Y;
