97e561d27d00cb81b28b6c00bab962fec5d10b10
Ivan Krysak <ivan@rev.ng>
2024-08-30 15:06:21 +0300

    ConstexprString: allow construction from ranges

--- include/revng/ADT/ConstexprString.h
+++ include/revng/ADT/ConstexprString.h
@@ -7,6 +7,33 @@
 #include <array>
 #include <string_view>
 
+namespace compile_time {
+
+namespace detail {
+
+template<std::ranges::range Range>
+using DecayedElement = std::decay_t<decltype(*std::declval<Range>().begin())>;
+
+template<std::size_t... Indices, std::ranges::range Range>
+constexpr std::array<DecayedElement<Range>, sizeof...(Indices)>
+makeArray(std::index_sequence<Indices...> Is, Range const &R) {
+  return std::array<DecayedElement<Range>, sizeof...(Indices)>{
+    { *(R.begin() + Indices)... }
+  };
+}
+
+} // namespace detail
+
+/// Makes an array out of an arbitrary constexpr range
+/// (for example, a string_view).
+template<std::size_t ElementCount, std::ranges::range Range>
+constexpr auto makeArray(Range &&R) {
+  return detail::makeArray(std::make_index_sequence<ElementCount>(),
+                           std::forward<Range>(R));
+}
+
+} // namespace compile_time
+
 /// Allows passing strings as template parameters
 ///
 /// Objects of this type should only ever be constructed from a string literal.
@@ -26,6 +53,12 @@ public:
   {
     std::copy(Value, Value + N - 1, String.data());
   }
+
+  consteval ConstexprString(const std::array<char, N - 1> Value) noexcept :
+    String(Value) {}
+
+  consteval ConstexprString(std::ranges::range auto &&Range) noexcept :
+    String(compile_time::makeArray<N - 1>(Range)) {}
 
   constexpr ConstexprString() noexcept
     requires(N == 1)
