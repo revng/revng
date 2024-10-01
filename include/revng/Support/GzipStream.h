33faa52a0eb0554bf44a23f11bc295435c7240b0
Giacomo Vercesi <giacomo.vercesi@rev.ng>
2024-10-01 11:39:37 +0200

    Uniform GzipStream
    Uniform the functions/classes in GzipStream with the ones in ZstdStream.

--- include/revng/Support/GzipStream.h
+++ include/revng/Support/GzipStream.h
@@ -7,20 +7,42 @@
 #include "llvm/ADT/ArrayRef.h"
 #include "llvm/Support/raw_ostream.h"
 
+#include "revng/Support/DataBuffer.h"
+
+#include "zlib.h"
+
 void gzipCompress(llvm::raw_ostream &OS,
-                  llvm::ArrayRef<uint8_t> Buffer,
+                  DataBuffer Buffer,
                   int CompressionLevel = 3);
 
-inline void gzipCompress(llvm::raw_ostream &OS, llvm::ArrayRef<char> Buffer) {
-  return gzipCompress(OS,
-                      { reinterpret_cast<const uint8_t *>(Buffer.data()),
-                        Buffer.size() });
+inline llvm::SmallVector<char> gzipCompress(DataBuffer Buffer) {
+  llvm::SmallVector<char> Result;
+  llvm::raw_svector_ostream OS(Result);
+  gzipCompress(OS, Buffer);
+  return Result;
 }
 
-void gzipDecompress(llvm::raw_ostream &OS, llvm::ArrayRef<uint8_t> Buffer);
+void gzipDecompress(llvm::raw_ostream &OS, DataBuffer Buffer);
 
-inline void gzipDecompress(llvm::raw_ostream &OS, llvm::ArrayRef<char> Buffer) {
-  return gzipDecompress(OS,
-                        { reinterpret_cast<const uint8_t *>(Buffer.data()),
-                          Buffer.size() });
+inline llvm::SmallVector<char> gzipDecompress(DataBuffer Buffer) {
+  llvm::SmallVector<char> Result;
+  llvm::raw_svector_ostream OS(Result);
+  gzipDecompress(OS, Buffer);
+  return Result;
 }
+
+class GzipCompressedOstream : public llvm::raw_ostream {
+private:
+  llvm::raw_ostream &OS;
+  llvm::SmallVector<uint8_t> OutBuffer;
+  z_stream Stream;
+
+public:
+  GzipCompressedOstream(llvm::raw_ostream &OS, int CompressionLevel = 3);
+  ~GzipCompressedOstream() override;
+  void flush();
+
+private:
+  void write_impl(const char *Ptr, size_t Size) override;
+  uint64_t current_pos() const override { return OS.tell(); }
+};
