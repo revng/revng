fd977359e847f3b82278c45374d439e375a43dfa
Alessandro Di Federico <ale@rev.ng>
2024-10-25 10:53:44 +0200

    Import revng-c's Ranks.h

--- include/revng/Pipes/Ranks.h
+++ include/revng/Pipes/Ranks.h
@@ -56,4 +56,28 @@ inline auto DynamicFunction = defineRank<"dynamic-function",
 
 inline auto PrimitiveType = defineRank<"primitive", std::string>(Binary);
 
+/// Rank for locations associated to QEMU and LLVM helper functions
+inline auto HelperFunction = defineRank<"helper-function", std::string>(Binary);
+
+/// Rank for locations associated to struct return types of QEMU and LLVM helper
+/// functions
+inline auto HelperStructType = defineRank<"helper-struct-type", // formatting
+                                          std::string>(Binary);
+
+/// Rank for locations associated to fields of struct return types of QEMU and
+/// LLVM helper functions
+inline auto HelperStructField = defineRank<"helper-struct-field",
+                                           std::string>(HelperStructType);
+
+/// Rank for locations associated to arguments of dynamic functions.
+inline auto DynamicFunctionArgument = defineRank<"dynamic-function-argument",
+                                                 std::string>(DynamicFunction);
+
+/// Rank for locations associated to function arguments and local variables.
+inline auto LocalVariable = defineRank<"local-variable", std::string>(Function);
+
+/// Rank for artificial structs returned by raw functions
+inline auto
+  ArtificialStruct = defineRank<"artificial-struct",
+                                model::RawFunctionDefinition::Key>(Binary);
 } // namespace revng::ranks
