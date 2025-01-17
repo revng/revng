6994395216b64e91017ea4ba125ecba02381ddb4
Ivan Krysak <ivan@rev.ng>
2025-01-17 11:09:26 +0200

    ModelConfiguration: introduce `CommentLineWidth`

--- include/revng/Model/Configuration.h
+++ include/revng/Model/Configuration.h
@@ -14,8 +14,21 @@ fields:
   - name: Disassembly
     type: DisassemblyConfiguration
     optional: true
+
   - name: Naming
     type: NamingConfiguration
+    optional: true
+
+  # Configuration options that don't belong anywhere else find themselves here.
+  # As this list grows, they should be split into their own sub-sections.
+  - name: CommentLineWidth
+    doc: |
+      Sets a recommended comment line width to improve their readability.
+
+      The default value is `80`.
+
+      Set to `-1` for unlimited line size.
+    type: uint64_t
     optional: true
 TUPLE-TREE-YAML */
 
@@ -32,6 +45,14 @@ public:
   bool verify(VerifyHelper &VH) const;
   bool verify(bool Assert) const debug_function;
   bool verify() const debug_function;
+
+public:
+  uint64_t commentLineWidth() const {
+    if (CommentLineWidth() == 0)
+      return 80;
+    else
+      return CommentLineWidth();
+  }
 };
 
 #include "revng/Model/Generated/Late/Configuration.h"
