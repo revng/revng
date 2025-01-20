a9c16f8c7914645d1f09501cffd8d1fcdd98d53e
Pietro Fezzardi <pietro@rev.ng>
2025-01-20 08:36:11 +0100

    Add helpers to manipulate model type metadata

---
+++ include/revng/LocalVariables/LocalVariableHelpers.h
@@ -0,0 +1,48 @@
+#pragma once
+
+//
+// This file is distributed under the MIT License. See LICENSE.md for details.
+//
+
+#include "llvm/ADT/StringRef.h"
+#include "llvm/IR/Instruction.h"
+#include "llvm/IR/Instructions.h"
+
+inline constexpr const char *StackTypeMDName = "revng.stack_type";
+inline const char *VariableTypeMDName = "revng.stack_type";
+
+inline bool hasNamedMetadata(const llvm::AllocaInst *I,
+                             llvm::StringRef MDName) {
+  return nullptr != I->getMetadata(I->getContext().getMDKindID(MDName));
+}
+
+/// \name Functions for manipulating stack model type metadata
+///
+///@{
+
+inline bool hasStackTypeMetadata(const llvm::AllocaInst *I) {
+  return hasNamedMetadata(I, StackTypeMDName);
+};
+
+void setStackTypeMetadata(llvm::AllocaInst *I, const model::Type &StackType);
+
+model::UpcastableType getStackTypeFromMetadata(llvm::AllocaInst *I,
+                                               const model::Binary &Model);
+
+///@}
+
+/// \name Helper functions for manipulating local variable model type metadata
+///
+///@{
+
+inline bool hasVariableTypeMetadata(const llvm::AllocaInst *I) {
+  return hasNamedMetadata(I, VariableTypeMDName);
+}
+
+void setVariableTypeMetadata(llvm::AllocaInst *I,
+                             const model::Type &VariableType);
+
+model::UpcastableType getVariableTypeFromMetadata(llvm::AllocaInst *I,
+                                                  const model::Binary &Model);
+
+///@}
