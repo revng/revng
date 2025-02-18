6a6b34b831e9b3c6c6823599b44e5afeca02c1cd
Ivan Krysak <ivan@rev.ng>
2025-02-18 15:39:51 +0200

    Introduce a general static registry helper

--- include/revng/Model/Pass/RegisterModelPass.h
+++ include/revng/Model/Pass/RegisterModelPass.h
@@ -4,63 +4,19 @@
 // This file is distributed under the MIT License. See LICENSE.md for details.
 //
 
-#include <compare>
-#include <set>
-
-#include "llvm/ADT/Twine.h"
-#include "llvm/Support/ManagedStatic.h"
-
 #include "revng/Model/Binary.h"
+#include "revng/Support/ManagedStaticRegistry.h"
 
-class RegisterModelPass {
+struct RegisteredModelPass {
 public:
-  using ModelPass = std::function<void(TupleTree<model::Binary> &)>;
-
-private:
-  struct Registered {
-    std::string Name;
-    std::string Description;
-    ModelPass Pass;
-  };
-  struct TransparentNameComparator {
-    using is_transparent = std::true_type;
-
-    template<typename Type>
-    llvm::StringRef get(const Type &Value) const {
-      return llvm::StringRef(Value);
-    }
-    llvm::StringRef get(const Registered &Value) const {
-      return llvm::StringRef(Value.Name);
-    }
-    template<typename LHS, typename RHS>
-    bool operator()(const LHS &Left, const RHS &Right) const {
-      return get(Left) < get(Right);
-    }
-  };
-  using RegistryType = std::set<Registered, TransparentNameComparator>;
-  static llvm::ManagedStatic<RegistryType> Registry;
+  using ModelPassType = std::function<void(TupleTree<model::Binary> &)>;
 
 public:
-  RegisterModelPass(const llvm::Twine &Name,
-                    const llvm::Twine &Description,
-                    ModelPass Pass) {
-    Registered RegisteredPass{ Name.str(), Description.str(), Pass };
-    auto &&[_, Success] = Registry->emplace(std::move(RegisteredPass));
-    revng_assert(Success);
-  }
+  std::string Name;
+  std::string Description;
+  ModelPassType Pass;
 
 public:
-  static const ModelPass *get(llvm::StringRef Name) {
-    if (auto It = Registry->find(Name); It == Registry->end())
-      return nullptr;
-    else
-      return &It->Pass;
-  }
-
-  static auto passes() {
-    return llvm::make_range(Registry->begin(), Registry->end());
-  }
+  const std::string &key() const { return Name; }
 };
-
-inline llvm::ManagedStatic<RegisterModelPass::RegistryType>
-  RegisterModelPass::Registry;
+using RegisterModelPass = RegisterManagedStaticImpl<RegisteredModelPass>;
