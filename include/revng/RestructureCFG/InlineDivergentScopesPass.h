40ebac06cc5a047565bbd743bd683185259d25e8
Andrea Gussoni <andreagus@rev.ng>
2025-03-20 12:21:37 +0100

    IDS: introduce pass
    Introduce the `inline-divergent-scopes` pass. This pass in is charge of
    reducing the exit nodes in a `ScopeGraph`, applying the _Inline
    Divergent Scopes_ transformation, which is able, in presence of
    divergent exits nodes, wrt. to a conditional, to remove the exit node
    and to transform the `ScopeGraph` in a form where semantics is
    preserved.

---
+++ include/revng/RestructureCFG/InlineDivergentScopesPass.h
@@ -0,0 +1,20 @@
+#pragma once
+
+//
+// Copyright rev.ng Labs Srl. See LICENSE.md for details.
+//
+
+#include "llvm/IR/Function.h"
+#include "llvm/Pass.h"
+
+class InlineDivergentScopesPass : public llvm::FunctionPass {
+public:
+  static char ID;
+
+public:
+  InlineDivergentScopesPass() : llvm::FunctionPass(ID) {}
+
+  bool runOnFunction(llvm::Function &F) override;
+
+  void getAnalysisUsage(llvm::AnalysisUsage &AU) const override;
+};
