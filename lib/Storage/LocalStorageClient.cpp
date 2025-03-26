1aa9c61c31990941743732ce8d60184e8de16345
Giacomo Vercesi <giacomo.vercesi@rev.ng>
2025-03-26 09:35:53 +0100

    LocalStorageClient: save filelist
    When saving to disk, `LocalStorageClient` will also save a file named
    `file_list.yml` which will list the list of files, which is needed for
    downstream save handlers to efficently save.

--- lib/Storage/LocalStorageClient.cpp
+++ lib/Storage/LocalStorageClient.cpp
@@ -5,6 +5,8 @@
 // This file is distributed under the MIT License. See LICENSE.md for details.
 //
 
+#include "llvm/Support/YAMLTraits.h"
+
 #include "revng/Support/PathList.h"
 
 #include "LocalFile.h"
@@ -34,10 +36,15 @@ std::string LocalStorageClient::dumpString() const {
 llvm::Expected<PathType> LocalStorageClient::type(llvm::StringRef Path) {
   std::string ResolvedPath = resolvePath(Path);
   if (llvm::sys::fs::exists(ResolvedPath)) {
-    if (llvm::sys::fs::is_directory(ResolvedPath))
+    if (llvm::sys::fs::is_directory(ResolvedPath)) {
       return PathType::Directory;
-    else
+    } else {
+      // If Path is not in files it means that the file existed before the
+      // StorageClient was created, add an entry with index 0.
+      if (Metadata.Files.count(Path) == 0)
+        Metadata.Files[Path] = { .Index = 0 };
       return PathType::File;
+    }
   } else {
     return PathType::Missing;
   }
@@ -64,6 +71,7 @@ llvm::Error LocalStorageClient::remove(llvm::StringRef Path) {
                                    ResolvedPath.c_str());
   }
 
+  Metadata.Files.erase(Path);
   return llvm::Error::success();
 }
 
@@ -84,6 +92,8 @@ llvm::Error LocalStorageClient::copy(llvm::StringRef Source,
                                    ResolvedDestination.c_str());
   }
 
+  Metadata.Files[Destination] = { .Index = Metadata.TransactionIndex,
+                                  .Encoding = Metadata.Files[Source].Encoding };
   return llvm::Error::success();
 }
 
@@ -97,6 +107,10 @@ LocalStorageClient::getReadableFile(llvm::StringRef Path) {
                                    ResolvedPath.c_str());
   }
 
+  // If Path is not in files it means that the file existed before the
+  // StorageClient was created, add an entry with index 0.
+  if (Metadata.Files.count(Path) == 0)
+    Metadata.Files[Path] = { .Index = 0 };
   return std::make_unique<LocalReadableFile>(std::move(MaybeBuffer.get()));
 }
 
@@ -114,7 +128,71 @@ LocalStorageClient::getWritableFile(llvm::StringRef Path,
                                    ResolvedPath.c_str());
   }
 
+  Metadata.Files[Path] = { .Index = Metadata.TransactionIndex,
+                           .Encoding = Encoding };
+
   return std::make_unique<LocalWritableFile>(std::move(OS));
 }
 
 } // namespace revng
+
+template<>
+struct llvm::yaml::MappingTraits<revng::LocalStorageClient::FileMetadata> {
+  static void mapping(IO &IO, revng::LocalStorageClient::FileMetadata &Obj) {
+    IO.mapRequired("Index", Obj.TransactionIndex);
+    IO.mapRequired("Files", Obj.Files);
+  }
+};
+
+using FileDataMap = llvm::StringMap<revng::LocalStorageClient::File>;
+template<>
+struct llvm::yaml::CustomMappingTraits<FileDataMap> {
+  static void inputOne(IO &, StringRef, FileDataMap &) {
+    revng_abort("Output only");
+  }
+
+  static void output(IO &IO, FileDataMap &Obj) {
+    for (auto &&[Key, Value] : Obj) {
+      IO.mapRequired(Key.str().c_str(), Value);
+    }
+  }
+};
+
+LLVM_YAML_IS_STRING_MAP(revng::LocalStorageClient::File)
+
+template<>
+struct llvm::yaml::MappingTraits<revng::LocalStorageClient::File> {
+  static void mapping(IO &IO, revng::LocalStorageClient::File &Obj) {
+    IO.mapRequired("Index", Obj.Index);
+    IO.mapRequired("Encoding", Obj.Encoding);
+  }
+};
+
+template<>
+struct llvm::yaml::ScalarEnumerationTraits<revng::ContentEncoding> {
+  static void enumeration(IO &IO, revng::ContentEncoding &Value) {
+    IO.enumCase(Value, "None", revng::ContentEncoding::None);
+    IO.enumCase(Value, "Gzip", revng::ContentEncoding::Gzip);
+  }
+};
+
+llvm::Error revng::LocalStorageClient::commit() {
+  // Write the `file-list.yml` file, this file contains
+  // * The transaction index
+  // * A serialization of the Files property
+  // These are needed for downstream synchronizers to effectively save the
+  // resume directory externally.
+  std::string ResolvedPath = resolvePath("file-list.yml");
+  std::error_code EC;
+  llvm::raw_fd_ostream OS(ResolvedPath, EC);
+  if (EC)
+    return llvm::createStringError(EC, "Could not write file list");
+
+  llvm::yaml::Output Out(OS);
+  Out << Metadata;
+  OS.flush();
+
+  // Increase the transaction index
+  Metadata.TransactionIndex++;
+  return llvm::Error::success();
+}
--- lib/Storage/LocalStorageClient.h
+++ lib/Storage/LocalStorageClient.h
@@ -4,14 +4,35 @@
 // This file is distributed under the MIT License. See LICENSE.md for details.
 //
 
+#include "llvm/ADT/StringMap.h"
+
 #include "revng/Storage/StorageClient.h"
-#include "revng/Support/Debug.h"
 
 namespace revng {
 
 class LocalStorageClient : public StorageClient {
+public:
+  struct File {
+    /// TransactionIndex when the file was last written by the Client, if the
+    /// value is '0' then the file existed before the Client was created.
+    uint64_t Index = 0;
+    /// Content Encoding of the file, this is used downstream.
+    ContentEncoding Encoding = ContentEncoding::None;
+  };
+
+  struct FileMetadata {
+    /// Index of the commit that will be written when calling the `commit`
+    /// method of this class. Increases by one every time `commit()` is called.
+    uint64_t TransactionIndex = 1;
+    /// Map that stores the state of the files managed by this StorageClient. It
+    /// records the last TransactionIndex the files were written and the
+    /// encoding used.
+    llvm::StringMap<File> Files;
+  };
+
 private:
   std::string Root;
+  FileMetadata Metadata;
 
 public:
   LocalStorageClient(llvm::StringRef Root);
@@ -30,6 +51,8 @@ public:
 
   llvm::Expected<std::unique_ptr<WritableFile>>
   getWritableFile(llvm::StringRef Path, ContentEncoding Encoding) override;
+
+  llvm::Error commit() override;
 
 private:
   std::string dumpString() const override;
