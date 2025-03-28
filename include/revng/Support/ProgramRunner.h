fede2f2090f36c1d7f5bfd8dbe4defad786aef34
Giacomo Vercesi <giacomo.vercesi@rev.ng>
2025-03-28 14:41:13 +0100

    ProgramRunner: allow redirection of streams
    Change ProgramRunner's API to allow to optionally provide stdin and
    capture stdout/stderr.

--- include/revng/Support/ProgramRunner.h
+++ include/revng/Support/ProgramRunner.h
@@ -11,6 +11,38 @@
 #include "llvm/ADT/StringRef.h"
 
 class ProgramRunner {
+public:
+  struct Result {
+    /// The exit code of the program, always present
+    int ExitCode = -1;
+    /// Captured stdout, if RunOptions::Capture was specified to do it,
+    /// otherwise an empty string
+    std::string Stdout;
+    /// Captured stderr, if RunOptions::Capture was specified to do it,
+    /// otherwise an empty string
+    std::string Stderr;
+  };
+
+  enum class CaptureOption {
+    /// No capure
+    None,
+    /// Capture only stdout
+    StdoutOnly,
+    /// Capture only stderr
+    StderrOnly,
+    /// Capture both stdout and stderr separately
+    StdoutAndStderrSeparately,
+    /// Capture stdout, redirect stderr to stdout
+    StdoutAndStderrJoined,
+  };
+
+  struct RunOptions {
+    /// If provided, will pipe the provided string to stdin
+    std::optional<std::string> Stdin;
+    /// Capture output options
+    CaptureOption Capture = CaptureOption::None;
+  };
+
 private:
   llvm::SmallVector<std::string, 64> Paths;
   llvm::SmallVector<llvm::StringRef, 64> PathsRef;
@@ -24,6 +56,11 @@ public:
   /// returns the exit code of the program.
   [[nodiscard]] int run(llvm::StringRef ProgramName,
                         llvm::ArrayRef<std::string> Args);
+
+  /// returns a Result object after running the program.
+  [[nodiscard]] Result run(llvm::StringRef ProgramName,
+                           llvm::ArrayRef<std::string> Args,
+                           RunOptions Options);
 };
 
 extern ProgramRunner Runner;
