3702fee9ca5de225c0f4623d3e5653a3f533031b
Lauri Vasama <vasama@rev.ng>
2025-03-31 11:05:13 +0300

    Minor Clift.cpp cleanup

--- lib/mlir/Dialect/Clift/IR/Clift.cpp
+++ lib/mlir/Dialect/Clift/IR/Clift.cpp
@@ -11,12 +11,13 @@
 // comment to force generated files to be included be after regular includes
 #include "revng/mlir/Dialect/Clift/IR/CliftOpsDialect.cpp.inc"
 
-class TypeAliasASMInterface : public mlir::OpAsmDialectInterface {
+namespace {
+
+class CliftOpAsmDialectInterface final : public mlir::OpAsmDialectInterface {
 public:
   using mlir::OpAsmDialectInterface::OpAsmDialectInterface;
 
-  AliasResult getAlias(mlir::Attribute Attr,
-                       llvm::raw_ostream &OS) const final {
+  AliasResult getAlias(mlir::Attribute Attr, llvm::raw_ostream &OS) const {
     if (auto Aliasable = mlir::dyn_cast<mlir::clift::AliasableAttr>(Attr)) {
       if (Aliasable.getAlias(OS))
         return AliasResult::FinalAlias;
@@ -24,7 +25,7 @@ public:
     return AliasResult::NoAlias;
   }
 
-  AliasResult getAlias(mlir::Type Type, llvm::raw_ostream &OS) const final {
+  AliasResult getAlias(mlir::Type Type, llvm::raw_ostream &OS) const {
     if (auto Aliasable = mlir::dyn_cast<mlir::clift::AliasableType>(Type)) {
       if (Aliasable.getAlias(OS))
         return AliasResult::FinalAlias;
@@ -33,9 +34,11 @@ public:
   }
 };
 
+} // namespace
+
 void mlir::clift::CliftDialect::initialize() {
   registerTypes();
   registerOperations();
   registerAttributes();
-  addInterfaces<TypeAliasASMInterface>();
+  addInterfaces<CliftOpAsmDialectInterface>();
 }
