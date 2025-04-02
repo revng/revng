f740badd688c190581cb9444c87a402ced06a180
Giacomo Vercesi <giacomo.vercesi@rev.ng>
2025-04-02 09:34:19 +0200

    PipelineManager: drop `storeContext`
    The `storeContext` function was a misguided attempt to performance
    optimization. Without storing all the containers and the invalidation
    metadata the resume directory would be corrupt by just storing the
    context.

--- include/revng/Pipes/PipelineManager.h
+++ include/revng/Pipes/PipelineManager.h
@@ -94,9 +94,6 @@ public:
   /// each of them.
   llvm::Error store(llvm::ArrayRef<std::string> StoresOverrides);
 
-  /// Triggers only the serialization of the context directory to the execution
-  /// directory
-  llvm::Error storeContext();
   /// Triggers the full serialization of every step and every container to the
   /// Execution directory.
   llvm::Error store();
