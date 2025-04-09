1bb5adeb9afd7ea6bc5028d865d66eb3ca5ba8fe
Ivan Krysak <ivan@rev.ng>
2025-04-09 13:09:10 +0300

    PTML: avoid auto in constexpr string literals

--- include/revng/Support/PTML.h
+++ include/revng/Support/PTML.h
@@ -11,37 +11,37 @@ namespace ptml::c {
 
 namespace tokens {
 
-inline constexpr auto Constant = "c.constant";
-inline constexpr auto Directive = "c.directive";
-inline constexpr auto Field = "c.field";
-inline constexpr auto Function = "c.function";
-inline constexpr auto FunctionParameter = "c.function_parameter";
-inline constexpr auto Keyword = "c.keyword";
-inline constexpr auto Operator = "c.operator";
-inline constexpr auto StringLiteral = "c.string_literal";
-inline constexpr auto Type = "c.type";
-inline constexpr auto Variable = "c.variable";
-inline constexpr auto GotoLabel = "c.goto_label";
+inline constexpr llvm::StringRef Constant = "c.constant";
+inline constexpr llvm::StringRef Directive = "c.directive";
+inline constexpr llvm::StringRef Field = "c.field";
+inline constexpr llvm::StringRef Function = "c.function";
+inline constexpr llvm::StringRef FunctionParameter = "c.function_parameter";
+inline constexpr llvm::StringRef Keyword = "c.keyword";
+inline constexpr llvm::StringRef Operator = "c.operator";
+inline constexpr llvm::StringRef StringLiteral = "c.string_literal";
+inline constexpr llvm::StringRef Type = "c.type";
+inline constexpr llvm::StringRef Variable = "c.variable";
+inline constexpr llvm::StringRef GotoLabel = "c.goto_label";
 
 } // namespace tokens
 
 namespace scopes {
 
-inline constexpr auto Function = "c.function";
-inline constexpr auto FunctionBody = "c.function_body";
-inline constexpr auto Scope = "c.scope";
-inline constexpr auto StructBody = "c.struct";
-inline constexpr auto UnionBody = "c.union";
-inline constexpr auto TypeDeclarationsList = "c.type_declarations_list";
-inline constexpr auto FunctionDeclarationsList = "c.function_declarations_"
-                                                 "list";
-inline constexpr auto DynamicFunctionDeclarationsList = "c.dynamic_"
-                                                        "function_"
-                                                        "declaration"
-                                                        "s_list";
-inline constexpr auto SegmentDeclarationsList = "c.segment_"
-                                                "declarations_"
-                                                "list";
+inline constexpr llvm::StringRef Function = "c.function";
+inline constexpr llvm::StringRef FunctionBody = "c.function_body";
+inline constexpr llvm::StringRef Scope = "c.scope";
+inline constexpr llvm::StringRef StructBody = "c.struct";
+inline constexpr llvm::StringRef UnionBody = "c.union";
+inline constexpr llvm::StringRef TypeDeclarationsList = "c.type_declarations_"
+                                                        "list";
+inline constexpr llvm::StringRef FunctionDeclarationsList = "c.function_"
+                                                            "declarations_list";
+inline constexpr llvm::StringRef DynamicFunctionDeclarationsList = "c.dynamic_"
+                                                                   "function_"
+                                                                   "declaration"
+                                                                   "s_list";
+inline constexpr llvm::StringRef SegmentDeclarationsList = "c.segment_"
+                                                           "declarations_list";
 
 } // namespace scopes
 
