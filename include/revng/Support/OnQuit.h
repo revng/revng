f4a1d5bba649a174419278b934ad2379cf48596f
Giacomo Vercesi <giacomo.vercesi@rev.ng>
2025-04-16 15:51:57 +0200

    OnQuit: clean up
    Split the handlers in `OnQuit` between the repeatable and non-repeatable
    ones. Trigger the non-repeatable ones only on program exit.

--- include/revng/Support/OnQuit.h
+++ include/revng/Support/OnQuit.h
@@ -10,20 +10,46 @@
 
 #include "llvm/Support/ManagedStatic.h"
 
+enum class AdditionalSignals : uint8_t {
+  None,
+  USR1,
+  USR2,
+  USR1AndUSR2,
+};
+
 class OnQuitRegistry {
+public:
 private:
-  std::vector<std::function<void()>> Registry;
+  struct RegistryEntry {
+    std::function<void()> Handler;
+    AdditionalSignals Signals = AdditionalSignals::None;
+  };
+  std::vector<RegistryEntry> Registry;
 
 public:
-  void install();
+  OnQuitRegistry() = default;
+  ~OnQuitRegistry() = default;
+  OnQuitRegistry(OnQuitRegistry &) = delete;
+  OnQuitRegistry &operator=(const OnQuitRegistry &) = delete;
+  OnQuitRegistry(OnQuitRegistry &&) = delete;
+  OnQuitRegistry &operator=(OnQuitRegistry &&) = delete;
 
-  /// Registers an object for having its onQuit method called upon program
+  /// Registers a cleanup Handler function which will be called upon program
   /// termination
-  void add(std::function<void()> &&Handler) {
-    Registry.push_back(std::move(Handler));
+  void add(std::function<void()> &&Handler,
+           AdditionalSignals Signals = AdditionalSignals::None) {
+    Registry.push_back({ std::move(Handler), Signals });
   }
 
+  /// Register the signal handler, must be called for the handlers to be called
+  void install();
+
+  /// Called at program exit, will call all registered handlers
   void quit();
+
+private:
+  static void signalHandler(int Signal);
+  void callHandlersFor(int Signal);
 };
 
 extern llvm::ManagedStatic<OnQuitRegistry> OnQuit;
