e9875574e6990c7e9c9c536a3e6fd7a26a34a982
Ivan Krysak <ivan@rev.ng>
2025-05-05 17:50:24 +0300

    Migration tests: add a test for model version 4

--- tests/pipeline/run_migration_test.sh
+++ tests/pipeline/run_migration_test.sh
@@ -19,6 +19,12 @@ trap 'rm --recursive --force "${TEST_DIR}"' EXIT
 
 mkdir --parents "${TEST_DIR}"/resume-dir/context
 
+#
+# version 1 -> 3
+#
+
+# TODO: consider splitting different versions into different files.
+
 cat > "${TEST_DIR}"/resume-dir/context/model.yml <<EOF
 Version: 1
 Configuration:
@@ -40,6 +46,59 @@ fi
 
 if [ "${USE_X86_ATT_SYNTAX}" != "true" ]; then
   log "Expected UseX86ATTSyntax = true, but got ${USE_X86_ATT_SYNTAX} instead"
+  log "Here's the migrated file for reference"
+  log_file "${TEST_DIR}"/resume-dir/context/model.yml
+  exit 1
+fi
+
+#
+# version 3 -> 4
+#
+
+# TODO: consider splitting different versions into different files.
+
+cat > "${TEST_DIR}"/resume-dir/context/model.yml <<EOF
+Version:         3
+Functions:
+  - Entry:           "0x1000:Code_aarch64"
+    Comments:
+      - Location:
+          - "0x1000:Code_aarch64"
+        Body:            test_0
+      - Location:
+          - "0x1000:Code_aarch64"
+        Body:            test_1
+      - Location:
+          - "0x1004:Code_aarch64"
+        Body:            test_2
+
+# All of the following is just here to ensure the basic model above verifies
+Segments:
+  - StartAddress:    "0x1000:Generic64"
+    VirtualSize:     256
+    StartOffset:     0
+    FileSize:        256
+    IsReadable:      false
+    IsWriteable:     false
+    IsExecutable:    true
+DefaultPrototype:
+  Kind:            DefinedType
+  Definition:      "/TypeDefinitions/0-RawFunctionDefinition"
+TypeDefinitions:
+  - ID:              0
+    Kind:            RawFunctionDefinition
+    Architecture:    aarch64
+Architecture:    aarch64
+EOF
+
+./bin/revng artifact --resume "${TEST_DIR}"/resume-dir emit-model-header /dev/null
+
+TEST_0_INDEX=$(yq '.Functions[0].Comments[0].Index' "${TEST_DIR}"/resume-dir/context/model.yml)
+TEST_1_INDEX=$(yq '.Functions[0].Comments[1].Index' "${TEST_DIR}"/resume-dir/context/model.yml)
+TEST_2_INDEX=$(yq '.Functions[0].Comments[2].Index' "${TEST_DIR}"/resume-dir/context/model.yml)
+
+if [[ "${TEST_0_INDEX}" != "0" || "${TEST_1_INDEX}" != "1" || "${TEST_2_INDEX}" != "2" ]]; then
+  log "Expected all the comment indices to be consistent after the migration"
   log "Here's the migrated file for reference"
   log_file "${TEST_DIR}"/resume-dir/context/model.yml
   exit 1
