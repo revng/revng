fcaa107ca82c8640281cf08575fac539b2ba96dc
Ivan Krysak <ivan@rev.ng>
2025-05-22 10:33:26 +0300

    Speed up unit tests
    Move relatively heavy llvm lit tests to the normal testing suite,
    this way there is no need to wait for these to finish before getting
    `build.ninja` for the test suite.

--- share/revng/test/tests/lit/llvm/lit.cfg.py
+++ share/revng/test/tests/lit/llvm/lit.cfg.py
@@ -12,4 +12,4 @@ config.test_format = lit.formats.ShTest(True)
 config.suffixes = [".ll"]
 config.test_source_root = os.path.dirname(__file__)
 config.test_exec_root = config.my_obj_root
-config.substitutions.append(("%revngopt", "./bin/revng opt "))
+config.substitutions.append(("%revngopt", "revng opt "))
