73ca618c4d708d66c710c778246c79bea1755dd0
Lauri Vasama <vasama@rev.ng>
2025-05-27 12:55:06 +0300

    Simplify import-clift-types pipe and tests

--- lib/mlir/Pipes/ImportCliftTypesPipe.cpp
+++ lib/mlir/Pipes/ImportCliftTypesPipe.cpp
@@ -2,68 +2,61 @@
 // This file is distributed under the MIT License. See LICENSE.md for details.
 //
 
-#include "mlir/Dialect/LLVMIR/LLVMDialect.h"
-#include "mlir/IR/BuiltinOps.h"
-#include "mlir/IR/FunctionInterfaces.h"
-
-#include "revng/EarlyFunctionAnalysis/CFGStringMap.h"
-#include "revng/EarlyFunctionAnalysis/ControlFlowGraphCache.h"
 #include "revng/Pipeline/Location.h"
 #include "revng/Pipeline/RegisterPipe.h"
 #include "revng/Pipes/Kinds.h"
-#include "revng/mlir/Dialect/Clift/IR/Clift.h"
 #include "revng/mlir/Dialect/Clift/IR/CliftOps.h"
 #include "revng/mlir/Dialect/Clift/Utils/Helpers.h"
 #include "revng/mlir/Dialect/Clift/Utils/ImportModel.h"
 #include "revng/mlir/Pipes/CliftContainer.h"
 
-using namespace mlir::clift;
+namespace clift = mlir::clift;
 
-static void importAllModelTypes(const model::Binary &Model,
-                                mlir::ModuleOp Module) {
-  mlir::MLIRContext *const Context = Module->getContext();
-  Context->loadDialect<CliftDialect>();
+static void importModelTypes(const model::Binary &Model,
+                             mlir::ModuleOp Module) {
+  mlir::MLIRContext *Context = Module->getContext();
+  Context->loadDialect<clift::CliftDialect>();
 
-  const auto EmitError = [&]() -> mlir::InFlightDiagnostic {
-    return Context->getDiagEngine().emit(mlir::UnknownLoc::get(Context),
-                                         mlir::DiagnosticSeverity::Error);
+  mlir::Location Loc = mlir::UnknownLoc::get(Context);
+  auto EmitError = [&]() -> mlir::InFlightDiagnostic {
+    return Context->getDiagEngine().emit(Loc, mlir::DiagnosticSeverity::Error);
   };
 
-  mlir::OpBuilder Builder(Module.getRegion());
-  for (const auto &ModelType : Model.TypeDefinitions()) {
-    auto CliftType = importModelType(EmitError, *Context, *ModelType, Model);
-    Builder.create<UndefOp>(mlir::UnknownLoc::get(Context), CliftType);
+  llvm::SmallVector<mlir::Attribute> TypeAttrs;
+  for (size_t I = 0; const auto &ModelType : Model.TypeDefinitions()) {
+    auto CliftType = clift::importModelType(EmitError,
+                                            *Context,
+                                            *ModelType,
+                                            Model);
+
+    TypeAttrs.push_back(mlir::TypeAttr::get(CliftType));
   }
+
+  Module->setAttr("clift.test", mlir::ArrayAttr::get(Context, TypeAttrs));
 }
 
-class ImportAllCliftTypesPipe {
+class ImportCliftTypesPipe {
 public:
-  static constexpr auto Name = "import-all-clift-types";
+  static constexpr auto Name = "import-clift-types";
 
   std::array<pipeline::ContractGroup, 1> getContract() const {
     using namespace pipeline;
     using namespace revng::kinds;
 
-    return { ContractGroup({ Contract(CFG,
+    return { ContractGroup({ Contract(MLIRFunctionKind,
                                       0,
                                       MLIRFunctionKind,
-                                      1,
-                                      InputPreservation::Preserve),
-                             Contract(MLIRFunctionKind,
-                                      1,
-                                      MLIRFunctionKind,
-                                      1,
+                                      0,
                                       InputPreservation::Preserve) }) };
   }
 
   void run(pipeline::ExecutionContext &EC,
-           const revng::pipes::CFGMap &CFGMap,
            revng::pipes::CliftContainer &CliftContainer) {
-    importAllModelTypes(*revng::getModelFromContext(EC),
-                        CliftContainer.getModule());
+    importModelTypes(*revng::getModelFromContext(EC),
+                     CliftContainer.getModule());
 
     EC.commitAllFor(CliftContainer);
   }
 };
 
-static pipeline::RegisterPipe<ImportAllCliftTypesPipe> Y;
+static pipeline::RegisterPipe<ImportCliftTypesPipe> Y;
