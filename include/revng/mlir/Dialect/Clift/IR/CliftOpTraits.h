98dc40b2b440f0536f86c0ae985fc46248fe7160
Lauri Vasama <vasama@rev.ng>
2025-05-30 11:17:53 +0300

    WIP: Add SideEffects op trait

--- include/revng/mlir/Dialect/Clift/IR/CliftOpTraits.h
+++ include/revng/mlir/Dialect/Clift/IR/CliftOpTraits.h
@@ -54,9 +54,13 @@ public:
 };
 
 template<typename ConcreteType>
+<<<<<<< HEAD
 class ReturnsBoolean : public OpTrait::TraitBase<ConcreteType, ReturnsBoolean> {
   using Base = OpTrait::TraitBase<ConcreteType, ReturnsBoolean>;
 };
+=======
+class SideEffects : public OpTrait::TraitBase<ConcreteType, SideEffects> {};
+>>>>>>> e6acebe00 (WIP: Add SideEffects op trait)
 
 } // namespace clift
 } // namespace OpTrait
