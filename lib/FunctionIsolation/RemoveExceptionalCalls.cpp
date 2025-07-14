690b1465ec178b9ff6842d2e10dc752b41e8878d
Ivan Krysak <ivan@rev.ng>
2025-07-14 17:51:38 +0300

    RemoveExceptionalCalls: preserve debug information

--- lib/FunctionIsolation/RemoveExceptionalCalls.cpp
+++ lib/FunctionIsolation/RemoveExceptionalCalls.cpp
@@ -4,6 +4,7 @@
 // This file is distributed under the MIT License. See LICENSE.md for details.
 //
 
+#include "llvm/IR/DebugInfoMetadata.h"
 #include "llvm/IR/Function.h"
 #include "llvm/IR/Instructions.h"
 #include "llvm/Transforms/Utils/BasicBlockUtils.h"
@@ -40,11 +41,16 @@ bool RemoveExceptionalCalls::runOnModule(llvm::Module &M) {
     // Split containing basic block
     BB->splitBasicBlock(Call);
 
+    // TODO: investigate whether it's safe to use this after the termination
+    //       was dropped.
+    DILocation *DI = BB->getTerminator()->getDebugLoc();
+
     // Drop terminator of the old basic block
     eraseFromParent(BB->getTerminator());
 
     // Terminate with an unreachable
-    new UnreachableInst(C, BB);
+    auto Unreachable = new UnreachableInst(C, BB);
+    Unreachable->setDebugLoc(DI);
 
     // Drop function call
     auto *Undef = UndefValue::get(Call->getType());
