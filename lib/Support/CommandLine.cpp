1faa781e6a26a00d2993c0c02fbf6b530ad55480
Andrea Gussoni <andreagus@rev.ng>
2025-07-23 10:52:54 +0200

    CommandLine: drop `pathToStream`
    Remove `pathToStream` in favour of `llvm::ToolOutputFile`.

--- lib/Support/CommandLine.cpp
+++ lib/Support/CommandLine.cpp
@@ -13,14 +13,3 @@ namespace cl = llvm::cl;
 
 cl::OptionCategory MainCategory("Options", "");
 std::string InputPath;
-
-std::ostream &pathToStream(const std::string &Path, std::ofstream &File) {
-  if (Path[0] == '-' && Path[1] == '\0') {
-    return std::cout;
-  } else {
-    if (File.is_open())
-      File.close();
-    File.open(Path);
-    return File;
-  }
-}
