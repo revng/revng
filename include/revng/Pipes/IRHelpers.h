f1ee21c2cd408b2a51333a07cfcd175d8f0721f2
Ivan Krysak <ivan@rev.ng>
2025-09-04 15:05:22 +0300

    Introduce `isDebugLocationInvalid` helper

--- include/revng/Pipes/IRHelpers.h
+++ include/revng/Pipes/IRHelpers.h
@@ -17,3 +17,19 @@ getLocation(const llvm::Instruction *I);
 
 [[nodiscard]] llvm::DenseMap<MetaAddress, const llvm::Function *>
 getTargetToFunctionMapping(const llvm::Module &M);
+
+namespace llvm {
+class DebugLoc;
+class Instruction;
+} // namespace llvm
+
+// This ensures debug information validity.
+//
+// In revng modules, valid debug information location is one that is:
+// - non-empty (`!!DebugLoc`),
+// - has a scope (`DebugLoc->getScope()`) with a non-empty name,
+// - where the name is a valid `/instruction/...` location.
+// (the last one is subject to change when we start attaching more than one
+//  address per llvm instruction).
+llvm::Error isDebugLocationInvalid(const llvm::DebugLoc &Instruction);
+llvm::Error isDebugLocationInvalid(const llvm::Instruction &Instruction);
