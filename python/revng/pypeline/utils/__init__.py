577d8cd22b35b47c54d19a5dabc54688b86e0df6
Tommaso Fontana <tfontana@rev.ng>
2025-09-24 14:25:23 +0200

    Pypeline: add mime_type and model_name to Model
    Now the model has a mime_type that we expect to return to the UI, and
    model_name so the name of the model on disk can be impl-specific.
    Moreover, now Model and Container use the same logic to detect if
    a mimetype is textual.

--- python/revng/pypeline/utils/__init__.py
+++ python/revng/pypeline/utils/__init__.py
@@ -1,3 +1,14 @@
 #
 # This file is distributed under the MIT License. See LICENSE.md for details.
 #
+
+
+def is_mime_type_text(mime_type: str) -> bool:
+    """
+    Returns True if the mime type represents a text file, False otherwise.
+    """
+    return mime_type.startswith("text/") or mime_type in {
+        "application/json",
+        "application/xml",
+        "application/x-yaml",
+    }
