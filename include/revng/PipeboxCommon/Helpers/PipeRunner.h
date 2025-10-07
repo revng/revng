10a396a3b590a39456bdbbad1799569a5c0a4e3d
Giacomo Vercesi <giacomo.vercesi@rev.ng>
2025-10-07 15:52:46 +0200

    PipeboxCommon: rework `{Analysis,Pipe}Runner`
    Rework the `AnalysisRunner` and `PipeRunner` structs into single
    functions, these do not need to have a template parameter specifying how
    to extract containers as that logic has been delegated to a newly
    introduced trait called `ExtractContainerFromList`.

--- include/revng/PipeboxCommon/Helpers/PipeRunner.h
+++ include/revng/PipeboxCommon/Helpers/PipeRunner.h
@@ -8,48 +8,36 @@
 
 #include "revng/PipeboxCommon/Common.h"
 #include "revng/PipeboxCommon/Concepts.h"
+#include "revng/PipeboxCommon/Helpers/Helpers.h"
 #include "revng/PipeboxCommon/Model.h"
 
 namespace revng::pypeline::helpers {
 
-/// Helper class that allows running a pipe
-// The main hiccup in doing so is to unpack the arguments which are in some
-// kind of sequence (e.g. std::vector) into arguments for the Pipe::run
-// method. The type of the Pipe, the type of the sequence and how to unpack
-// them are conveyed through the Info type, which cannot be a function pointer
-// due to the unpacking function requiring template parameters.
-template<typename ContainerListUnwrapper>
-struct PipeRunner {
-private:
-  using ObjectDeps = pypeline::ObjectDependencies;
+/// Helper function that allows running a pipe, deals with unpacking the
+/// container list to multiple parameters that will be passed to the run
+/// function to of the Pipe.
+template<IsPipe T, typename ListType>
+inline ObjectDependencies runPipe(T &Pipe,
+                                  const Model &TheModel,
+                                  const Request &Incoming,
+                                  const Request &Outgoing,
+                                  llvm::StringRef Configuration,
+                                  ListType &Containers) {
+  using Traits = PipeRunTraits<T>;
+  revng_assert(Incoming.size() == Traits::ContainerCount);
+  revng_assert(Outgoing.size() == Traits::ContainerCount);
+  revng_assert(Containers.size() == Traits::ContainerCount);
 
-  using ListType = ContainerListUnwrapper::ListType;
-
-public:
-  template<IsPipe T>
-  static ObjectDeps run(T &Pipe,
-                        const Model &TheModel,
-                        const pypeline::Request &Incoming,
-                        const pypeline::Request &Outgoing,
-                        llvm::StringRef Configuration,
-                        ListType Containers) {
-    using Traits = PipeRunTraits<T>;
-    revng_assert(Incoming.size() == Traits::ContainerCount);
-    revng_assert(Outgoing.size() == Traits::ContainerCount);
-
-    auto Runner = ([&]<size_t... ContainerIndexes>(const std::index_sequence<
-                                                   ContainerIndexes...> &) {
-      return Pipe.run(TheModel,
-                      Incoming,
-                      Outgoing,
-                      Configuration,
-                      ContainerListUnwrapper::template unwrap<
-                        std::tuple_element_t<ContainerIndexes,
-                                             typename Traits::ContainerTypes> &,
-                        ContainerIndexes>(Containers)...);
-    });
-    return Runner(std::make_index_sequence<Traits::ContainerCount>());
-  }
-};
+  using CT = Traits::ContainerTypes;
+  return compile_time::callWithIndexSequence<CT>([&]<size_t... I>() {
+    return Pipe.run(TheModel,
+                    Incoming,
+                    Outgoing,
+                    Configuration,
+                    ExtractContainerFromList<std::tuple_element_t<I, CT>,
+                                             I,
+                                             ListType>::get(Containers)...);
+  });
+}
 
 } // namespace revng::pypeline::helpers
