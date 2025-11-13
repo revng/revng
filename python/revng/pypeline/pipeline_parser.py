a821c0c2684bfb8ccb2f6b58f3c5d8f519612b5b
Giacomo Vercesi <giacomo.vercesi@rev.ng>
2025-11-13 18:50:17 +0100

    pipeline yaml: allow specifying pure analyses
    Some analyses do not require any container to run, these can be now
    specified as a standalone array.

--- python/revng/pypeline/pipeline_parser.py
+++ python/revng/pypeline/pipeline_parser.py
@@ -362,12 +362,17 @@ def load_pipeline(values: Any) -> Pipeline:
         analyses=analyses,
     )
 
-    if len(roots) == 1:
+    root_analyses_raw = values.get("analyses", [])
+    if len(roots) == 1 and len(root_analyses_raw) == 0:
         root = roots[0]
     else:
         root = DummyPipelineNode("root")
         for node in roots:
             root.add_successor(node)
+        root_analyses: set[AnalysisBinding] = set()
+        root_analyses_dict = [{"analysis": x, "containers": []} for x in root_analyses_raw]
+        parse_analyses(root_analyses_dict, root_analyses, root, container_decls)
+        analyses.update(root_analyses)
 
     return Pipeline(
         declarations=set(container_decls.values()),
