e2482a193f8845ca3ee6a7c422144738d1cc2f53
Ivan Krysak <ivan@rev.ng>
2025-12-03 15:24:43 +0000

    `import-types`: create a new module

--- lib/CliftPipes/ImportTypes.cpp
+++ lib/CliftPipes/ImportTypes.cpp
@@ -8,6 +8,7 @@
 #include "revng/CliftPipes/CliftContainer.h"
 #include "revng/Pipeline/Location.h"
 #include "revng/Pipeline/RegisterPipe.h"
+#include "revng/Pipes/FileContainer.h"
 #include "revng/Pipes/Kinds.h"
 
 class ImportTypesPipe {
@@ -15,17 +16,14 @@ public:
   static constexpr auto Name = "import-types";
 
   std::array<pipeline::ContractGroup, 1> getContract() const {
-    using namespace pipeline;
-    using namespace revng::kinds;
-
-    return { ContractGroup({ Contract(CliftModule,
-                                      0,
-                                      CliftModule,
-                                      0,
-                                      InputPreservation::Preserve) }) };
+    return { pipeline::ContractGroup(revng::kinds::Binary,
+                                     0,
+                                     revng::kinds::CliftModule,
+                                     1) };
   }
 
   void run(pipeline::ExecutionContext &EC,
+           const revng::pipes::BinaryFileContainer &,
            revng::pipes::CliftContainer &CliftContainer) {
     mlir::MLIRContext *Context = CliftContainer.getContext();
     Context->loadDialect<clift::CliftDialect>();
