896bb8d5236892b5b23381b2c2cdeecc4cecca9a
Alessandro Di Federico <ale@rev.ng>
2025-12-03 11:29:42 +0100

    Drop SegregateStackAccessesPass.h

--- lib/PromoteStackPointer/SegregateStackAccessesPass.cpp
+++ lib/PromoteStackPointer/SegregateStackAccessesPass.cpp
@@ -269,6 +269,21 @@ using GCBIWP = GeneratedCodeBasicInfoWrapperPass;
 template<bool IsLegacy>
 using LVB = LocalVariableBuilder<IsLegacy>;
 
+/// Rewrite all stack memory accesses
+///
+/// This pass changes the base address of stack memory access to either:
+///
+/// * The stack frame of the function (allocated by the `revng_stack_frame`
+///   function).
+/// * The stack arguments of a call site (allocated by the
+///   `revng_call_stack_arguments` function), which is then passed in as the
+///   last argument of the function.
+/// * The (newly introduced) last argument of the function representing the
+///   stack arguments.
+///
+/// After this pass, all stack accesses have positive offsets and
+/// `revng_undefined_local_sp` is dropped entirely.
+///
 /// This pass has two modes of operation:
 /// - when LegacyLocalVariables is true it uses old FunctionTags and dedicated
 /// functions to represent local variables, and accesses to them;
