c82a01f68b5794f0d472d8a783b9b327ce0601dd
Ivan Krysak <ivan@rev.ng>
2025-12-22 15:53:46 +0000

    `emit-type-and-global-header`: new pipeline

---
+++ include/revng/CliftPipes/Headers.h
@@ -0,0 +1,45 @@
+#pragma once
+
+//
+// This file is distributed under the MIT License. See LICENSE.md for details.
+//
+
+#include "revng/Pipebox/Containers.h"
+#include "revng/PipeboxCommon/CliftContainers.h"
+#include "revng/PipeboxCommon/Common.h"
+#include "revng/PipeboxCommon/LLVMContainer.h"
+#include "revng/PipeboxCommon/Model.h"
+
+namespace revng::pypeline::piperuns {
+
+class EmitTypeAndGlobalHeader {
+private:
+  const model::Binary &Binary;
+  const CliftModuleContainer &Input;
+  PTMLCBytesContainer &Output;
+
+public:
+  static constexpr llvm::StringRef Name = "emit-type-and-global-header";
+  using Arguments = TypeList<PipeRunArgument<CliftModuleContainer,
+                                             "Input",
+                                             "MLIR container containing "
+                                             "the type system, as well as "
+                                             "function and segment "
+                                             "declarations",
+                                             Access::Read>,
+                             PipeRunArgument<PTMLCBytesContainer,
+                                             "Output",
+                                             "The model header",
+                                             Access::Write>>;
+
+  EmitTypeAndGlobalHeader(const class Model &Model,
+                          llvm::StringRef Config,
+                          llvm::StringRef DynamicConfig,
+                          const CliftModuleContainer &Input,
+                          PTMLCBytesContainer &Output) :
+    Binary(*Model.get().get()), Input(Input), Output(Output){};
+
+  void run();
+};
+
+} // namespace revng::pypeline::piperuns
