029261021230255a108495f034a65c9e38b99b2c
Ivan Krysak <ivan@rev.ng>
2025-12-22 15:57:27 +0000

    Introduce `emit-single-type-definition`

--- lib/CliftPipes/HeaderContainers.h
+++ lib/CliftPipes/HeaderContainers.h
@@ -30,6 +30,12 @@ inline pipeline::SingleElementKind
 inline pipeline::SingleElementKind
   HelperHeader("helper-header", Binary, revng::ranks::Binary, {}, {});
 
+inline TypeKind SingleTypeDefinition("single-type-definition",
+                                     TypeAndGlobalHeader,
+                                     ranks::TypeDefinition,
+                                     {},
+                                     {});
+
 } // namespace revng::kinds
 
 namespace detail {
@@ -39,6 +45,10 @@ inline constexpr char HelperHeaderName[] = "helper-header";
 
 inline constexpr char HeaderMIMEType[] = "text/x.h+ptml";
 inline constexpr char HeaderSuffix[] = ".h";
+
+inline constexpr char TypeDefinitionName[] = "single-type-definition";
+inline constexpr char TypeDefinitionMimeType[] = "text/x.c+tar+gz";
+inline constexpr char TypeDefinitionSuffix[] = ".c";
 
 template<auto... Values>
 using SBF = revng::pipes::StringBufferContainer<Values...>;
@@ -65,3 +75,9 @@ using HelperHeaderContainer = detail::SBF<&revng::kinds::HelperHeader,
                                           detail::HeaderMIMEType,
                                           detail::HeaderSuffix>;
 inline detail::RegisterDCC<HelperHeaderContainer> RegisteredHHC;
+
+using TypeDefinitionContainer = detail::TSM<&revng::kinds::SingleTypeDefinition,
+                                            detail::TypeDefinitionName,
+                                            detail::TypeDefinitionMimeType,
+                                            detail::TypeDefinitionSuffix>;
+inline detail::RegisterDCC<TypeDefinitionContainer> RegisteredTDC;
--- lib/CliftPipes/Headers.cpp
+++ lib/CliftPipes/Headers.cpp
@@ -3,13 +3,17 @@
 //
 
 #include "mlir/IR/BuiltinOps.h"
+#include "mlir/IR/MLIRContext.h"
 #include "mlir/Support/LogicalResult.h"
 
+#include "revng/ABI/Definition.h"
 #include "revng/Clift/CliftTypeInterfaces.h"
 #include "revng/CliftEmitC/CEmitter.h"
 #include "revng/CliftEmitC/CSemantics.h"
 #include "revng/CliftEmitC/Headers.h"
+#include "revng/CliftImportModel/ImportModel.h"
 #include "revng/CliftPipes/CliftContainer.h"
+#include "revng/Model/Binary.h"
 #include "revng/PTML/CTokenEmitter.h"
 #include "revng/Pipeline/RegisterPipe.h"
 
@@ -40,6 +44,32 @@ static void emitHelperHeaderImpl(llvm::raw_ostream &Out,
                                  std::vector<mlir::ModuleOp> Modules) {
   ptml::CTokenEmitter Tokens(Out, ptml::Tagging::Enabled);
   emitHelperHeader(Tokens, Modules);
+
+  Out.flush();
+}
+
+static void emitTypeDefinitionImpl(llvm::raw_ostream &Out,
+                                   mlir::ModuleOp Module,
+                                   const CDataModel &DataModel,
+                                   const model::TypeDefinition &Type,
+                                   const model::Binary &Binary) {
+  ptml::CTokenEmitter Tokens(Out, ptml::Tagging::Disabled);
+
+  mlir::MLIRContext &Context = *Module.getContext();
+  auto EmitError = [&Context]() -> mlir::InFlightDiagnostic {
+    return Context.getDiagEngine().emit(mlir::UnknownLoc::get(&Context),
+                                        mlir::DiagnosticSeverity::Error);
+  };
+  auto CliftType = clift::importType(EmitError, &Context, Type, Binary);
+  revng_check(CliftType != nullptr);
+
+  TypeEmitterConfiguration Configuration = {
+    .TypeToOmit = {},
+    .EmitMaximumEnumValue = true,
+    .ExplicitPadding = false,
+  };
+
+  emitSingleTypeDefinition(Tokens, DataModel, CliftType, Configuration);
 
   Out.flush();
 }
@@ -101,6 +131,41 @@ public:
 };
 
 static pipeline::RegisterPipe<HelperHeaderPipe> HelperHeader;
+
+class SingleTypeDefinitionPipe {
+public:
+  static constexpr auto Name = "emit-single-type-definition";
+
+  std::array<pipeline::ContractGroup, 1> getContract() const {
+    using namespace pipeline;
+    using namespace revng::kinds;
+
+    return { ContractGroup({ Contract(CliftModule,
+                                      0,
+                                      SingleTypeDefinition,
+                                      1,
+                                      InputPreservation::Preserve) }) };
+  }
+
+  void run(pipeline::ExecutionContext &EC,
+           const revng::pipes::CliftContainer &CliftContainer,
+           TypeDefinitionContainer &ModelTypesContainer) {
+    const model::Binary &Binary = *revng::getModelFromContext(EC);
+
+    for (const model::TypeDefinition &Type :
+         revng::getTypeDefinitionsAndCommit(EC, ModelTypesContainer.name())) {
+      std::string &Result = ModelTypesContainer[Type.key()];
+      llvm::raw_string_ostream Out(Result);
+      emitTypeDefinitionImpl(Out,
+                             CliftContainer.getModule(),
+                             abi::getDataModel(Binary),
+                             Type,
+                             Binary);
+    }
+  }
+};
+
+static pipeline::RegisterPipe<SingleTypeDefinitionPipe> TypeDefinition;
 
 } // namespace
 
