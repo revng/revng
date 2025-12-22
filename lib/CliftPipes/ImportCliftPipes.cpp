4f78f32bf05254b1dd3ad63e1de5822dc7f98eca
Ivan Krysak <ivan@rev.ng>
2025-12-22 12:26:35 +0000

    Introduce `import-clift-function-declarations`

--- lib/CliftPipes/ImportCliftPipes.cpp
+++ lib/CliftPipes/ImportCliftPipes.cpp
@@ -2,7 +2,10 @@
 // This file is distributed under the MIT License. See LICENSE.md for details.
 //
 
+#include "mlir/IR/MLIRContext.h"
+
 #include "revng/Clift/CliftDialect.h"
+#include "revng/Clift/CliftTypes.h"
 #include "revng/Clift/Helpers.h"
 #include "revng/CliftImportModel/ImportModel.h"
 #include "revng/CliftPipes/CliftContainer.h"
@@ -10,8 +13,13 @@
 #include "revng/Pipeline/RegisterPipe.h"
 #include "revng/Pipes/FileContainer.h"
 #include "revng/Pipes/Kinds.h"
+#include "revng/Pipes/Ranks.h"
 
 namespace clift = mlir::clift;
+
+//
+// Shared logic
+//
 
 static void importModelTypes(const model::Binary &Model,
                              mlir::ModuleOp Module) {
@@ -36,6 +44,40 @@ static void importModelTypes(const model::Binary &Model,
   Module->setAttr("clift.types", mlir::ArrayAttr::get(Context, TypeAttrs));
 }
 
+template<typename FunctionT, typename RankT, typename... ArgsT>
+clift::FunctionOp importModelFunctionDeclaration(const FunctionT &MF,
+                                                 RankT &Rank,
+                                                 mlir::ModuleOp Module,
+                                                 const model::Binary &Binary) {
+  auto EmitError =
+    [Context = Module.getContext()]() -> mlir::InFlightDiagnostic {
+    return Context->getDiagEngine().emit(mlir::UnknownLoc::get(Context),
+                                         mlir::DiagnosticSeverity::Error);
+  };
+
+  auto ModelPrototype = Binary.prototypeOrDefault(MF.prototype());
+  revng_check(ModelPrototype);
+
+  auto CliftType = mlir::clift::importModelType(EmitError,
+                                                *Module.getContext(),
+                                                *ModelPrototype,
+                                                Binary);
+  auto Prototype = mlir::cast<mlir::clift::FunctionType>(CliftType);
+
+  // NOTE: neither debug information nor name matter for the users of this.
+  std::string Handle = pipeline::locationString(Rank, MF.key());
+  auto UnknownLocation = mlir::UnknownLoc::get(Module.getContext());
+  return mlir::clift::importFunctionDeclaration(Module,
+                                                UnknownLocation,
+                                                toString(MF.key()),
+                                                Handle,
+                                                Prototype);
+}
+
+//
+// Old style pipes
+//
+
 class ImportCliftTypesPipe {
 public:
   static constexpr auto Name = "import-clift-types";
@@ -58,3 +100,46 @@ public:
 };
 
 static pipeline::RegisterPipe<ImportCliftTypesPipe> Y;
+
+class ImportFunctionDeclarations {
+public:
+  static constexpr auto Name = "import-clift-function-declarations";
+
+  std::array<pipeline::ContractGroup, 1> getContract() const {
+    return { pipeline::ContractGroup(revng::kinds::CliftModule,
+                                     0,
+                                     pipeline::InputPreservation::Preserve) };
+  }
+
+  void run(pipeline::ExecutionContext &EC,
+           revng::pipes::CliftContainer &CliftContainer) {
+    const model::Binary &Binary = *revng::getModelFromContext(EC);
+    for (const auto &ModelFunction : Binary.Functions()) {
+      importModelFunctionDeclaration(ModelFunction,
+                                     revng::ranks::Function,
+                                     CliftContainer.getModule(),
+                                     Binary);
+    }
+
+    for (const auto &ModelFunction : Binary.ImportedDynamicFunctions()) {
+      importModelFunctionDeclaration(ModelFunction,
+                                     revng::ranks::DynamicFunction,
+                                     CliftContainer.getModule(),
+                                     Binary);
+    }
+
+    EC.commitUniqueTarget(CliftContainer);
+  }
+};
+
+static pipeline::RegisterPipe<ImportFunctionDeclarations> Z;
+
+//
+// New style pipes
+//
+
+namespace revng::pypeline::piperuns {
+
+// TODO
+
+} // namespace revng::pypeline::piperuns
