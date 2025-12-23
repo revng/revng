df9c82ac6d5fc96330b43f7dc97a6e8c11756fed
Ivan Krysak <ivan@rev.ng>
2025-12-23 17:49:49 +0000

    pipebox: rename `CliftContainer.h`
    The new name is `CliftContainers.h`.

--- include/revng/CliftPipes/ImportModelNamesPipe.h
+++ include/revng/CliftPipes/ImportModelNamesPipe.h
@@ -4,7 +4,7 @@
 // This file is distributed under the MIT License. See LICENSE.md for details.
 //
 
-#include "revng/PipeboxCommon/CliftContainer.h"
+#include "revng/PipeboxCommon/CliftContainers.h"
 #include "revng/PipeboxCommon/Helpers/PipeRuns/CliftFunctionMixin.h"
 #include "revng/PipeboxCommon/Model.h"
 
