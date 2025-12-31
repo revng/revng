d44655b211f1a87ddaa88d9316cbc76583ba7021
Ivan Krysak <ivan@rev.ng>
2025-12-31 10:52:30 +0000

    Introduce module-level `CliftContainer`

--- include/revng/CliftPipes/CliftContainer.h
+++ include/revng/CliftPipes/CliftContainer.h
@@ -59,4 +59,52 @@ public:
   }
 };
 
+class CliftContainer : public pipeline::Container<CliftContainer> {
+public:
+  static const char ID;
+  static constexpr auto Name = "clift-module";
+  static constexpr auto MIMEType = "application/x.mlir.bc";
+
+private:
+  // unique_ptr is used to allow moving the context.
+  std::unique_ptr<mlir::MLIRContext> Context;
+  mlir::OwningOpRef<mlir::ModuleOp> Module;
+
+public:
+  explicit CliftContainer(const llvm::StringRef Name) :
+    pipeline::Container<CliftContainer>(Name) {
+    clearImpl();
+  }
+
+  mlir::MLIRContext *getContext() const { return Context.get(); }
+  mlir::ModuleOp getModule() const { return *Module; }
+  void setModule(mlir::OwningOpRef<mlir::ModuleOp> &&NewModule) {
+    Module = std::move(NewModule);
+  }
+
+  std::unique_ptr<pipeline::ContainerBase>
+  cloneFiltered(const pipeline::TargetsList &Targets) const override;
+
+  void mergeBackImpl(CliftContainer &&Container) override;
+
+  pipeline::TargetsList enumerate() const override;
+
+  bool removeImpl(const pipeline::TargetsList &Targets) override {
+    clearImpl();
+    return true;
+  }
+
+  void clearImpl() override;
+
+  llvm::Error serialize(llvm::raw_ostream &OS) const override;
+  llvm::Error deserializeImpl(const llvm::MemoryBuffer &Buffer) override;
+
+  llvm::Error extractOne(llvm::raw_ostream &OS,
+                         const pipeline::Target &Target) const override;
+
+  static std::vector<pipeline::Kind *> possibleKinds() {
+    return { &kinds::CliftModule };
+  }
+};
+
 } // namespace revng::pipes
