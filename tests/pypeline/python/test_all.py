df4f8b76ac39088f4c15450cfa767a3418e9cc0d
Giacomo Vercesi <giacomo.vercesi@rev.ng>
2026-02-09 15:44:21 +0100

    pypeline: re-organize tests
    Split the tests into two subdirectories: those that work in pure python
    and those that use the C++ bindings.

--- tests/pypeline/python/test_all.py
+++ tests/pypeline/python/test_all.py
@@ -1,7 +1,7 @@
 #
 # This file is distributed under the MIT License. See LICENSE.md for details.
 #
-# pylint: disable=redefined-outer-name
+# mypy: disable-error-code="attr-defined"
 
 from __future__ import annotations
 
