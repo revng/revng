1a865f7f799e81fa4ae925f827c452c54a029bb6
Pietro Fezzardi <pietro@rev.ng>
2026-02-25 14:35:44 +0100

    CTokenEmitter: abort on unknown location

--- lib/PTML/CTokenEmitter.cpp
+++ lib/PTML/CTokenEmitter.cpp
@@ -161,7 +161,7 @@ getAllowedActions(llvm::StringRef Location) {
   if (auto L = pipeline::locationFromString(rr::StackFrameVariable, Location))
     return { pa::Rename, pa::EditType };
 
-  return {};
+  revng_abort(("Unknown Location: " + Location.str()).c_str());
 }
 
 static std::string getActionContextLocation(llvm::StringRef Location) {
