c0b774369b06dfba918e5a1cc77c45e0ff04dfad
Lauri Vasama <vasama@rev.ng>
2026-03-11 20:55:01 +0200

    Remove bad comment in CliftOpInterfaces.cpp

--- lib/Clift/CliftOpInterfaces.cpp
+++ lib/Clift/CliftOpInterfaces.cpp
@@ -1,9 +1,7 @@
-/// Tests for the Clift Dialect
-
 //
 // This file is distributed under the MIT License. See LICENSE.md for details.
 //
-//
+
 #include "revng/Clift/Clift.h"
 #include "revng/Clift/CliftOpInterfaces.h"
 
