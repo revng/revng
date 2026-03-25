3e6c42f69c30236b2ac13d81267326ab5d65b09d
Ivan Krysak <ivan@rev.ng>
2026-03-25 16:19:35 +0000

    `attributes.h` introduce custom attribute helpers

--- share/revng/include/attributes.h
+++ share/revng/include/attributes.h
@@ -8,22 +8,16 @@
 #define __attribute__(argument)
 #endif
 
-#define _STR(x) #x
+#define _CUSTOM_ATTRIBUTE(value) __attribute__((annotate(#value)))
+#define _CUSTOM_ANNOTATION(key, value) \
+  __attribute__((annotate(#key ":" #value)))
 
-// NOLINTNEXTLINE
-// clang-format off
-// (clang-format is disabled because it breaks formatting around `:`s)
+#define _REG(x) _CUSTOM_ANNOTATION(reg, x)
+#define _ABI(x) _CUSTOM_ANNOTATION(abi, x)
+#define _STACK _CUSTOM_ATTRIBUTE(stack)
 
-#define _REG(x) __attribute__((annotate(_STR(reg:x))))
-#define _ABI(x) __attribute__((annotate(_STR(abi:x))))
-#define _STACK __attribute__((annotate(_STR(stack))))
-
-#define _ENUM_UNDERLYING(x) \
-  __attribute__((annotate(_STR(enum_underlying_type:x))))
+#define _ENUM_UNDERLYING(x) _CUSTOM_ANNOTATION(enum_underlying_type, x)
 #define _PACKED __attribute__((packed))
-#define _CAN_CONTAIN_CODE __attribute__((annotate(_STR(can_contain_code))))
-#define _STARTS_AT(x) __attribute__((annotate(_STR(field_start_offset:x))))
-#define _SIZE(x) __attribute__((annotate(_STR(struct_size:x))))
-
-// NOLINTNEXTLINE
-// clang-format on
+#define _CAN_CONTAIN_CODE _CUSTOM_ATTRIBUTE(can_contain_code)
+#define _STARTS_AT(x) _CUSTOM_ANNOTATION(field_start_offset, x)
+#define _SIZE(x) _CUSTOM_ANNOTATION(struct_size, x)
