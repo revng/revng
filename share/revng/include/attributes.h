5631fe68fb269e34ccae65c22e5a66de5417865e
Ivan Krysak <ivan@rev.ng>
2026-03-25 16:24:35 +0000

    `attributes.h`: reorganize the attribute order

--- share/revng/include/attributes.h
+++ share/revng/include/attributes.h
@@ -8,16 +8,21 @@
 #define __attribute__(argument)
 #endif
 
+// Helper macros
 #define _CUSTOM_ATTRIBUTE(value) __attribute__((annotate(#value)))
 #define _CUSTOM_ANNOTATION(key, value) \
   __attribute__((annotate(#key ":" #value)))
 
+// Custom attributes
+#define _STACK _CUSTOM_ATTRIBUTE(stack)
+#define _CAN_CONTAIN_CODE _CUSTOM_ATTRIBUTE(can_contain_code)
+
+// Custom attributes with an argument
 #define _REG(x) _CUSTOM_ANNOTATION(reg, x)
 #define _ABI(x) _CUSTOM_ANNOTATION(abi, x)
-#define _STACK _CUSTOM_ATTRIBUTE(stack)
-
-#define _ENUM_UNDERLYING(x) _CUSTOM_ANNOTATION(enum_underlying_type, x)
-#define _PACKED __attribute__((packed))
-#define _CAN_CONTAIN_CODE _CUSTOM_ATTRIBUTE(can_contain_code)
 #define _STARTS_AT(x) _CUSTOM_ANNOTATION(field_start_offset, x)
 #define _SIZE(x) _CUSTOM_ANNOTATION(struct_size, x)
+#define _ENUM_UNDERLYING(x) _CUSTOM_ANNOTATION(enum_underlying_type, x)
+
+// Real attribute aliases
+#define _PACKED __attribute__((packed))
