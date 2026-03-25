16611a93fc97c403beb54a25534d8a881aaf5b6b
Ivan Krysak <ivan@rev.ng>
2026-03-25 16:14:13 +0000

    `attributes.h`: drop unnecessary macro definitions

--- share/revng/include/attributes.h
+++ share/revng/include/attributes.h
@@ -8,23 +8,22 @@
 #define __attribute__(argument)
 #endif
 
-#define STR(x) #x
+#define _STR(x) #x
+
 // NOLINTNEXTLINE
 // clang-format off
-#define REG_ATTRIBUTE_STRING(reg_name) STR(reg:reg_name)
-#define ABI_ATTRIBUTE_STRING(abi_name) STR(abi:abi_name)
-#define ENUM_ATTRIBUTE_STRING(type_name) STR(enum_underlying_type:type_name)
-#define FIELD_START_ATTRIBUTE_STRING(offset) STR(field_start_offset:offset)
-#define STRUCT_SIZE_ATTRIBUTE_STRING(size) STR(struct_size:size)
+// (clang-format is disabled because it breaks formatting around `:`s)
+
+#define _REG(x) __attribute__((annotate(_STR(reg:x))))
+#define _ABI(x) __attribute__((annotate(_STR(abi:x))))
+#define _STACK __attribute__((annotate(_STR(stack))))
+
+#define _ENUM_UNDERLYING(x) \
+  __attribute__((annotate(_STR(enum_underlying_type:x))))
+#define _PACKED __attribute__((packed))
+#define _CAN_CONTAIN_CODE __attribute__((annotate(_STR(can_contain_code))))
+#define _STARTS_AT(x) __attribute__((annotate(_STR(field_start_offset:x))))
+#define _SIZE(x) __attribute__((annotate(_STR(struct_size:x))))
+
 // NOLINTNEXTLINE
 // clang-format on
-
-#define _REG(x) __attribute__((annotate(REG_ATTRIBUTE_STRING(x))))
-#define _ABI(x) __attribute__((annotate(ABI_ATTRIBUTE_STRING(x))))
-#define _STACK __attribute__((annotate("stack")))
-
-#define _ENUM_UNDERLYING(x) __attribute__((annotate(ENUM_ATTRIBUTE_STRING(x))))
-#define _PACKED __attribute__((packed))
-#define _CAN_CONTAIN_CODE __attribute__((annotate("can_contain_code")))
-#define _STARTS_AT(x) __attribute__((annotate(FIELD_START_ATTRIBUTE_STRING(x))))
-#define _SIZE(x) __attribute__((annotate(STRUCT_SIZE_ATTRIBUTE_STRING(x))))
