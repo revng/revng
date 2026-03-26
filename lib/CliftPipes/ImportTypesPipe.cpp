73ddff7126fac5538a7da0b56608e22e567cdf96
Ivan Krysak <ivan@rev.ng>
2026-03-26 15:37:36 +0000

    `import-clift-types` -> `import-types`

--- lib/CliftPipes/ImportTypesPipe.cpp
+++ lib/CliftPipes/ImportTypesPipe.cpp
@@ -31,9 +31,9 @@ static void importTypes(const model::Binary &Model, mlir::ModuleOp Module) {
   Module->setAttr("clift.test", mlir::ArrayAttr::get(Context, TypeAttrs));
 }
 
-class ImportCliftTypesPipe {
+class ImportTypesPipe {
 public:
-  static constexpr auto Name = "import-clift-types";
+  static constexpr auto Name = "import-types";
 
   std::array<pipeline::ContractGroup, 1> getContract() const {
     using namespace pipeline;
@@ -54,4 +54,4 @@ public:
   }
 };
 
-static pipeline::RegisterPipe<ImportCliftTypesPipe> Y;
+static pipeline::RegisterPipe<ImportTypesPipe> Y;
