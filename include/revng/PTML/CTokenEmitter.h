5019891429715bf71d453c47013ab3cbebd541e9
Ivan Krysak <ivan@rev.ng>
2026-07-02 14:55:44 +0000

    CTokenEmitter: add more documentation

--- include/revng/PTML/CTokenEmitter.h
+++ include/revng/PTML/CTokenEmitter.h
@@ -18,6 +18,23 @@ namespace ptml {
 /// Provides a stream-like interface for emitting C tokens and simple
 /// preprocessor directives. It is ensured that through this interface, only
 /// lexically valid C code can be emitted.
+//
+// IMPORTANT: If you are about to add a new method in here, please make sure
+//            what it emits is wrapped into a ptml `<span>` tag with:
+//            - `data-token` set (determines syntax highlighting),
+//            - either `data-location-definition` or `data-location-references`
+//              set (ensures correct ctrl+click behavior),
+//            - `data-allowed-actions` and `data-action-context-location` set
+//              iff it is actionable (allows for user interactivity).
+//
+//            Practically, you should use:
+//            - `PTML.initializeOpenTag(ptml::tags::Span)` for the tag,
+//            - `CTokenEmitter::getEntityKindAttribute` to select
+//              the `data-token` attribute,
+//            - `CTokenEmitter::getAllowedActions` to select
+//              the `data-allowed-actions`.
+//            - `data-location-definition` or `data-location-references` should
+//              be set based on the handle you already likely have.
 class CTokenEmitter {
   // It is very important to hide the PTML emitter and not to expose any direct
   // access to it in the public interface of this class. This design prevents
