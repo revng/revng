ff14b8a75f70da3c3c9ed42c8a85f0dc70a10236
Pietro Fezzardi <pietro@rev.ng>
2026-07-23 15:06:37 +0200

    FunctionTags: drop the unused CharInteger tag
    The char-integer literal-print decorator had no producers left. Its only
    reference was the isProgramPoint abort check in SwitchToStatements.

--- lib/Model/FunctionTags.cpp
+++ lib/Model/FunctionTags.cpp
@@ -88,14 +88,6 @@ FunctionPoolTag<llvm::Type *>
 Tag LiteralPrintDecorator("literal-print-decorator");
 
 FunctionPoolTag<llvm::Type *>
-  CharInteger("char-integer",
-              { llvm::Attribute::NoUnwind, llvm::Attribute::WillReturn },
-              llvm::MemoryEffects::none(),
-              { &FunctionTags::LiteralPrintDecorator,
-                &FunctionTags::UniquedByPrototype },
-              InitializationMode::InitializeFromReturnType);
-
-FunctionPoolTag<llvm::Type *>
   BoolInteger("bool-integer",
               { llvm::Attribute::NoUnwind, llvm::Attribute::WillReturn },
               llvm::MemoryEffects::none(),
