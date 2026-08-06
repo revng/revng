cc0177f32670e2740cf6af9cb4b34d242d3e6513
Giacomo Vercesi <giacomo.vercesi@rev.ng>
2026-08-06 14:39:28 +0200

    Lift.h: drop `KillReason`
    Drop the `KillReason` enum, as its has been unused since the commit
    'Introduce `AdvancedValueInfo` for JT discovery', which dropped
    `NoReturnAnalysis`.

--- include/revng/Lift/Lift.h
+++ include/revng/Lift/Lift.h
@@ -4,52 +4,9 @@
 // This file is distributed under the MIT License. See LICENSE.md for details.
 //
 
-#include "llvm/ADT/StringRef.h"
-#include "llvm/Pass.h"
-
 #include "revng/PipeboxCommon/BinariesContainer.h"
 #include "revng/PipeboxCommon/LLVMContainer.h"
 #include "revng/PipeboxCommon/Model.h"
-#include "revng/Support/Debug.h"
-
-namespace KillReason {
-
-enum Values {
-  NonKiller,
-  KillerSyscall,
-  EndlessLoop,
-  LeadsToKiller
-};
-
-inline llvm::StringRef getName(Values Reason) {
-  switch (Reason) {
-  case NonKiller:
-    return "NonKiller";
-  case KillerSyscall:
-    return "KillerSyscall";
-  case EndlessLoop:
-    return "EndlessLoop";
-  case LeadsToKiller:
-    return "LeadsToKiller";
-  }
-
-  revng_abort("Unexpected reason");
-}
-
-inline Values fromName(llvm::StringRef Name) {
-  if (Name == "NonKiller")
-    return NonKiller;
-  if (Name == "KillerSyscall")
-    return KillerSyscall;
-  else if (Name == "EndlessLoop")
-    return EndlessLoop;
-  else if (Name == "LeadsToKiller")
-    return LeadsToKiller;
-  else
-    revng_abort("Unexpected name");
-}
-
-} // namespace KillReason
 
 namespace revng::pypeline::piperuns {
 
