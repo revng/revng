0afaa57e404112cc0003c2ba4bbd369b80367202
Pietro Fezzardi <pietro@rev.ng>
2026-08-25 16:26:55 +0200

    Clifter: drop local variables for aggregate return
    SegregateStackAccesses already creates a new local variable for each
    call site whose model prototype returns an aggregate.
    Due to the quirks of the legacy decompilation pipeline, the Clifter
    created a second variable.
    This is no longer necessary and it makes code generation worse.

--- lib/Clifter/Clifter.cpp
+++ lib/Clifter/Clifter.cpp
@@ -752,15 +752,6 @@ class ClifterImpl::FunctionClifter {
   // variable.
   llvm::DenseMap<const llvm::AllocaInst *, mlir::Value> AllocaMapping;
 
-  // Maps an LLVM CallInst that returns SPTAR to the Clift LocalVariableOp that
-  // holds the return value.
-  // All LLVM uses of the CallInst use it as a pointer, as if it was pointing to
-  // the storage holding the return value. This is a hack, and should be avoided
-  // in the future. Until then, for the generation of uses of the SPTAR call in
-  // Clift, the address of the LocalVariablOp must be taken, to preserve
-  // semantic.
-  llvm::DenseMap<const llvm::CallInst *, mlir::Value> SPTARCallVariableMapping;
-
 public:
   static clift::FunctionOp import(ClifterImpl &C, const llvm::Function *F) {
     const model::Function *MF = llvmToModelFunction(C.Model, *F);
@@ -1419,19 +1410,6 @@ private:
 
       mlir::Location Loc = C.getLocation(I);
 
-      if (auto It = SPTARCallVariableMapping.find(I);
-          It != SPTARCallVariableMapping.end()) {
-        revng_log(ExpressionLog, "<existing value>");
-
-        mlir::Value Value = It->second;
-
-        Value = Builder.create<AddressofOp>(SurroundingLocation,
-                                            C.getPointerType(Value.getType()),
-                                            Value);
-
-        rc_return Value;
-      }
-
       // Any call without a prototype is considered a helper function and is
       // imported separately:
       if (not I->hasMetadata(PrototypeMDName))
@@ -1685,17 +1663,6 @@ private:
     Builder.create<GotoOp>(Loc, Iterator->second.Label);
   }
 
-  bool returnsAggregate(const llvm::CallInst *Call) {
-    if (not Call->hasMetadata(PrototypeMDName))
-      return false;
-
-    const auto *ModelCallType = getCallSitePrototype(C.Model, Call);
-    auto Layout = abi::FunctionType::Layout::make(*ModelCallType);
-    namespace ReturnMethod = abi::FunctionType::ReturnMethod;
-
-    return Layout.returnMethod() == ReturnMethod::ModelAggregate;
-  }
-
   // This function emits a single basic block as part of a larger C scope.
   // Nested scopes reached through branches at the end of this basic block are
   // emitted recursively using emitScope.
@@ -1795,13 +1762,8 @@ private:
         continue;
       }
 
-      // Call instructions require special handling:
-      // 1. Some call instructions are only used to represent information about
-      //    the scope-graph structure. Such calls are not emitted in Clift.
-      // 2. Some calls must only be evaluated once and as such are emitted as
-      //    local variable initializers, with any further use of that call value
-      //    during expression import being redirected to instead use the
-      //    local variable created here.
+      // Some call instructions are only used to represent information about
+      // the scope-graph structure. Such calls are not emitted in Clift.
       if (const llvm::CallInst *Call = llvm::dyn_cast<llvm::CallInst>(&I)) {
         if (isCallToTagged(Call, FunctionTags::GotoBlockMarker)) {
           HasGotoMarker = true;
@@ -1812,28 +1774,6 @@ private:
         // graph structure.
         if (isCallToTagged(Call, FunctionTags::ScopeCloserMarker))
           continue;
-
-        // Some function calls are emitted in local variable initializers.
-        if (returnsAggregate(Call)) {
-          mlir::Location Loc = C.getLocation(Call);
-
-          auto Op = Builder.create<ExpressionStatementOp>(Loc);
-
-          mlir::Value Local;
-          emitExpressionTreeImpl(Op.getExpression(), [&]() {
-            mlir::Value Initializer = emitExpression(Call, Loc);
-
-            mlir::Type Type = Initializer.getType();
-            Local = emitLocalDeclaration<LocalVariableOp>(Loc, Type);
-
-            return Builder.create<AssignOp>(Loc, Type, Local, Initializer);
-          });
-
-          auto [_, Inserted] = SPTARCallVariableMapping.try_emplace(Call,
-                                                                    Local);
-          revng_assert(Inserted);
-          continue;
-        }
       }
 
       // Finally, any instruction with no uses represents the root of an
