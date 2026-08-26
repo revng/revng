652c7b57b429eebae33adc2f3356b95223faad22
Lauri Vasama <vasama@rev.ng>
2026-08-26 11:05:34 +0300

    Add expression post processing pass
    Rewrite integer immediates with negative values as negations of
    non-negative values.

--- include/revng/CliftTransforms/Passes.h
+++ include/revng/CliftTransforms/Passes.h
@@ -26,6 +26,7 @@ PassPtr<clift::FunctionOp> createOptimizeExpressionsPass();
 
 PassPtr<clift::FunctionOp> createEmitFieldAccessesPass();
 
+PassPtr<clift::FunctionOp> createExpressionPostProcessingPass();
 PassPtr<clift::FunctionOp> createTerminalBranchComplementHoistingPass();
 PassPtr<clift::FunctionOp> createVariableScopeTighteningPass();
 PassPtr<clift::FunctionOp> createVariableInitializerHoistingPass();
