be9e5fe5c57dc93717db9469844aa1a083fd3ac6
Lauri Vasama <vasama@rev.ng>
2026-09-08 12:33:24 +0300

    Canonicalize immediates during legalization

--- lib/CliftTransforms/Legalization.cpp
+++ lib/CliftTransforms/Legalization.cpp
@@ -603,8 +603,6 @@ mlir::LogicalResult clift::legalizeForC(clift::FunctionOp Function) {
     Set.add<PointerComparisonPattern<SCmpLeOp>>(Context);
     Set.add<PointerComparisonPattern<SCmpGeOp>>(Context);
 
-    Set.add<BooleanCanonicalizationPattern>(Context, DataModel);
-
     Set.add<ArithmeticPromotionPattern<NegOp>>(Context, DataModel);
     Set.add<ArithmeticPromotionPattern<AddOp>>(Context, DataModel);
     Set.add<ArithmeticPromotionPattern<SubOp>>(Context, DataModel);
@@ -626,18 +624,26 @@ mlir::LogicalResult clift::legalizeForC(clift::FunctionOp Function) {
     // other rewrites.
     populateWithCastCanonicalizations(Set);
 
+    // Immediate canonicalization is used to re-type immediates after
+    // introduction of casts by the other rewrites.
+    populateWithImmediateCanonicalizations(Set);
+
     auto Patterns = mlir::FrozenRewritePatternSet(std::move(Set));
     if (mlir::applyPatternsAndFoldGreedily(Function, Patterns).failed())
       return mlir::failure();
   }
 
-  // Emit casts around unrepresentable immediates. This should only be done
-  // after promotions and pointer resizing, because those rewrites may surface
-  // opportunities for immediate canonicalisations.
+  // * Emit casts around unrepresentable immediates.
+  //   This should only be done after promotions and pointer resizing, because
+  //   those rewrites may surface opportunities for immediate canonicalisations.
+  // * Canonicalize boolean extensions.
+  //   This should only be done without cast canonicalization rewrites, as those
+  //   would undo the boolean extension canonicalization rewrites.
   {
     mlir::RewritePatternSet Set(Context);
 
     Set.add<ImmediateCastPattern>(Context, DataModel);
+    Set.add<BooleanCanonicalizationPattern>(Context, DataModel);
 
     auto Patterns = mlir::FrozenRewritePatternSet(std::move(Set));
     if (mlir::applyPatternsAndFoldGreedily(Function, Patterns).failed())
