8e0026bf056031e341f8d6988827c66b7567f15d
Lauri Vasama <vasama@rev.ng>
2026-09-16 11:17:51 +0300

    Add dedicated boolean negation simplification pass

--- lib/CliftTransforms/BooleanNegations.cpp
+++ lib/CliftTransforms/BooleanNegations.cpp
@@ -2,18 +2,67 @@
 // This file is distributed under the MIT License. See LICENSE.md for details.
 //
 
+#include "mlir/IR/PatternMatch.h"
+#include "mlir/Pass/Pass.h"
+#include "mlir/Transforms/GreedyPatternRewriteDriver.h"
+
 #include "revng/Clift/Clift.h"
 #include "revng/Clift/CliftOpHelpers.h"
 #include "revng/CliftTransforms/Expressions.h"
+#include "revng/CliftTransforms/Passes.h"
+
+namespace clift {
+#define GEN_PASS_DEF_CLIFTBOOLEANNEGATIONSIMPLIFICATION
+#include "revng/CliftTransforms/Passes.h.inc"
+} // namespace clift
 
 using namespace clift;
 
 namespace {
 
+namespace boolean_negations {
 #include "revng/CliftTransforms/BooleanNegations.h.inc"
+} // namespace boolean_negations
+
+template<typename T>
+using PassBase = impl::CliftBooleanNegationSimplificationBase<T>;
+
+struct BooleanNegationSimplificationPass
+  : PassBase<BooleanNegationSimplificationPass> {
+
+  mlir::FrozenRewritePatternSet Patterns;
+
+  mlir::LogicalResult initialize(mlir::MLIRContext *Context) override {
+    mlir::RewritePatternSet Set(Context);
+
+    populateWithBooleanNegationPatterns(Set);
+    Patterns = mlir::FrozenRewritePatternSet(std::move(Set),
+                                             disabledPatterns,
+                                             enabledPatterns);
+
+    return mlir::success();
+  }
+
+  void runOnOperation() override {
+    FunctionOp Function = getOperation();
+    mlir::Region &Body = Function.getBody();
+
+    if (Body.empty())
+      return;
+
+    mlir::GreedyRewriteConfig Config;
+    Config.maxIterations = mlir::GreedyRewriteConfig::kNoLimit;
+    if (mlir::applyPatternsAndFoldGreedily(Function, Patterns, Config).failed())
+      signalPassFailure();
+  }
+};
 
 } // namespace
 
 void clift::populateWithBooleanNegationPatterns(mlir::RewritePatternSet &Set) {
-  populateWithGenerated(Set);
+  boolean_negations::populateWithGenerated(Set);
+}
+
+PassPtr<FunctionOp> clift::createBooleanNegationSimplificationPass() {
+  return std::make_unique<BooleanNegationSimplificationPass>();
 }
